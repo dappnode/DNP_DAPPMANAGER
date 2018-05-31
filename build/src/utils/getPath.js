75b4c9845e92acf4e6ae557c597fbbb557521be1
Unknown <guillem.lion.benosa@gmail.com>
2018-05-31 14:51:12 +0200

    Changed paths for core packages
    Now all core packages will be in ./DNCORE

--- build/src/utils/getPath.js
+++ build/src/utils/getPath.js
@@ -1,23 +1,57 @@
 
 // Define paths
 module.exports = {
-  REPO_DIR: function(params) {
-    return params.REPO_DIR
+
+  PACKAGE_REPO_DIR: function(PACKAGE_NAME, params, IS_CORE) {
+    return REPO_DIR(PACKAGE_NAME, params, IS_CORE)
   },
-  PACKAGE_REPO_DIR: function(PACKAGE_NAME, params) {
-    return params.REPO_DIR + PACKAGE_NAME
+
+  MANIFEST: function(PACKAGE_NAME, params, IS_CORE) {
+    return REPO_DIR(PACKAGE_NAME, params, IS_CORE) + '/' + MANIFEST_NAME(PACKAGE_NAME, params, IS_CORE)
   },
-  MANIFEST: function(PACKAGE_NAME, params) {
-    return params.REPO_DIR + PACKAGE_NAME + '/' + params.DAPPNODE_PACKAGE_NAME
-  },
+
   DOCKERCOMPOSE: function(PACKAGE_NAME, params, IS_CORE) {
-    if (IS_CORE) return params.DNCORE_DIR + '/' + params.DOCKERCOMPOSE_NAME
-    else return params.REPO_DIR + PACKAGE_NAME + '/' + params.DOCKERCOMPOSE_NAME
+    return REPO_DIR(PACKAGE_NAME, params, IS_CORE) + '/' + DOCKERCOMPOSE_NAME(PACKAGE_NAME, params, IS_CORE)
   },
-  ENV_FILE: function(PACKAGE_NAME, params) {
-    return params.REPO_DIR + PACKAGE_NAME + '/' + PACKAGE_NAME + params.ENV_FILE_EXTENSION
+
+  ENV_FILE: function(PACKAGE_NAME, params, IS_CORE) {
+    return REPO_DIR(PACKAGE_NAME, params, IS_CORE) + '/' + PACKAGE_NAME + params.ENV_FILE_EXTENSION
   },
-  IMAGE: function(PACKAGE_NAME, IMAGE_NAME, params) {
-    return params.REPO_DIR + PACKAGE_NAME + '/' + IMAGE_NAME
+
+  IMAGE: function(PACKAGE_NAME, IMAGE_NAME, params, IS_CORE) {
+    return REPO_DIR(PACKAGE_NAME, params, IS_CORE) + '/' + IMAGE_NAME
   }
+}
+
+
+// Helper functions
+
+
+function REPO_DIR(PACKAGE_NAME, params, IS_CORE) {
+  if (IS_CORE) return params.DNCORE_DIR
+  return params.REPO_DIR + PACKAGE_NAME
+}
+
+
+function DOCKERCOMPOSE_NAME(PACKAGE_NAME, params, IS_CORE) {
+
+  if (!IS_CORE) return params.DOCKERCOMPOSE_NAME
+
+  const FILE_PREFIX = params.DOCKERCOMPOSE_NAME.split('.')[0]
+  const EXTENSION = params.DOCKERCOMPOSE_NAME.split('.')[1]
+  const PACKAGE_SHORT_NAME = PACKAGE_NAME.split('.')[0]
+
+  return FILE_PREFIX + '-' + PACKAGE_SHORT_NAME + '.' + EXTENSION
+}
+
+
+function MANIFEST_NAME(PACKAGE_NAME, params, IS_CORE) {
+
+  if (!IS_CORE) return params.DAPPNODE_PACKAGE_NAME
+
+  const FILE_PREFIX = params.DAPPNODE_PACKAGE_NAME.split('.')[0]
+  const EXTENSION = params.DAPPNODE_PACKAGE_NAME.split('.')[1]
+  const PACKAGE_SHORT_NAME = PACKAGE_NAME.split('.')[0]
+
+  return FILE_PREFIX + '-' + PACKAGE_SHORT_NAME + '.' + EXTENSION
 }
