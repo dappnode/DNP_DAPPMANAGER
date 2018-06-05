0d92649643243e2f3d5dd264c91d8a77deb6fc15
Unknown <guillem.lion.benosa@gmail.com>
2018-06-05 03:44:59 +0200

    Display version errors in the UI

--- build/src/calls/createListDirectory.js
+++ build/src/calls/createListDirectory.js
@@ -69,10 +69,12 @@ function createListDirectory(getDirectory,
 
 
 function getTag(v_now, v_avail) {
-  // Prevent the function from crashing
-  if (semver.valid(v_now) || semver.valid(v_avail)) return 'Install (unk ver)'
-  // Compare versions and return appropiate tag
+  // If there is no current version, display install
   if (!v_now) return 'Install'
+  // Prevent the function from crashing
+  if (!semver.valid(v_now)) return 'Install (unk v_now='+v_now+')'
+  if (!semver.valid(v_avail)) return 'Install (unk v_avail='+v_avail+')'
+  // Compare versions and return appropiate tag
   if (semver.lt(v_now, v_avail)) return 'Update'
   else return 'Installed'
 }
