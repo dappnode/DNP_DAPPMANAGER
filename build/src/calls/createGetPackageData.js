493d4255ba7a448ddc1cbbf12477a7dac3168673
Unknown <guillem.lion.benosa@gmail.com>
2018-06-11 17:32:43 +0200

    Added get package data call
    provided a package name, returns it's latest manifest and avatar

---
+++ build/src/calls/createGetPackageData.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const base64Img = require('base64-img')
+const dockerList_default = require('../modules/dockerList')
+const parse = require('../utils/parse')
+const res =   require('../utils/res')
+const ethchain = require('../watchers/ethchain')
+
+// CALL DOCUMENTATION:
+// > result = packages =
+//   [
+//     {
+//       name: packageName, (string)
+//       status: 'Preparing', (string)
+//       manifest: <Manifest>, (object)
+//       tag: 'Instaled', (string)
+//       avatar: <base64Img>, (string)
+//       avatarHash: <IPFS hash> (string)
+//     },
+//     ...
+//   ]
+
+function createGetPackageData(
+  getManifest,
+  ipfsCalls,
+  dockerList=dockerList_default) {
+
+  return async function getPackageData(req) {
+
+    const packageReq = parse.packageReq(req[0])
+
+    // Make sure the chain is synced
+    if (await ethchain.isSyncing()) {
+      return res.success("Mainnet is syncing", [])
+    }
+
+    const manifest = await getManifest(parse.packageReq(packageReq.name))
+
+    // Correct manifest
+    if (!manifest.type) manifest.type = 'library'
+
+    // Fetch the package image
+    const avatarHash = manifest.avatar
+    let avatar
+    if (avatarHash) {
+
+      // If the avatar can not be fetched don't crash
+      try {
+        await ipfsCalls.cat(avatarHash)
+        avatar = base64Img.base64Sync('cache/'+avatarHash)
+
+      } catch(e) {
+        console.log('(createGetPackageData.js line 49) Could not fetch avatar of '+packageReq.name+' at '+avatarHash)
+      }
+
+    }
+
+    return res.success('Got data of '+packageReq.name, {
+      manifest,
+      avatar
+    })
+
+  }
+}
+
+
+
+module.exports = createGetPackageData
