0d547dfcec5ded34d5a1d33a3e9147afa46f8c4d
Unknown <guillem.lion.benosa@gmail.com>
2018-08-01 12:41:33 +0200

    Paralelize port closing

--- build/src/src/calls/createRemovePackage.js
+++ build/src/src/calls/createRemovePackage.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const getPath = require('utils/getPath');
 const shellSync = require('utils/shell');
-const parse = require('utils/parse');
 const logUI = require('utils/logUI');
 const paramsDefault = require('params');
 const dockerDefault = require('modules/docker');
@@ -34,15 +33,6 @@ function createRemovePackage({
       throw Error('The installer cannot be restarted');
     }
 
-    // Close ports
-    logUI({logId, pkg: 'all', msg: 'closing ports...'});
-    try {
-      await closePorts(dockerComposePath, docker);
-    } catch (e) {
-      logUI({logId, pkg: 'all', msg: 'Error closing ports '+(e ? e.message : '')});
-    }
-
-
     // Remove container (and) volumes
     logUI({logId, pkg: 'all', msg: 'Shutting down containers...'});
     await docker.compose.down(dockerComposePath, {volumes: Boolean(deleteVolumes)});
@@ -58,13 +48,6 @@ function createRemovePackage({
 
   // Expose main method
   return removePackage;
-}
-
-async function closePorts(dockerComposePath, docker) {
-  const ports = parse.dockerComposePorts(dockerComposePath);
-  for (const port of ports) {
-    await docker.closePort(port);
-  }
 }
 
 
