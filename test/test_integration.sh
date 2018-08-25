acd595906e8be08dad232410f151309b1710ffda
Unknown <guillem.lion.benosa@gmail.com>
2018-08-25 14:00:43 +0200

    No need to sleep 60s
    No need to sleep. The IPFS, WAMP and BIND module have 117s to initialize 
    while the dappmanager is building. Also the dappmanager has time to initialize while it is installing git and mocha

--- test/test_integration.sh
+++ test/test_integration.sh
@@ -3,7 +3,11 @@
 DAPPNODE_DIR="test_build"
 docker-compose -f ${DAPPNODE_DIR}/docker-compose-dappmanager.yml up -d
 
-sleep 60
+# No need to sleep. The IPFS, WAMP and BIND module have 117s to initialize 
+# while the dappmanager is building. Also the dappmanager has time to initialize
+# while it is installing git and mocha
+# sleep 60
+
 docker logs DAppNodeCore-dappmanager.dnp.dappnode.eth
 
 # Print state for debugging
