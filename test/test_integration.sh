e70a0f81eb2d536224ee6f514f02722829b453e1
Unknown <guillem.lion.benosa@gmail.com>
2018-08-25 14:27:14 +0200

    add logs for debugging

--- test/test_integration.sh
+++ test/test_integration.sh
@@ -3,12 +3,10 @@
 DAPPNODE_DIR="test_build"
 docker-compose -f ${DAPPNODE_DIR}/docker-compose-dappmanager.yml up -d
 
-# No need to sleep. The IPFS, WAMP and BIND module have 117s to initialize 
-# while the dappmanager is building. Also the dappmanager has time to initialize
-# while it is installing git and mocha
-# sleep 60
+sleep 30
 
 docker logs DAppNodeCore-dappmanager.dnp.dappnode.eth
+docker logs DAppNodeCore-ipfs.dnp.dappnode.eth
 
 # Print state for debugging
 docker network ls
