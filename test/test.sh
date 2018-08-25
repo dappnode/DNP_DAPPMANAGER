f522f06390de44e295c39cd74a698a65a3d7bb5b
Unknown <guillem.lion.benosa@gmail.com>
2018-08-25 12:44:35 +0200

    run integration tests

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,13 @@ docker-compose -f ${DAPPNODE_DIR}/docker-compose-dappmanager.yml up -d
 sleep 60
 docker logs DAppNodeCore-dappmanager.dnp.dappnode.eth
 
-# docker-compose -f ${DAPPNODE_DIR}/docker-compose-test.yml build
+# Print state for debugging
 docker network ls
 docker ps -a
-# docker-compose -f ${DAPPNODE_DIR}/docker-compose-test.yml run test
+
+# Install git and mocha
+docker exec -it DAppNodeCore-dappmanager.dnp.dappnode.eth apk update
+docker exec -it DAppNodeCore-dappmanager.dnp.dappnode.eth apk add git
+docker exec -it DAppNodeCore-dappmanager.dnp.dappnode.eth npm i mocha
+# Run integration tests
+docker exec -it DAppNodeCore-dappmanager.dnp.dappnode.eth npm run test.int
