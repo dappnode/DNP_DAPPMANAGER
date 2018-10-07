3cdfbe79db3c068f480d0651a03c1aae2db52fd4
Unknown <guillem.lion.benosa@gmail.com>
2018-10-07 23:16:15 +0200

    Resize + compress avatars

--- build/src/src/calls/fetchDirectory.js
+++ build/src/src/calls/fetchDirectory.js
@@ -2,13 +2,12 @@ const getDirectory = require('modules/getDirectory');
 const {eventBus, eventBusTag} = require('eventBus');
 const logs = require('logs.js')(module);
 const getManifest = require('modules/getManifest');
-const base64Img = require('base64-img');
-const ipfs = require('modules/ipfs');
-const params = require('params');
+const getAvatar = require('modules/getAvatar');
 const parse = require('utils/parse');
 const isSyncing = require('utils/isSyncing');
 
 let packagesCache;
+let avatarCache = {};
 
 function emitPkg(pkg) {
   const pkgsObj = {
@@ -76,11 +75,17 @@ const fetchDirectory = async () => {
 
     // Fetch the package image
     const avatarHash = manifest.avatar;
+
     let avatar;
     if (avatarHash) {
       try {
-        await ipfs.cat(avatarHash);
-        avatar = base64Img.base64Sync(params.CACHE_DIR + avatarHash);
+        // Retrieve cached avatar or fetch it
+        if (avatarCache[avatarHash]) {
+          avatar = avatarCache[avatarHash];
+        } else {
+          avatar = await getAvatar(avatarHash);
+          avatarCache[avatarHash] = avatar;
+        }
         emitPkg({name, avatar});
       } catch (e) {
         // If the avatar can not be fetched don't crash
@@ -96,8 +101,9 @@ const fetchDirectory = async () => {
     };
   }));
 
+  const payloadSize = Math.floor(Buffer.byteLength(JSON.stringify(packagesCache), 'utf8')/1000);
   return {
-    message: 'Listed directory with ' + packagesCache.length + ' packages',
+    message: `Listed directory with ${packagesCache.length} packages (${payloadSize} KB)`,
     result: packagesCache,
     logMessage: true,
   };
