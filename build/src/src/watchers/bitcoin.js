a1fb8015a9f6e9b2ce866f296b892361ee6599f6
Unknown <guillem.lion.benosa@gmail.com>
2018-12-07 00:12:54 +0800

    Added a bitcoin chain watcher
    Former-commit-id: 9235a8c232b4ffe532d8d1c5fc2a17bf60a8b078

---
+++ build/src/src/watchers/bitcoin.js
@@ -0,0 +1,31 @@
+const Client = require('bitcoin-core');
+
+const MIN_BLOCK_DIFF_SYNC = 10;
+
+async function bitcoin(chain) {
+    const res = {name: chain.name};
+    try {
+        const client = new Client({
+            host: chain.api,
+            password: 'dappnode',
+            username: 'dappnode',
+        });
+        const blockIndex = await client.getBlockCount();
+        const blockHash = await client.getBlockHash(blockIndex);
+        const block = await client.getBlock(blockHash);
+        const secondsDiff = Math.floor(Date.now()/1000) - block.time;
+        const blockDiffAprox = Math.floor(secondsDiff/(60*10));
+
+        if (blockDiffAprox > MIN_BLOCK_DIFF_SYNC) {
+            res.syncing = true;
+            res.msg = `Blocks synced: ${blockIndex} / ${blockDiffAprox + blockIndex}`;
+        } else {
+            res.syncing = false;
+            res.msg = 'Synced #' + blockIndex;
+        }
+    } catch (e) {
+        res.error = e.message;
+    }
+}
+
+module.exports = bitcoin;
