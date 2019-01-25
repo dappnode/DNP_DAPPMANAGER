fc11701169967e6ee9a7a5db0e2ec9f64d66f991
Unknown <guillem.lion.benosa@gmail.com>
2019-01-25 21:57:33 +0700

    Move all test cases into the case folder
    Former-commit-id: cc027635d650ff0b48c34d7c42f31890a34d9f00

--- build/src/test/modules/dappGet/cases/index.js
+++ build/src/test/modules/dappGet/cases/index.js
@@ -1,5 +1,7 @@
 module.exports = [
-    // require('./normal-case'),
-    // require('./dependency-loop'),
+    require('./normal-case'),
     require('./core-update'),
+    require('./circular_dependencies'),
+    require('./upgrade-state-because-dependency.js'),
+    require('./dont_install_package.js'),
 ];
