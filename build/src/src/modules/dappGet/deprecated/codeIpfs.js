cac74e02cf8f5745d14dcaac497b7cf5471692c8
Unknown <guillem.lion.benosa@gmail.com>
2019-01-25 15:30:16 +0700

    Remove deprecated files

--- build/src/src/modules/dappGet/deprecated/codeIpfs.js
+++
@@ -1,9 +0,0 @@
-function codeIpfs(manifest, verReq) {
-    if (verReq && verReq.startsWith('/ipfs/')) {
-        return manifest.version + '-' + verReq.replace('/ipfs/', 'ipfs-');
-    } else {
-        return;
-    }
-}
-
-module.exports = codeIpfs;
