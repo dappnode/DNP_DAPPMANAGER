5df3720079978dbe0674bf4cf72729921a6dd819
Unknown <guillem.lion.benosa@gmail.com>
2019-02-12 18:32:55 +0100

    copy files from and to a container
    Former-commit-id: 3cdb62c57a5c14c3463249206dac69daf0075d14

---
+++ build/src/test/utils/randomToken.test.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const randomToken = require('utils/randomToken');
+
+// module.exports = {
+//     load: loadEnvs,
+//     write: writeEnvs,
+//     getManifestEnvs,
+//   };
+
+describe('Util: randomToken', () => {
+  it('should generate a random token', async () => {
+    const byteLength = 32;
+    const s = await randomToken(32);
+
+    expect(s).to.be.a('string');
+    expect(s.length).to.equal(2 * byteLength);
+  });
+});
