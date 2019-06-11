7a766e1c6b52b4d26d78ab4d6be086cb2fce7170
vdo <vdo@greyfaze.net>
2019-06-11 11:55:40 +0200

    2h timeout for mappings

--- build/src/src/calls/managePorts.js
+++ build/src/src/calls/managePorts.js
@@ -7,6 +7,7 @@ const upnpc = require("modules/upnpc");
  * @param {array} ports: array of port objects
  * ports = [ { number: 30303, type: TCP }, ... ]
  */
+
 const managePorts = async ({ action, ports }) => {
   if (!Array.isArray(ports)) {
     throw Error(`kwarg ports must be an array: ${JSON.stringify(ports)}`);
--- build/src/src/modules/upnpc/open.js
+++ build/src/src/modules/upnpc/open.js
@@ -3,9 +3,9 @@ const validateKwargs = require("./validateKwargs");
 const parseOpenOutput = require("./parseOpenOutput");
 
 /**
- * Closes port = deletes port mapping
+ * Opens port = adds port mapping
  * Actual command example:
- *   docker run --rm --net=host ${IMAGE} upnpc -e DAppNode -r 500 UDP
+ *   docker run --rm --net=host ${IMAGE} upnpc -e DAppNode -r 500 UDP 7200
  *
  * @param {object} kwargs: {
  *   number: '3000',
@@ -13,10 +13,13 @@ const parseOpenOutput = require("./parseOpenOutput");
  * }
  * @returns {*}
  */
+// Timeout in seconds. Should be greater than the natRenewalInterval
+const natRenewalTimeout = 120 * 60;
+
 async function open({ number, type }) {
   validateKwargs({ number, type });
   try {
-    const res = await upnpcCommand(`-e DAppNode -r ${number} ${type}`);
+    const res = await upnpcCommand(`-e DAppNode -r ${number} ${type} ${natRenewalTimeout}`);
     return parseOpenOutput(res);
   } catch (e) {
     parseOpenOutput(e.message);
