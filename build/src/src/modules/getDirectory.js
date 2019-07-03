ede7276e0e5f526928175e8b21cb4375899bbd3d
dapplion <guillem.lion.benosa@gmail.com>
2019-07-03 12:23:17 +0200

    Implement new directory contract
    fix

--- build/src/src/modules/getDirectory.js
+++ build/src/src/modules/getDirectory.js
@@ -4,13 +4,7 @@ const web3 = require("./web3Setup");
 const isEnsDomain = require("utils/isEnsDomain");
 
 // Contract parameters
-const DAppNodePackageStatus = [
-  "Preparing",
-  "Develop",
-  "Active",
-  "Deprecated",
-  "Deleted"
-];
+const DAppNodePackageStatus = ["Deleted", "Active", "Developing"];
 
 /**
  * Fetches all package names in the custom dappnode directory.
@@ -29,25 +23,70 @@ async function getDirectory() {
     directoryContract.abi,
     directoryContract.address
   );
-  const numberOfDAppNodePackages = parseFloat(
+  const numberOfDAppNodePackages = parseInt(
     await directory.methods.numberOfDAppNodePackages().call()
   );
 
-  let packages = [];
+  // Get featured packages list
+  // 0x0b00000000000000000000000000000000000000000000000000000000000000
+  const featuredBytes = await directory.methods.featured().call();
+  // ["0b", "00", ...]
+  /**
+   * 1. Strip hex prefix
+   * 2. Split by substrings of 2 characters
+   * 3. Remove 0 indexes
+   * 4. Remove duplicate indexes
+   * 5. Base64 to decimal index
+   */
+  const featuredIndexes = featuredBytes
+    .replace("0x", "")
+    .match(/.{1,2}/g)
+    .filter(value => value !== "00")
+    .filter((value, index, self) => self.indexOf(value) === index)
+    .map(base64 => parseInt(base64, 16));
+
+  let directoryIds = [];
   for (let i = 0; i < numberOfDAppNodePackages; i++) {
-    try {
-      const { name, status } = await directory.methods.getPackage(i).call();
-      // Make sure the DNP is not Deprecated or Deleted
-      if (isEnsDomain(name) && status < 3)
-        packages.push({
-          name,
-          status: DAppNodePackageStatus[status],
-          directoryId: i
-        });
-    } catch (e) {
-      logs.error(`Error retrieving DNP #${i} from directory: ${e.stack}`);
-    }
+    directoryIds.push(i);
   }
+
+  let packages = [];
+  await Promise.all(
+    directoryIds.map(async i => {
+      try {
+        const {
+          name,
+          status: statusBn,
+          position: positionBn
+        } = await directory.methods.getPackage(i).call();
+
+        const status = parseInt(statusBn);
+        const position = parseInt(positionBn);
+
+        // Make sure the DNP is not Deprecated or Deleted
+        if (!isEnsDomain(name) || status === 0) return;
+
+        const pkg = {
+          name,
+          status,
+          statusName: DAppNodePackageStatus[status],
+          position,
+          directoryId: i
+        };
+
+        const featuredIndex = featuredIndexes.indexOf(i);
+        if (featuredIndex > -1) {
+          pkg.isFeatured = true;
+          pkg.featuredIndex = featuredIndex;
+        }
+
+        packages.push(pkg);
+      } catch (e) {
+        logs.error(`Error retrieving DNP #${i} from directory: ${e.stack}`);
+      }
+    })
+  );
+
   return packages;
 }
 
