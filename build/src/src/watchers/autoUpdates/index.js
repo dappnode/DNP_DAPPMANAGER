b5e02352615a2d0ea6805e5fbf66b38d02159166
dapplion <guillem.lion.benosa@gmail.com>
2019-07-16 13:09:57 +0200

    Simplify to only two package groups and on/off

--- build/src/src/watchers/autoUpdates/index.js
+++ build/src/src/watchers/autoUpdates/index.js
@@ -5,52 +5,74 @@ const apm = require("modules/apm");
 // External calls
 const installPackage = require("calls/installPackage");
 // Utils
-const { isUpdateAllowed } = require("utils/autoUpdateHelper");
+const {
+  isDnpUpdateEnabled,
+  isDnpUpdateAllowed,
+  isCoreUpdateEnabled,
+  isCoreUpdateAllowed
+} = require("utils/autoUpdateHelper");
 const parse = require("utils/parse");
 
 const coreDnpName = "core.dnp.dappnode.eth";
 
 const monitoringInterval = 5 * 60 * 1000; // (ms) (5 minutes)
 
-/**
- * Auto-update approach:
- * - (1) The watcher should JUST check if the relevant packages
- *   have a more recent version. Then trigger a version check
- *   and if activated, auto-update
- * - ### TODO (2) There have to be a method that checks if a version is ready.
- *   That means resolving the dependencies and making sure all the
- *   necessary content is available. This method has to be called by
- *   - Fetch directory RPC (Directory show)
- *   - Fetch package info RPC (single DNP search in the UI)
- *   - When the watcher finds a new version
- * - (3) The auto-update method when (1) finds a new version and (2)
- *   can verify that it is fully available.
- */
+// Wrappers / utils
+const getLatestVersion = id => apm.getLatestSemver(parse.packageReq(id));
+const update = (id, version, options) =>
+  installPackage({ id: [id, version].join("."), options });
 
+/**
+ * Auto-update:
+ * All code is sequential, to not perform more than one update at once.
+ * One of the update might be the core and crash the other updates.
+ */
 async function autoUpdates() {
   try {
+    const dnpUpdateEnabled = await isDnpUpdateEnabled();
+    const coreUpdateEnabled = await isCoreUpdateEnabled();
+
+    // Abort early if there are no updates to perform
+    if (!dnpUpdateEnabled && !coreUpdateEnabled) return;
+
     const dnpList = await dockerList.listContainers();
-    const dnps = dnpList
-      .filter(dnp => (!dnp.isCore && !dnp.origin) || dnp.name === coreDnpName)
-      .filter(dnp => semver.valid(dnp.version));
 
-    for (const { name, version } of dnps) {
-      try {
-        const latestVersion = await apm.getLatestSemver(parse.packageReq(name));
+    if (dnpUpdateEnabled) {
+      const dnps = dnpList.filter(
+        dnp => dnp.isDnp && semver.valid(dnp.version)
+      );
+      for (const { name, version } of dnps) {
+        try {
+          const latestVersion = await getLatestVersion(name);
 
-        if (await isUpdateAllowed(name, version, latestVersion)) {
-          // Do not perform more than one update at once
-          // One of the update might be the core and crash the other updates
-          try {
-            logs.info(`Auto-updating ${name} to ${latestVersion}`);
-            await installPackage({ id: [name, latestVersion].join(".") });
-            logs.info(`Successful auto-update ${name} to ${latestVersion}`);
-          } catch (e) {
-            logs.error(`Error auto-updating ${name}: ${e.stack}`);
+          if (await isDnpUpdateAllowed(version, latestVersion)) {
+            logs.info(`Auto-updating ${name} to ${latestVersion}...`);
+            await update(name, latestVersion);
+            logs.info(`Successfully auto-updated ${name} to ${latestVersion}`);
           }
+        } catch (e) {
+          logs.error(`Error auto-updating ${name}: ${e.stack}`);
+        }
+      }
+    }
+
+    if (coreUpdateEnabled) {
+      try {
+        const coreDnp = dnpList.find(dnp => dnp.name === coreDnpName);
+        if (!coreDnp) throw Error(`Core DNP not found`);
+        const version = coreDnp.version;
+        if (!semver.valid(version))
+          throw Error(`Invalid core version ${version}`);
+
+        const latestVersion = await getLatestVersion(coreDnpName);
+
+        if (await isCoreUpdateAllowed(version, latestVersion)) {
+          logs.info(`Auto-updating system to ${latestVersion}...`);
+          await update(coreDnpName, latestVersion, { BYPASS_RESOLVER: true });
+          logs.info(`Successfully auto-updated system to ${latestVersion}`);
         }
       } catch (e) {
-        logs.error(`Error checking updates for ${name}: ${e.stack}`);
+        logs.error(`Error auto-updating system: ${e.stack}`);
       }
     }
   } catch (e) {
