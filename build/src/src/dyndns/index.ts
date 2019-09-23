f024d1b147dfebad41dbe329fbabd27ea064b226
vdo <vdo@greyfaze.net>
2019-09-23 16:38:26 +0200

    Adding dyndns stuff

---
+++ build/src/src/dyndns/index.ts
@@ -0,0 +1,9 @@
+import updateIp from "./updateIp";
+import generateKeys from "./generateKeys";
+import checkIpAndUpdateIfNecessary from "./checkIpAndUpdateIfNecessary";
+
+export = {
+  updateIp,
+  generateKeys,
+  checkIpAndUpdateIfNecessary
+};
