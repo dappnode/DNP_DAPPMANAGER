8d5ac071e7a3d88bb62b9cc0509b37af0fe50915
vdo <vdo@greyfaze.net>
2019-10-01 11:11:50 +0200

    Migrate vpn db

--- build/src/src/db/systemFlags.ts
+++ build/src/src/db/systemFlags.ts
@@ -2,6 +2,7 @@ import { staticKey } from "./lowLevelDb";
 
 const ARE_ENV_FILES_MIGRATED = "are-env-files-migrated";
 const IMPORTED_INSTALLATION_STATIC_IP = "imported-installation-staticIp";
+const IS_VPN_DB_MIGRATED = "is-vpn-db-migrated";
 
 export const areEnvFilesMigrated = staticKey<boolean>(
   ARE_ENV_FILES_MIGRATED,
@@ -12,3 +13,5 @@ export const importedInstallationStaticIp = staticKey<boolean>(
   IMPORTED_INSTALLATION_STATIC_IP,
   false
 );
+
+export const isVpnDbMigrated = staticKey<boolean>(IS_VPN_DB_MIGRATED, false);
