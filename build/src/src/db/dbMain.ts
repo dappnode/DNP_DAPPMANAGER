6156e4a0be8dd29c3a57ed906474e610265c7baf
dapplion <guillem.lion.benosa@gmail.com>
2019-10-01 14:23:58 +0200

    Migrate old db key to new main db

--- build/src/src/db/dbMain.ts
+++ build/src/src/db/dbMain.ts
@@ -2,7 +2,7 @@ import params from "../params";
 import dbFactory from "./dbFactory";
 
 // Initialize db
-const db = dbFactory(params.DB_PATH || "./dappmanagerdb.json");
+const db = dbFactory(params.DB_MAIN_PATH || "./maindb.json");
 export const staticKey = db.staticKey;
 export const dynamicKeyValidate = db.dynamicKeyValidate;
-export const clearDb = db.clearDb;
+export const lowLevel = db.lowLevel;
