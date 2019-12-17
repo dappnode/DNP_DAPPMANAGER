3772561b4c1851b56f5d9f16768752f9eebbb124
vdo <vdo@greyfaze.net>
2019-12-17 14:20:58 +0100

    Detect usb symlinks

--- build/src/hostScripts/detect_fs.sh
+++ build/src/hostScripts/detect_fs.sh
@@ -12,12 +12,22 @@ for line in $(df -hlPT | grep ^/); do
     free=$(echo "$line" | awk '{ print $5 }')
     partition=$(echo "$line" | awk '{ print $1 }' | xargs basename)
     device="$(readlink -f "/sys/class/block/$partition/.." | xargs basename || true)"
-    model="$(lsblk -no MODEL /dev/$device 2> /dev/null | sed -e 's/[[:space:]]*$//' || true)"
-    vendor="$(lsblk -no VENDOR /dev/$device 2> /dev/null | sed -e 's/[[:space:]]*$//' || true)"
-	  # print in JSON format
-	  [[ "$NEXT" != "true" ]] || echo -n ","
+    model="$(lsblk -no MODEL /dev/$device 2>/dev/null | sed -e 's/[[:space:]]*$//' || true)"
+    vendor="$(lsblk -no VENDOR /dev/$device 2>/dev/null | sed -e 's/[[:space:]]*$//' || true)"
+
+    # Detect devices mounted with usbmount, and grab a deterministic mount point
+    usbmount_symlink=""
+    if [ -d /run/usbmount ]; then
+      usbmount_symlink=$(ls -1d /run/usbmount/* | xargs -I {} -i sh -c 'echo $(readlink -f "{}"):{}' | grep ${mountpoint} | cut -d":" -f2)
+      if [ -n "${usbmount_symlink}" ]; then
+        mountpoint="${usbmount_symlink}"
+      fi
+    fi
+
+    # print in JSON format
+    [[ "$NEXT" != "true" ]] || echo -n ","
     echo -n "{\"mountpoint\": \"$mountpoint\", \"use\": \"$use\", \"free\": \"$free\", \"vendor\": \"$vendor\", \"model\": \"$model\"}"
-	  NEXT=true
+    NEXT=true
   fi
 done
 echo "]"
