be50aa30ba77b787c314f0051cd04ce6eab3830e
vdo <vdo@greyfaze.net>
2020-03-17 11:20:26 +0100

    Check number of args on migration

--- build/src/hostScripts/migrate_volume.sh
+++ build/src/hostScripts/migrate_volume.sh
@@ -4,12 +4,14 @@
 # It's original purpose is to migrate ETHCHAIN volumes to multi-client model.
 #
 
-# Exit on error
-set -e
-
 FROM_VOLUME_NAME=$1
 TO_VOLUME_NAME=$2
 
 DOCKER_ROOT_DIR=$(docker info -f '{{ .DockerRootDir }}')
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters. Please provide origin and destination volume."
+    exit 1
+fi
 
 mv "${DOCKER_ROOT_DIR}/volumes/${FROM_VOLUME_NAME}" "${DOCKER_ROOT_DIR}/volumes/${TO_VOLUME_NAME}"
