ef55efd4c7a1851d9a38bb4295b387fadc8b71e5
vdo <vdo@greyfaze.net>
2020-03-17 11:20:26 +0100

    Check args on migrate_volume script

--- build/src/hostScripts/migrate_volume.sh
+++ build/src/hostScripts/migrate_volume.sh
@@ -2,7 +2,6 @@
 #
 # Script to migrate a docker volume.
 # It's original purpose is to migrate ETHCHAIN volumes to multi-client model.
-#
 
 # Exit on error
 set -e
@@ -11,5 +10,22 @@ FROM_VOLUME_NAME=$1
 TO_VOLUME_NAME=$2
 
 DOCKER_ROOT_DIR=$(docker info -f '{{ .DockerRootDir }}')
+
+if [[ -z "${FROM_VOLUME_NAME}" ]]; then
+  echo "arg 1 FROM_VOLUME_NAME is empty"
+  exit 1
+fi
+
+if [[ -z "${TO_VOLUME_NAME}" ]]; then
+  echo "arg 2 TO_VOLUME_NAME is empty"
+  exit 1
+fi
+
+if [[ -z "${DOCKER_ROOT_DIR}" ]]; then
+  echo "DOCKER_ROOT_DIR is empty"
+  exit 1
+fi
+
+
 
 mv "${DOCKER_ROOT_DIR}/volumes/${FROM_VOLUME_NAME}" "${DOCKER_ROOT_DIR}/volumes/${TO_VOLUME_NAME}"
--- build/src/src/modules/hostScripts/runScripts.ts
+++ build/src/src/modules/hostScripts/runScripts.ts
@@ -77,6 +77,13 @@ export async function migrateVolume(
   fromVolumeName: string,
   toVolumeName: string
 ): Promise<void> {
+  for (const [id, name] of Object.entries({ fromVolumeName, toVolumeName })) {
+    // Make sure a wrong path is not created, and prevent "../../" paths
+    if (!name) throw new Error(`${id} must not be empty`);
+    if (/(\.){2,}/.test(name)) throw Error(`${id} must not contain '..'`);
+    if (/\//.test(name)) throw Error(`${id} must not be a path`);
+  }
+
   await runScript("migrate_volume.sh", `${fromVolumeName} ${toVolumeName}`);
 }
 
