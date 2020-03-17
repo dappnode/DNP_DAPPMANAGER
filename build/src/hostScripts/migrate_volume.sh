7d47d33f5618c4b9f709124db037b5b70577bf65
dapplion <guillem.lion.benosa@gmail.com>
2020-03-17 13:34:25 +0100

    Check all migrate_volume arguments

--- build/src/hostScripts/migrate_volume.sh
+++ build/src/hostScripts/migrate_volume.sh
@@ -2,16 +2,30 @@
 #
 # Script to migrate a docker volume.
 # It's original purpose is to migrate ETHCHAIN volumes to multi-client model.
-#
+
+# Exit on error
+set -e
 
 FROM_VOLUME_NAME=$1
 TO_VOLUME_NAME=$2
 
 DOCKER_ROOT_DIR=$(docker info -f '{{ .DockerRootDir }}')
 
-if [ "$#" -ne 2 ]; then
-    echo "Illegal number of parameters. Please provide origin and destination volume."
-    exit 1
+if [[ -z "${FROM_VOLUME_NAME}" ]]; then
+  echo "arg 1 FROM_VOLUME_NAME is empty"
+  exit 1
 fi
+
+if [[ -z "${TO_VOLUME_NAME}" ]]; then
+  echo "arg 2 TO_VOLUME_NAME is empty"
+  exit 1
+fi
+
+if [[ -z "${DOCKER_ROOT_DIR}" ]]; then
+  echo "DOCKER_ROOT_DIR is empty"
+  exit 1
+fi
+
+
 
 mv "${DOCKER_ROOT_DIR}/volumes/${FROM_VOLUME_NAME}" "${DOCKER_ROOT_DIR}/volumes/${TO_VOLUME_NAME}"
