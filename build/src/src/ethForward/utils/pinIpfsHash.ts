49469e172415d227eb4d2870d600664b6903dca2
dapplion <guillem.lion.benosa@gmail.com>
2020-04-09 18:33:55 +0200

    Fix EthForward IPFS pin add url parsing error

--- build/src/src/ethForward/utils/pinIpfsHash.ts
+++ build/src/src/ethForward/utils/pinIpfsHash.ts
@@ -1,14 +1,20 @@
-import fetch from "node-fetch";
+const ipfsAPI = require("ipfs-http-client");
+import { promisify } from "util";
 import params from "../../params";
 
-const ipfsApi = params.IPFS_HOST;
-
-const addEndpoint = (hash: string): string => `${ipfsApi}/pin/add?arg=${hash}`;
+/**
+ * [ { hash: 'QmNqDvqAyy3pN3PvymB6chM7S1FgYyive8LosVKUuaDdfd' } ]
+ */
+type IpfsPinAddResult = { hash: string }[];
 
 /**
  * Additive behaviour of pinning a visited hash
  * @param hash
  */
-export async function pinIpfsHash(hash: string): Promise<any> {
-  return await fetch(addEndpoint(hash)).then(res => res.text());
+export async function pinIpfsHash(hash: string): Promise<IpfsPinAddResult> {
+  const ipfsHost = params.IPFS_HOST;
+  const protocol = process.env.IPFS_PROTOCOL || "http";
+  const ipfs = ipfsAPI(ipfsHost, "5001", { protocol });
+
+  return await promisify(ipfs.pin.add)(hash);
 }
