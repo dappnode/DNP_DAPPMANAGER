a3fd494912f3870d0fc15cbc97e14e1d946a9df8
dapplion <guillem.lion.benosa@gmail.com>
2020-05-03 21:24:56 +0200

    Call deviceAdminToggle should update devices dynamically

--- build/src/src/calls/device/deviceAdminToggle.ts
+++ build/src/src/calls/device/deviceAdminToggle.ts
@@ -1,9 +1,11 @@
 import { vpnWampCall } from "../../api";
+import * as eventBus from "../../eventBus";
 
 /**
  * Gives/removes admin rights to the provided device id.
  * @param id Device id name
  */
 export async function deviceAdminToggle({ id }: { id: string }): Promise<void> {
-  return await vpnWampCall<void>("toggleAdmin", { id });
+  await vpnWampCall<void>("toggleAdmin", { id });
+  eventBus.requestDevices.emit();
 }
