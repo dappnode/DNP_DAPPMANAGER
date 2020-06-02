74206e3d28f16f7b536d64daae0abbc1454e54ca
dapplion <guillem.lion.benosa@gmail.com>
2020-06-02 17:06:02 +0200

    Download user action logs from memory instead of disk

--- build/src/src/httpApi/routes/downloadUserActionLogs.ts
+++ build/src/src/httpApi/routes/downloadUserActionLogs.ts
@@ -1,13 +1,16 @@
 import express from "express";
-import params from "../../params";
-import Logs from "../../logs";
-const logs = Logs(module);
+import * as logUserAction from "../../logUserAction";
 
 /**
  * Endpoint to download user action logs.
  */
 export const downloadUserActionLogs: express.Handler = async (req, res) => {
-  return res.download(params.USER_ACTION_LOGS_DB_PATH, errHttp => {
-    logs.info(`Error downloading user action logs: ${errHttp.message}`);
-  });
+  const logs = logUserAction.get();
+
+  const filename = `dappnode-user-action-logs_${new Date().toISOString()}.json`;
+  const mimetype = "application/json";
+  res.setHeader("Content-disposition", "attachment; filename=" + filename);
+  res.setHeader("Content-type", mimetype);
+
+  res.status(200).send(JSON.stringify(logs, null, 2));
 };
