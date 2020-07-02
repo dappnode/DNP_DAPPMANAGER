a30597df817392840d298c1bfa550d33f313e2a8
dapplion <guillem.lion.benosa@gmail.com>
2020-07-02 15:18:54 +0200

    Remove call to IPFS API without proper error handling

--- packages/dappmanager/src/modules/ipfs/ipfsSetup.ts
+++ packages/dappmanager/src/modules/ipfs/ipfsSetup.ts
@@ -1,6 +1,5 @@
 const ipfsClient = require("ipfs-http-client");
 import params from "../../params";
-import { logs } from "../../logs";
 
 export const timeoutMs = 30 * 1000;
 /**
@@ -13,14 +12,6 @@ export const TimeoutErrorKy = class TimeoutError extends Error {
   }
 };
 
-interface IpfsHttpApiVersionReturn {
-  Commit: string;
-  Golang: string;
-  Repo: string;
-  System: string;
-  Version: string;
-}
-
 /**
  * IPFS client setup.
  *
@@ -31,23 +22,5 @@ const IPFS_HOST = params.IPFS_HOST;
 const ipfs = process.env.TEST
   ? {}
   : ipfsClient(IPFS_HOST, { timeout: timeoutMs });
-
-/**
- * Prevents web3 from executing to unit-testing.
- * It can result in infinite non-ending tests
- *
- * verify on the background, don't stop execution
- */
-if (!process.env.TEST) {
-  logs.info(`Attempting IPFS connection to: ${IPFS_HOST}`);
-  ipfs.id((err: Error, identity: { id: string }) => {
-    if (err)
-      ipfs.version((err2: Error, version: IpfsHttpApiVersionReturn) => {
-        if (err2) logs.error("Error checking IPFS connection", err2);
-        else logs.info("Connected to IPFS", version);
-      });
-    else logs.info("Connected to IPFS", (identity || {}).id);
-  });
-}
 
 export default ipfs;
