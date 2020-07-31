d06f2f4137ec69dff1e9addeaa2bea03367342e9
dapplion <guillem.lion.benosa@gmail.com>
2020-07-31 13:12:00 +0200

    Fix packageGet bug that omits manifest data

--- packages/dappmanager/src/calls/packageGet.ts
+++ packages/dappmanager/src/calls/packageGet.ts
@@ -45,34 +45,42 @@ export async function packageGet({
 
   // Add non-blocking data
   try {
-    const manifest = readManifestIfExists(dnpData);
-    if (manifest && manifest.setupWizard) {
-      // Setup wizard, only include the environment fields
-      dnpData.setupWizard = {
-        ...manifest.setupWizard,
-        fields: manifest.setupWizard.fields.filter(
-          field => field.target && field.target.type === "environment"
-        )
-      };
-
-      // Getting started
-      dnpData.gettingStarted = manifest.gettingStarted;
-      dnpData.gettingStartedShow = Boolean(
-        db.packageGettingStartedShow.get(dnpData.name)
-      );
-
-      // Backup
-      dnpData.backup = manifest.backup;
-
+    const manifest = readManifestIfExists(dnp);
+    if (manifest) {
       // Append manifest for general info
       dnpData.manifest = omit(manifest, [
         "setupWizard",
         "gettingStarted",
         "backup"
       ]);
+
+      // Getting started
+      if (manifest.gettingStarted) {
+        dnpData.gettingStarted = manifest.gettingStarted;
+        dnpData.gettingStartedShow = Boolean(
+          db.packageGettingStartedShow.get(dnp.name)
+        );
+      }
+
+      // Backup
+      if (manifest.backup) {
+        dnpData.backup = manifest.backup;
+      }
+
+      // Setup wizard, only include the environment fields
+      if (manifest.setupWizard) {
+        dnpData.setupWizard = {
+          ...manifest.setupWizard,
+          fields: manifest.setupWizard.fields.filter(
+            field => field.target && field.target.type === "environment"
+          )
+        };
+      }
+    } else {
+      logs.debug(`No manifest found for ${dnp.name} core = ${dnp.isCore}`);
     }
   } catch (e) {
-    logs.warn(`Error getting manifest for ${dnpData.name}`, e);
+    logs.warn(`Error getting manifest for ${dnp.name}`, e);
   }
 
   // User settings
@@ -80,12 +88,12 @@ export async function packageGet({
     // Why not fetch the ENVs from a container inspect > config ??
     // ENVs that are not declared in the compose will show up (i.e. PATH)
     // So it's easier and cleaner to just parse the docker-compose.yml
-    const compose = new ComposeFileEditor(dnpData.name, dnpData.isCore);
+    const compose = new ComposeFileEditor(dnp.name, dnp.isCore);
     dnpData.userSettings = {
       environment: compose.service().getEnvs()
     };
   } catch (e) {
-    logs.warn(`Error getting user settings for ${dnpData.name}`, e);
+    logs.warn(`Error getting user settings for ${dnp.name}`, e);
   }
 
   return dnpData;
