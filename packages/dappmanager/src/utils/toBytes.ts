587e696768d720701e1720ae6222381b0b89cab5
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2020-11-18 08:49:46 +0000

    Update toBytes.ts

--- packages/dappmanager/src/utils/toBytes.ts
+++ packages/dappmanager/src/utils/toBytes.ts
@@ -1,16 +1,13 @@
 /**
  * Function reuturns bytes. Accepts mb, kb and gb
- * @param param0
  */
 export function toBytes(amount: number, unit: "kb" | "mb" | "gb"): number {
-  const constant = 1024;
-
   switch (unit) {
     case "kb":
-      return Math.round(amount * Math.pow(constant, 1));
+      return Math.round(amount * Math.pow(1024, 1));
     case "mb":
-      return Math.round(amount * Math.pow(constant, 2));
+      return Math.round(amount * Math.pow(1024, 2));
     case "gb":
-      return Math.round(amount * Math.pow(constant, 3));
+      return Math.round(amount * Math.pow(1024, 3));
   }
 }
