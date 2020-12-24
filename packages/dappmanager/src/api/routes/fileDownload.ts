c7742d134865c35b848645da3302c5efd1aaea99
dapplion <guillem.lion.benosa@gmail.com>
2020-12-24 20:13:06 +0100

    Check that file exists before calling getArchive

--- packages/dappmanager/src/api/routes/fileDownload.ts
+++ packages/dappmanager/src/api/routes/fileDownload.ts
@@ -1,4 +1,5 @@
 import { wrapHandlerHtml } from "../utils";
+import { dockerInfoArchive } from "../../modules/docker";
 import {
   dockerGetFileOrDirBasedOnExtension,
   dockerGetPathType
@@ -16,6 +17,16 @@ export const fileDownload = wrapHandlerHtml<Params>(async (req, res) => {
   const filepath = req.query.path as string;
 
   const filePathAbsolute = filepath;
+
+  // Display a nice error if path does not exist,
+  // since dockerGetFileOrDirBasedOnExtension can be very obscure
+  try {
+    await dockerInfoArchive(containerNameOrId, filepath);
+  } catch (e) {
+    if (e.message.includes("404"))
+      e.message = `No file found at:\ncontainer: ${containerNameOrId}\npath: ${filepath}\n\n${e.message}`;
+    throw e;
+  }
 
   const filetype = await dockerGetPathType(containerNameOrId, filepath);
   const isSingleFile = filetype === "file";
