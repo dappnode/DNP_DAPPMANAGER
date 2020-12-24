d5679a9ba9a244c8629a5e023f5a30150378ba65
dapplion <guillem.lion.benosa@gmail.com>
2020-12-24 18:54:07 +0100

    Test copyFileFrom through API

--- packages/dappmanager/test/integration/fileTransfer.test.int.ts
+++ packages/dappmanager/test/integration/fileTransfer.test.int.ts
@@ -1,8 +1,14 @@
 import { expect } from "chai";
 import { Writable } from "stream";
+import http from "http";
+import express from "express";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
 import { docker } from "../../src/modules/docker/api/docker";
 import { dockerGetArchiveSingleFile } from "../../src/modules/docker/api/getArchive";
 import { dockerPutArchiveSingleFile } from "../../src/modules/docker/api/putArchive";
+import { fileDownload } from "../../src/api/routes/fileDownload";
+import { URL } from "url";
 
 describe("file transfer - docker archive put, get", function() {
   const containerName = "DAppNodeTest-file-transfer";
@@ -44,28 +50,95 @@ describe("file transfer - docker archive put, get", function() {
     await container.start();
   });
 
-  it("Should put and get a file", async () => {
-    await dockerPutArchiveSingleFile(
-      containerName,
-      filePath,
-      Buffer.from(fileContent)
-    );
+  describe("through docker", () => {
+    it("Should put and get a file", async () => {
+      await dockerPutArchiveSingleFile(
+        containerName,
+        filePath,
+        Buffer.from(fileContent)
+      );
 
-    const returnedFileBufferArr: Buffer[] = [];
-    const fileContentSink = new Writable({
-      write: (chunk, encoding, cb): void => {
-        returnedFileBufferArr.push(chunk);
-        cb();
+      const returnedFileBufferArr: Buffer[] = [];
+      const fileContentSink = new Writable({
+        write: (chunk, encoding, cb): void => {
+          returnedFileBufferArr.push(chunk);
+          cb();
+        }
+      });
+
+      await dockerGetArchiveSingleFile(
+        containerName,
+        filePath,
+        fileContentSink
+      );
+
+      const returnedFileBuffer = Buffer.concat(returnedFileBufferArr);
+      const returnedFileContent = returnedFileBuffer.toString("utf8");
+      expect(returnedFileContent).to.equal(
+        fileContent,
+        "returned file does not match put file"
+      );
+    });
+  });
+
+  describe("through API", () => {
+    const afterCallbacks: (() => void)[] = [];
+    afterEach(() => {
+      while (afterCallbacks.length > 0) {
+        const callback = afterCallbacks.pop();
+        if (callback) callback();
       }
     });
 
-    await dockerGetArchiveSingleFile(containerName, filePath, fileContentSink);
+    beforeEach("Add file to container", async () => {
+      await dockerPutArchiveSingleFile(
+        containerName,
+        filePath,
+        Buffer.from(fileContent)
+      );
+    });
 
-    const returnedFileBuffer = Buffer.concat(returnedFileBufferArr);
-    const returnedFileContent = returnedFileBuffer.toString("utf8");
-    expect(returnedFileContent).to.equal(
-      fileContent,
-      "returned file does not match put file"
-    );
+    it("Sould put and get a file from the API directly", async () => {
+      const app = express();
+      app.use(bodyParser.json());
+      app.use(bodyParser.text());
+      app.use(bodyParser.urlencoded({ extended: true }));
+
+      const port = 8965;
+      app.get<{ containerName: string }>(
+        "/file-download/:containerName",
+        fileDownload
+      );
+
+      const server = new http.Server(app);
+
+      await new Promise<void>(resolve => {
+        server.listen(port, () => {
+          resolve();
+        });
+      });
+
+      afterCallbacks.push(() => server.close());
+
+      // ==========
+      // Do request
+      // ==========
+
+      const url = new URL("http://localhost");
+      url.port = String(port);
+      url.pathname = `file-download/${containerName}`;
+      url.searchParams.set("path", filePath);
+
+      const res = await fetch(url.toString());
+      const resText = await res.text();
+      if (!res.ok) {
+        throw Error(`${res.statusText}\n${resText}`);
+      }
+
+      expect(resText).to.equal(
+        fileContent,
+        "returned file does not match put file"
+      );
+    });
   });
 });
