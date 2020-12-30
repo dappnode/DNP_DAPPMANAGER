5b25f7f8679639a5e3e319b7fd9a7b73c35d91af
dapplion <guillem.lion.benosa@gmail.com>
2020-12-30 12:34:11 +0100

    Simplify typing mock calls

--- packages/admin-ui/src/__mock-backend__/index.ts
+++ packages/admin-ui/src/__mock-backend__/index.ts
@@ -2,24 +2,164 @@ import { Routes } from "common";
 
 import { autoUpdate } from "./autoUpdate";
 import { devices } from "./devices";
+import { fetchPkgsData } from "./fetchPkgsData";
 import { notifications } from "./notifications";
-import * as packages from "./packages";
+import { packages } from "./packages";
 import { password } from "./password";
-import * as pending from "./pending";
 import { ssh } from "./ssh";
 import { telegram } from "./telegram";
 import { userActionLogs } from "./userActionLogs";
 import { volumes } from "./volumes";
 
-export const calls: Routes = {
+const namedSpacedCalls = {
   ...autoUpdate,
   ...devices,
+  ...fetchPkgsData,
   ...notifications,
   ...packages,
   ...password,
-  ...pending,
   ...ssh,
   ...telegram,
   ...userActionLogs,
   ...volumes
+};
+
+export const otherCalls: Omit<Routes, keyof typeof namedSpacedCalls> = {
+  backupGet: async () =>
+    "64020f6e8d2d02aa2324dab9cd68a8ccb186e192232814f79f35d4c2fbf2d1cc",
+  backupRestore: async () => {},
+  chainDataGet: async () => [
+    {
+      dnpName: "geth.dnp.dappnode.eth",
+      syncing: true,
+      error: false,
+      message: [
+        "Blocks synced: 543000 / 654000",
+        "States pulled: 25314123 / 154762142"
+      ].join("\n\n"),
+      help: "http://geth.io"
+    },
+    {
+      dnpName: "rinkeby.dnp.dappnode.eth",
+      syncing: true,
+      error: false,
+      message: "Blocks synced: 543000 / 654000",
+      progress: 0.83027522935
+    }
+  ],
+  changeIpfsTimeout: async () => {},
+  cleanCache: async () => {},
+  copyFileTo: async () => {},
+  diagnose: async () => [],
+  domainAliasSet: async () => {},
+  ethClientFallbackSet: async () => {},
+  ethClientTargetSet: async () => {},
+  ipPublicGet: async () => ({
+    publicIp: "85.84.83.82"
+  }),
+
+  statsCpuGet: async () => ({
+    usedPercentage: 88
+  }),
+  statsMemoryGet: async () => ({
+    total: 8093155328,
+    used: 6535839744,
+    free: 179961856,
+    usedPercentage: 34
+  }),
+  statsDiskGet: async () => ({
+    total: 241440808960,
+    used: 189458415616,
+    free: 39646527488,
+    usedPercentage: 83
+  }),
+
+  mountpointsGet: async () => [
+    {
+      mountpoint: "",
+      use: "87%",
+      used: 43e9,
+      total: 0,
+      free: 121e9,
+      vendor: "Host",
+      model: "(default)"
+    },
+    {
+      mountpoint: "/data",
+      use: "68%",
+      used: 380e9,
+      total: 500e9,
+      free: 141e9,
+      vendor: "ATA",
+      model: "CT500MX500SSD4"
+    },
+    {
+      mountpoint: "/media/usb0",
+      use: "89%",
+      used: 198642520,
+      total: 235782040,
+      free: 25092776,
+      vendor: "SanDisk",
+      model: "Ultra_USB_3.0"
+    },
+    {
+      mountpoint: "/media/usb1",
+      use: "100%",
+      used: 4e9,
+      total: 16e9,
+      free: 7.1e9,
+      vendor: "SanDisk",
+      model: "Ultra_USB_3.0"
+    }
+  ],
+
+  newFeatureStatusSet: async () => {},
+  poweroffHost: async () => {},
+  rebootHost: async () => {},
+  seedPhraseSet: async () => {},
+  setStaticIp: async () => {},
+
+  systemInfoGet: async () => ({
+    versionData: {
+      branch: "test",
+      commit: "a5a5a5a5",
+      version: "0.2.0"
+    },
+    versionDataVpn: {
+      branch: "test",
+      commit: "a8a8a8a8",
+      version: "0.2.1"
+    },
+    ip: "85.84.83.82",
+    name: "My-DAppNode",
+    staticIp: "", // "85.84.83.82",
+    domain: "1234acbd.dyndns.io",
+    upnpAvailable: true,
+    noNatLoopback: false,
+    alertToOpenPorts: false,
+    internalIp: "192.168.0.1",
+    publicIp: "85.84.83.82",
+    dappmanagerNaclPublicKey: "cYo1NA7/+PQ22PeqrRNGhs1B84SY/fuomNtURj5SUmQ=",
+    identityAddress: "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B",
+    ethClientTarget: "openethereum",
+    ethClientFallback: "off",
+    ethClientStatus: {
+      ok: false,
+      code: "STATE_CALL_ERROR",
+      error: { message: "Some Error", stack: "Some Error\nline 56 file.ts" }
+    },
+    ethProvider: "http://geth.dappnode:8545",
+    fullnodeDomainTarget: "geth.dnp.dappnode.eth",
+    newFeatureIds: [
+      // "repository",
+      // "repository-fallback",
+      // "system-auto-updates",
+      // "change-host-password"
+    ]
+  })
+};
+
+export const calls: Routes = {
+  ...namedSpacedCalls,
+  ...otherCalls
 };
--- packages/admin-ui/src/__mock-backend__/packages.ts
+++ packages/admin-ui/src/__mock-backend__/packages.ts
@@ -1,20 +1,11 @@
-import {
-  PackageEnvs,
-  UserSettingsAllDnps,
-  CoreUpdateData,
-  DirectoryItem,
-  RequestedDnp,
-  PortMapping,
-  InstalledPackageData,
-  InstalledPackageDetailData
-} from "../common";
-import { dnpInstalled, directory, dnpRequests } from "./data";
+import { InstalledPackageDetailData, Routes } from "../common";
+import { dnpInstalled } from "./data";
 import { sampleContainer, sampleDnp } from "./data/sample";
 import { pause } from "./utils/pause";
 
 const pkgRestartMs = 2000;
 
-const packages = new Map<string, InstalledPackageDetailData>(
+const packagesState = new Map<string, InstalledPackageDetailData>(
   dnpInstalled.map(dnp => [dnp.dnpName, dnp])
 );
 
@@ -22,292 +13,144 @@ function update(
   dnpName: string,
   fn: (dnp: InstalledPackageDetailData) => Partial<InstalledPackageDetailData>
 ) {
-  const dnp = packages.get(dnpName);
+  const dnp = packagesState.get(dnpName);
   if (!dnp) throw Error(`dnpName ${dnpName} not found`);
-  packages.set(dnpName, { ...dnp, ...fn(dnp) });
+  packagesState.set(dnpName, { ...dnp, ...fn(dnp) });
 }
 
-/**
- * Return formated core update data
- */
-export async function fetchCoreUpdateData(kwarg: {
-  version?: string;
-}): Promise<CoreUpdateData> {
-  return {
-    available: true,
-    type: "patch",
-    packages: [
-      {
-        name: "admin.dnp.dappnode.eth",
-        from: "0.2.0",
-        to: "0.2.6",
-        warningOnInstall: "Warning on **install**"
-      }
-    ],
-    changelog:
-      "Major improvements to the 0.2 version https://github.com/dappnode/DAppNode/wiki/DAppNode-Migration-guide-to-OpenVPN",
-    updateAlerts: [
-      {
-        from: "0.2.0",
-        to: "0.2.0",
-        message: "Conditional update alert: **Markdown**"
-      }
-    ],
-    versionId: ""
-  };
-}
+export const packages: Pick<
+  Routes,
+  | "packageGet"
+  | "packageGettingStartedToggle"
+  | "packageInstall"
+  | "packageLog"
+  | "packageRemove"
+  | "packageRestart"
+  | "packageRestartVolumes"
+  | "packageSetEnvironment"
+  | "packageSetPortMappings"
+  | "packageStartStop"
+  | "packagesGet"
+> = {
+  packageGet: async ({ dnpName }) => {
+    const dnp = packagesState.get(dnpName);
+    if (!dnp) throw Error(`dnpName ${dnpName} not found`);
+    return dnp;
+  },
 
-/**
- * Fetch directory summary
- */
-export async function fetchDirectory(): Promise<DirectoryItem[]> {
-  return directory;
-}
+  packageGettingStartedToggle: async ({ dnpName, show }) => {
+    update(dnpName, () => ({ gettingStartedShow: show }));
+  },
 
-/**
- * Fetch extended info about a new DNP
- */
-export async function fetchDnpRequest({
-  id
-}: {
-  id: string;
-}): Promise<RequestedDnp> {
-  const dnpRequest = dnpRequests[id];
-  if (!dnpRequest) throw Error(`No dnp request found for ${id}`);
-  return dnpRequest;
-}
+  packageInstall: async ({ name, version, userSettings }) => {
+    await pause(pkgRestartMs);
+    packagesState.set(name, {
+      ...sampleDnp,
+      dnpName: name,
+      version: version || "0.1.0",
+      avatarUrl: "http://ipfs.dappnode:8080/ipfs/Qm",
+      origin: undefined,
+      gettingStarted: `Welcome to the package **${name}**`,
+      gettingStartedShow: true,
+      userSettings: (userSettings || {})[name],
+      containers: [
+        {
+          ...sampleContainer,
+          containerName: `DAppNodePackage-${name}`,
+          containerId: `00000000000${name}`,
+          serviceName: name,
+          instanceName: "",
+          created: 1500000000,
+          image: `${name}:${version}`,
+          state: "running",
+          running: true,
+          ports: [],
+          volumes: []
+        }
+      ]
+    });
+  },
 
-/**
- * Installs a DAppNode Package.
- * Resolves dependencies, downloads release assets, loads the images to docker,
- * sets userSettings and starts the docker container for each package.
- *
- * The logId is the requested id. It is used for the UI to track the progress
- * of the installation in real time and prevent double installs
- *
- * Options
- * - BYPASS_RESOLVER {bool}: Skips dappGet to only fetche first level dependencies
- * - BYPASS_CORE_RESTRICTION {bool}: Allows unverified core DNPs (from IPFS)
- */
-export async function packageInstall({
-  name,
-  version,
-  userSettings
-}: {
-  name: string;
-  version?: string;
-  userSettings?: UserSettingsAllDnps;
-  options?: {
-    BYPASS_RESOLVER?: boolean;
-    BYPASS_CORE_RESTRICTION?: boolean;
-  };
-}): Promise<void> {
-  await pause(pkgRestartMs);
-  packages.set(name, {
-    ...sampleDnp,
-    dnpName: name,
-    version: version || "0.1.0",
-    avatarUrl: "http://ipfs.dappnode:8080/ipfs/Qm",
-    origin: undefined,
-    gettingStarted: `Welcome to the package **${name}**`,
-    gettingStartedShow: true,
-    userSettings: (userSettings || {})[name],
-    containers: [
-      {
-        ...sampleContainer,
-        containerName: `DAppNodePackage-${name}`,
-        containerId: `00000000000${name}`,
-        serviceName: name,
-        instanceName: "",
-        created: 1500000000,
-        image: `${name}:${version}`,
-        state: "running",
-        running: true,
-        ports: [],
-        volumes: []
-      }
-    ]
-  });
-}
+  packageLog: async ({ containerName }) => `INFO: ${containerName} logs`,
 
-/**
- * Get package detail information
- */
-export async function packageGet({
-  dnpName
-}: {
-  dnpName: string;
-}): Promise<InstalledPackageDetailData> {
-  const dnp = packages.get(dnpName);
-  if (!dnp) throw Error(`dnpName ${dnpName} not found`);
-  return dnp;
-}
+  packageRemove: async ({ dnpName }) => {
+    await pause(pkgRestartMs);
+    packagesState.delete(dnpName);
+  },
 
-/**
- * Returns the list of current containers associated to packages
- */
-export async function packagesGet(): Promise<InstalledPackageData[]> {
-  return Array.from(packages.values());
-}
-
-/**
- * Toggles the visibility of a getting started block
- */
-export async function packageGettingStartedToggle({
-  dnpName,
-  show
-}: {
-  dnpName: string;
-  show: boolean;
-}): Promise<void> {
-  update(dnpName, () => ({ gettingStartedShow: show }));
-}
-
-/**
- * Returns the logs of the docker container of a package
- * Log options
- * - timestamps: Show timestamps
- * - tail: Number of lines to return from bottom: 200
- * @returns String with escape codes
- */
-export async function packageLog({
-  containerName
-}: {
-  containerName: string;
-  options?: { timestamps?: boolean; tail?: number };
-}): Promise<string> {
-  return `INFO: ${containerName} logs`;
-}
-
-/**
- * Remove a package and its data
- * deleteVolumes: flag to also clear permanent package data
- */
-export async function packageRemove({
-  dnpName
-}: {
-  dnpName: string;
-  deleteVolumes?: boolean;
-  timeout?: number;
-}): Promise<void> {
-  await pause(pkgRestartMs);
-  packages.delete(dnpName);
-}
-
-/**
- * Calls docker rm and docker up on a package
- */
-export async function packageRestart({
-  dnpName
-}: {
-  dnpName: string;
-}): Promise<void> {
-  await pause(pkgRestartMs);
-  update(dnpName, dnp => ({
-    containers: dnp.containers.map(container => ({
-      ...container,
-      state: "exited"
-    }))
-  }));
-
-  await pause(pkgRestartMs);
-  update(dnpName, dnp => ({
-    containers: dnp.containers.map(container => ({
-      ...container,
-      state: "running"
-    }))
-  }));
-}
-
-/**
- * Removes a package volumes. The re-ups the package
- */
-export async function packageRestartVolumes({
-  dnpName
-}: {
-  dnpName: string;
-  volumeId?: string;
-}): Promise<void> {
-  throw Error(`Not implemented: ${dnpName}`);
-}
-
-/**
- * Updates the .env file of a package. If requested, also re-ups it
- */
-export async function packageSetEnvironment({
-  dnpName,
-  environmentByService
-}: {
-  dnpName: string;
-  environmentByService: { [serviceName: string]: PackageEnvs };
-}): Promise<void> {
-  await pause(pkgRestartMs);
-
-  for (const [serviceName, environment] of Object.entries(
-    environmentByService
-  )) {
+  packageRestart: async ({ dnpName }) => {
+    await pause(pkgRestartMs);
     update(dnpName, dnp => ({
-      userSettings: {
-        ...dnp.userSettings,
-        environment: {
-          [serviceName]: {
-            ...((dnp.userSettings?.environment || {})[serviceName] || {}),
-            ...environment
+      containers: dnp.containers.map(container => ({
+        ...container,
+        state: "exited"
+      }))
+    }));
+
+    await pause(pkgRestartMs);
+    update(dnpName, dnp => ({
+      containers: dnp.containers.map(container => ({
+        ...container,
+        state: "running"
+      }))
+    }));
+  },
+
+  packageRestartVolumes: async ({ dnpName }) => {
+    throw Error(`Not implemented: ${dnpName}`);
+  },
+
+  packageSetEnvironment: async ({ dnpName, environmentByService }) => {
+    await pause(pkgRestartMs);
+    for (const [serviceName, environment] of Object.entries(
+      environmentByService
+    )) {
+      update(dnpName, dnp => ({
+        userSettings: {
+          ...dnp.userSettings,
+          environment: {
+            [serviceName]: {
+              ...((dnp.userSettings?.environment || {})[serviceName] || {}),
+              ...environment
+            }
           }
         }
-      }
-    }));
-  }
-}
+      }));
+    }
+  },
 
-/**
- * Updates the .env file of a package. If requested, also re-ups it
- */
-export async function packageSetPortMappings({
-  dnpName,
-  portMappingsByService
-}: {
-  dnpName: string;
-  portMappingsByService: { [serviceName: string]: PortMapping[] };
-  options?: { merge: boolean };
-}): Promise<void> {
-  await pause(pkgRestartMs);
-  for (const [serviceName, portMappings] of Object.entries(
-    portMappingsByService
-  )) {
-    update(dnpName, dnp => ({
-      containers: dnp.containers.map(container =>
-        container.serviceName === serviceName
-          ? { ...container, ports: portMappings }
+  packageSetPortMappings: async ({ dnpName, portMappingsByService }) => {
+    await pause(pkgRestartMs);
+    for (const [serviceName, portMappings] of Object.entries(
+      portMappingsByService
+    )) {
+      update(dnpName, dnp => ({
+        containers: dnp.containers.map(container =>
+          container.serviceName === serviceName
+            ? { ...container, ports: portMappings }
+            : container
+        )
+      }));
+    }
+  },
+
+  packageStartStop: async ({ dnpName, serviceNames }) => {
+    await pause(pkgRestartMs);
+    const dnp = packagesState.get(dnpName);
+    if (!dnp) throw Error(`dnpName ${dnpName} not found`);
+
+    update(dnp.dnpName, d => ({
+      containers: d.containers.map(container =>
+        !serviceNames || serviceNames?.includes(container.serviceName)
+          ? {
+              ...container,
+              running: !container.running,
+              state: container.running ? "exited" : "running"
+            }
           : container
       )
     }));
-  }
-}
+  },
 
-/**
- * Stops or starts after fetching its status
- */
-export async function packageStartStop({
-  dnpName,
-  serviceNames
-}: {
-  dnpName: string;
-  serviceNames?: string[];
-}): Promise<void> {
-  await pause(pkgRestartMs);
-  const dnp = packages.get(dnpName);
-  if (!dnp) throw Error(`dnpName ${dnpName} not found`);
-
-  update(dnp.dnpName, d => ({
-    containers: d.containers.map(container =>
-      !serviceNames || serviceNames?.includes(container.serviceName)
-        ? {
-            ...container,
-            running: !container.running,
-            state: container.running ? "exited" : "running"
-          }
-        : container
-    )
-  }));
-}
+  packagesGet: async () => Array.from(packagesState.values())
+};
