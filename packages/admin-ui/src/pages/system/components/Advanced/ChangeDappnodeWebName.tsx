2cb5c7df509b6615f4a8fa12f2504b67e9f9275a
Tropicar <zerozept@protonmail.com>
2021-02-19 16:23:05 +0100

    Change to make reactive this change on the UI

--- packages/admin-ui/src/pages/system/components/Advanced/ChangeDappnodeWebName.tsx
+++ packages/admin-ui/src/pages/system/components/Advanced/ChangeDappnodeWebName.tsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from "react";
 import Button from "components/Button";
 import { api } from "api";
 import { InputForm } from "components/InputForm";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { getDappnodeName } from "services/dappnodeStatus/selectors";
 import { withToastNoThrow } from "components/toast/Toast";
-
+import { fetchSystemInfo } from "services/dappnodeStatus/actions";
 export function ChangeDappnodeWebName() {
   const dappnodeWebName = useSelector(getDappnodeName);
   const [input, setInput] = useState(dappnodeWebName);
-
+  const dispatch = useDispatch();
   useEffect(() => {
     setInput(dappnodeWebName);
   }, [dappnodeWebName]);
 
   function onChangeDappNodeWebName() {
     withToastNoThrow(() => api.dappnodeWebNameSet(input), {
-      message: "Setting dappnode name ...",
-      onSuccess: "Regresh the page to see the changes"
+      message: "Setting DAppNode name ...",
+      onSuccess: "DAppNode name changed correctly"
+    }).then(() => {
+      dispatch(fetchSystemInfo());
     });
   }
 
