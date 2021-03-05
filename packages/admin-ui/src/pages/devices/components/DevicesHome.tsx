15361a4a22c91e71d55b97633bb460bdc222d049
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-03-05 15:11:49 +0100

    Update DevicesHome.tsx

--- packages/admin-ui/src/pages/devices/components/DevicesHome.tsx
+++ packages/admin-ui/src/pages/devices/components/DevicesHome.tsx
@@ -13,7 +13,6 @@ import Card from "components/Card";
 import Switch from "components/Switch";
 import Button from "components/Button";
 import { renderResponse } from "components/SwrRender";
-import SubTitle from "components/SubTitle";
 // Icons
 import { MdDelete, MdRefresh } from "react-icons/md";
 import { MAIN_ADMIN_NAME } from "params";
@@ -78,11 +77,6 @@ export default function DevicesHome() {
   return (
     <>
       <Title title={title} />
-      <SubTitle>What can you do in this VPN section?</SubTitle>
-      <p>
-      On this page, you can set up your VPN for every device you have. You should set up every device you
-      will access the UI of your dappnode, you should generate VPN credential for everyone.
-      </p>
       <Input
         placeholder="Device's unique name"
         value={input}
