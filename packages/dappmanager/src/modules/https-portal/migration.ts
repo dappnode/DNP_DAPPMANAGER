4e6814d36dd07c3be5a4d7c96bbd4e884d2a84de
pablo <mendez4a@gmail.com>
2021-03-29 12:17:56 +0200

    Improved function migrae

--- packages/dappmanager/src/modules/https-portal/migration.ts
+++ packages/dappmanager/src/modules/https-portal/migration.ts
@@ -8,10 +8,12 @@ import {
   dockerNetworkDisconnect
 } from "../docker";
 import { listContainers } from "../docker/list";
-import { addNetworkAliasCompose, migrateCoreNetworkInCompose } from "./utils";
 import Dockerode from "dockerode";
 import shell from "../../utils/shell";
 import * as getPath from "../../utils/getPath";
+import { ComposeFileEditor } from "../compose/editor";
+import { PackageContainer } from "../../types";
+import { parseServiceNetworks } from "../compose/networks";
 
 /** Alias for code succinctness */
 const dncoreNetworkName = params.DNP_PRIVATE_NETWORK_NAME;
@@ -28,14 +30,16 @@ export async function addAliasToRunningContainersMigration(): Promise<void> {
     const alias = getPrivateNetworkAlias(container);
 
     try {
-      migrateCoreNetworkInCompose(container);
+      // Info from docker inspect and compose file might be not-syncrhnonyzed
+      // So this function must be before the check hasAlias()
+      migrateCoreNetworkAndAliasInCompose(container, alias);
+
       const currentEndpointConfig = await getEndpointConfig(containerName);
       if (hasAlias(currentEndpointConfig, alias)) continue;
       const endpointConfig: Partial<Dockerode.NetworkInfo> = {
         ...currentEndpointConfig,
         Aliases: [...(currentEndpointConfig?.Aliases || []), alias]
       };
-      addNetworkAliasCompose(container, dncoreNetworkName, [alias]);
 
       // Wifi and VPN containers needs a refresh connect due to its own network configuration
       if (
@@ -80,4 +84,57 @@ async function getEndpointConfig(
 ): Promise<Dockerode.NetworkInfo | null> {
   const inspectInfo = await dockerContainerInspect(containerName);
   return inspectInfo.NetworkSettings.Networks[dncoreNetworkName] ?? null;
+}
+
+/**
+ * Get compose file network and compose network settings from dncore_network
+ * And rewrites the compose with the core network edited
+ */
+export function migrateCoreNetworkAndAliasInCompose(
+  container: PackageContainer,
+  alias: string
+): void {
+  const compose = new ComposeFileEditor(container.dnpName, container.isCore);
+
+  // 0. Check if network migration has been done
+
+  // 1. Get compose network settings: not needed since will be declared as external
+  // const networkConfig = compose.getComposeNetwork(dncoreNetworkNameFromCore);
+
+  // 2. Get compose service network settings
+  const composeService = compose.services()[container.serviceName];
+
+  const serviceNetworks = parseServiceNetworks(
+    composeService.get().networks || {}
+  );
+
+  const serviceNetwork =
+    serviceNetworks?.[params.DNP_PRIVATE_NETWORK_NAME_FROM_CORE];
+
+  // 3. Check if migration was done
+  const composeNetwork = compose.getComposeNetwork(
+    params.DNP_PRIVATE_NETWORK_NAME
+  );
+  if (
+    composeNetwork?.name === params.DNP_PRIVATE_NETWORK_NAME &&
+    composeNetwork?.external &&
+    serviceNetwork.aliases?.includes(alias)
+  )
+    return;
+
+  // 4. Ensure compose file version 3.5
+  compose.compose = {
+    ...compose.compose,
+    version: params.MINIMUM_COMPOSE_VERSION
+  };
+
+  // 5. core network migration: network => dncore_network
+  composeService.removeNetwork(params.DNP_PRIVATE_NETWORK_NAME_FROM_CORE);
+  composeService.addNetwork(
+    params.DNP_PRIVATE_NETWORK_NAME,
+    { ...serviceNetwork, aliases: [alias] },
+    { external: true, name: params.DNP_PRIVATE_NETWORK_NAME } //...networkConfig,
+  );
+
+  compose.write();
 }
