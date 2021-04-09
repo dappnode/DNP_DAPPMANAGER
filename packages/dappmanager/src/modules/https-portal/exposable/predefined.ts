80dc357be7870ef8102dcd7d969a15756952f8fd
tropicar <zerozept@protonmail.com>
2021-04-09 14:45:46 +0200

    fix goerli services and adding kovan service

--- packages/dappmanager/src/modules/https-portal/exposable/predefined.ts
+++ packages/dappmanager/src/modules/https-portal/exposable/predefined.ts
@@ -15,10 +15,20 @@ export const exposablePredefined: {
   "goerli-geth.dnp.dappnode.eth": [
     {
       dnpName: "goerli-geth.dnp.dappnode.eth",
-      serviceName: "geth.dnp.dappnode.eth",
+      serviceName: "goerli-geth.dnp.dappnode.eth",
       port: 8545,
-      name: "Geth JSON RPC",
-      description: "JSON RPC endpoint for Geth mainnet"
+      name: "Goerli Geth JSON RPC",
+      description: "JSON RPC endpoint for Goerli network"
     }
-  ]
+  ],
+  "kovan.dnp.dappnode.eth": [
+    {
+      dnpName: "kovan.dnp.dappnode.eth",
+      serviceName: "kovan.dnp.dappnode.eth",
+      port: 8545,
+      name: "Kovan JSON RPC",
+      description: "JSON RPC endpoint for Kovan network"
+    }
+  ],
 };
+
