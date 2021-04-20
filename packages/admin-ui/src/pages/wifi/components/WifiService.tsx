2b09d9a38cb1f760e9d88fd1c2770e112aec4256
pablo <mendez4a@gmail.com>
2021-04-20 12:13:07 +0200

    Added css and confirm dialoges

--- packages/admin-ui/src/pages/wifi/components/WifiService.tsx
+++ packages/admin-ui/src/pages/wifi/components/WifiService.tsx
@@ -1,40 +1,53 @@
-import { api, useApi } from "api";
-
+// React
+import React, { useEffect } from "react";
+// Own components
 import Card from "components/Card";
 import ErrorView from "components/ErrorView";
 import Loading from "components/Loading";
-import React from "react";
 import Button from "components/Button";
-import Alert from "react-bootstrap/esm/Alert";
-import { WifiReport } from "types";
-import { withToastNoThrow } from "components/toast/Toast";
-import { continueIfCalleDisconnected } from "api/utils";
+import { confirm } from "components/ConfirmDialog";
+import { withToast } from "components/toast/Toast";
+// External components
 import Badge from "react-bootstrap/esm/Badge";
+import Alert from "react-bootstrap/esm/Alert";
+// API
+import { api, useApi } from "api";
+import { continueIfCalleDisconnected } from "api/utils";
+// Types
+import { WifiReport } from "types";
+// css
+import "./wifi.scss";
+import { ContainerState } from "common";
 
 const wifiDnpName = "wifi.dnp.dappnode.eth";
 
 function RenderWifiStatus({ wifiReport }: { wifiReport: WifiReport }) {
   return (
     <>
-      <div className="display inline-block">
+      <div className="wifi-status">
         <h4>
           Wifi status{" "}
           <Badge
             variant={
-              wifiReport.containerState === "running" ? "success" : "warning"
+              wifiReport.containerState === "running"
+                ? "success"
+                : wifiReport.containerState ===
+                  ("restarting" || "paused" || "created")
+                ? "info"
+                : "warning"
             }
           >
             {wifiReport.containerState}
           </Badge>
         </h4>
 
-        <RestartWifi />
+        <ActionsWifi wifiStatus={wifiReport.containerState} />
       </div>
 
       <hr />
 
       <Alert
-        dismissible={true}
+        dismissible
         variant={
           wifiReport.containerState === "running" ? "success" : "warning"
         }
@@ -47,27 +60,73 @@ function RenderWifiStatus({ wifiReport }: { wifiReport: WifiReport }) {
   );
 }
 
-function RestartWifi() {
+function ActionsWifi({ wifiStatus }: { wifiStatus: ContainerState }) {
   async function restartWifi() {
-    withToastNoThrow(
+    await new Promise<void>(resolve => {
+      confirm({
+        title: `Restart wifi service`,
+        text: `Warming, you may loose connection to your dappnode during wifi restart`,
+        label: "Restart",
+        onClick: resolve
+      });
+    });
+
+    withToast(
       continueIfCalleDisconnected(
         () => api.packageRestart({ dnpName: wifiDnpName }),
         wifiDnpName
       )
     );
   }
+
+  async function pauseWifi() {
+    if (wifiStatus === "running")
+      await new Promise<void>(resolve => {
+        confirm({
+          title: `Pause wifi service`,
+          text: `Warming, you may loose connection to your dappnode if stopping this service`,
+          label: "Pause",
+          onClick: resolve
+        });
+      });
+
+    withToast(
+      continueIfCalleDisconnected(
+        () => api.packageStartStop({ dnpName: wifiDnpName }),
+        wifiDnpName
+      )
+    );
+  }
+
   return (
-    <Button
-      variant="dappnode"
-      onClick={() => restartWifi().catch(console.error)}
-    >
-      Restart wifi
-    </Button>
+    <div className="wifi-actions">
+      <Button
+        variant="warning"
+        onClick={() => restartWifi().catch(console.error)}
+      >
+        Restart wifi
+      </Button>
+      <Button
+        variant="warning"
+        onClick={() => pauseWifi().catch(console.error)}
+      >
+        {wifiStatus === "running" ? "Pause" : "Start"}
+      </Button>
+    </div>
   );
 }
 
 export default function WifiService() {
   const wifiReport = useApi.wifiReportGet();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      wifiReport.revalidate();
+    }, 20 * 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [wifiReport]);
 
   return (
     <>
@@ -81,6 +140,14 @@ export default function WifiService() {
             <p>
               Connect to your dappnode through the wifi hostpot exposed by your
               dappnode.
+              <ol>
+                <li>Scan your local network</li>
+                <li>Connect to the wifi hostpot using your credentials</li>
+                <li>
+                  Go to:{" "}
+                  <a href="http://my.dappnode/#/">http://my.dappnode/#/</a>
+                </li>
+              </ol>
             </p>
             <hr />
             <RenderWifiStatus wifiReport={wifiReport.data} />
