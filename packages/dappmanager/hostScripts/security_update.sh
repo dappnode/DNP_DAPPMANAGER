2274526ffebacb59d0534d1ddc302e1f4f2436c8
pablo <mendez4a@gmail.com>
2021-04-27 19:11:26 +0200

    improved script

--- packages/dappmanager/hostScripts/security_update.sh
+++ packages/dappmanager/hostScripts/security_update.sh
@@ -14,87 +14,106 @@ DOCKER_INSTALLATION_REPOSITORIES=false
 #1.PRE-UPDATE#
 ##############
 
-# Prevent needrestart interactive mode
-# If set to a non-empty value the apt-get(8) hook will not run needrestart after installing or updating packages.
-export NEEDRESTART_SUSPEND=true
+function pre_update () {
+  # Prevent needrestart interactive mode and debian frontend during upgrade: https://manpages.debian.org/testing/needrestart/needrestart.1.en.html
+  export NEEDRESTART_SUSPEND=true # If set to a non-empty value the apt-get(8) hook will not run needrestart after installing or updating packages.
+  export DEBIAN_FRONTEND=noninteractive
 
-# Check docker exists
-if ! command -v docker &> /dev/null; then
-  echo "docker not installed"
-  exit 1
-fi
+  # Check docker exists
+  if ! command -v docker &> /dev/null; then
+    echo "docker not installed"
+    exit 1
+  fi
 
-# Check installation method
-if [[ "$DOCKER_PACKAGES_LENGTH" -eq 3 ]]; then
-  DOCKER_INSTALLATION_REPOSITORIES=true
-fi
+  # Check installation method
+  if [[ "$DOCKER_PACKAGES_LENGTH" -eq 3 ]]; then
+    DOCKER_INSTALLATION_REPOSITORIES=true
+  fi
 
-# Check sources.list exist
-if [ ! -f "$FILE_SOURCES_LIST" ]; then
-  echo "$FILE_SOURCES_LIST does not exist"
-  exit 1
-fi
+  # Check sources.list exist
+  if [ ! -f "$FILE_SOURCES_LIST" ]; then
+    echo "$FILE_SOURCES_LIST does not exist"
+    exit 1
+  fi
 
-# Check security.sources.list does not exist
-if [ -f "$FILE_SECURITY_SOURCES_LIST" ]; then
-  echo "$FILE_SECURITY_SOURCES_LIST already exist"
-  exit 1
-fi
+  # Check security.sources.list does not exist
+  if [ -f "$FILE_SECURITY_SOURCES_LIST" ]; then
+    echo "$FILE_SECURITY_SOURCES_LIST already exist"
+    exit 1
+  fi
 
-# Create security.sources.list
-grep security "$FILE_SOURCES_LIST" | tee "$FILE_SECURITY_SOURCES_LIST" || echo "error creating file $FILE_SECURITY_SOURCES_LIST" && exit 1
+  # Create security.sources.list
+  grep security "$FILE_SOURCES_LIST" | tee "$FILE_SECURITY_SOURCES_LIST" || echo "error creating file $FILE_SECURITY_SOURCES_LIST" && exit 1
 
-# Prevent docker packages from been updated if docker installed via repositories
-if [ "$DOCKER_INSTALLATION_REPOSITORIES" = true ] ; then
-  for DOCKER_PACKAGE in "${DOCKER_PACKAGES[@]}"; do
-    echo "${DOCKER_PACKAGE} hold" | sudo dpkg --set-selections
-  done
-fi
+  # Prevent docker packages from been updated if docker installed via repositories
+  if [ "$DOCKER_INSTALLATION_REPOSITORIES" = true ] ; then
+    for DOCKER_PACKAGE in "${DOCKER_PACKAGES[@]}"; do
+      echo "${DOCKER_PACKAGE} hold" | sudo dpkg --set-selections
+    done
+  fi
+}
+
+
 
 ##################
 #2.UPDATE-UPGRADE#
 ##################
 
-apt-get update > /dev/null || echo "error on apt-get update" && exit 1
-apt-get upgrade -y -o "Dir::Etc::SourceList=$FILE_SECURITY_SOURCES_LIST" > /dev/null || echo "error on apt-get upgrade" && exit 1
+function update () {
+  apt-get update -y > /dev/null || echo "error on apt-get update" && exit 1
+  apt-get upgrade -y -o "Dir::Etc::SourceList=$FILE_SECURITY_SOURCES_LIST" > /dev/null || echo "error on apt-get upgrade" && exit 1
+}
 
 ###############
 #3.POSTUPGRADE#
 ###############
 
-# Remove security-sources.list
-rm -rf "$FILE_SECURITY_SOURCES_LIST"
-# Put normal values on repositories
-if [ "$DOCKER_INSTALLATION_REPOSITORIES" = true ] ; then
-  for DOCKER_PACKAGE in "${DOCKER_PACKAGES[@]}"; do
-    echo "${DOCKER_PACKAGE} install" | sudo dpkg --set-selections
-  done
-fi
+function post_upgrade_clean () {
+  # Remove security-sources.list
+  rm -rf "$FILE_SECURITY_SOURCES_LIST"
+  # Put normal values on repositories
+  if [ "$DOCKER_INSTALLATION_REPOSITORIES" = true ] ; then
+    for DOCKER_PACKAGE in "${DOCKER_PACKAGES[@]}"; do
+      echo "${DOCKER_PACKAGE} install" | sudo dpkg --set-selections
+    done
+  fi
+}
 
 ###########
 #4.RESTART#
 ###########
 
-# Security UPDATE: https://www.debian.org/doc/manuals/securing-debian-manual/security-update.en.html
-# - libraries: "Once you have executed a security update you might need to restart some of the system services.
-# If you do not do this, some services might still be vulnerable after a security upgrade"
-# - kernel: "If you are doing a security update which includes the kernel image you need to reboot the system 
-# in order for the security update to be useful. Otherwise, you will still be running the old (and vulnerable) kernel image"
+function post_upgrade_restart () {
+  # Security UPDATE: https://www.debian.org/doc/manuals/securing-debian-manual/security-update.en.html
+  # - libraries: "Once you have executed a security update you might need to restart some of the system services.
+  # If you do not do this, some services might still be vulnerable after a security upgrade"
+  # - kernel: "If you are doing a security update which includes the kernel image you need to reboot the system 
+  # in order for the security update to be useful. Otherwise, you will still be running the old (and vulnerable) kernel image"
 
-# Install needrestart: https://manpages.debian.org/testing/needrestart/needrestart.1.en.html
-if ! command -v needrestart &> /dev/null: then
-  apt-get install -y needrestart > /dev/null || echo "upgraded successfully, error installing needrestart" && exit 1
-fi
+  # Install needrestart: https://manpages.debian.org/testing/needrestart/needrestart.1.en.html
+  if ! command -v sudo needrestart &> /dev/null; then
+    sudo apt-get install -y needrestart > /dev/null || echo "Security updates have been executed successfully, error installing needrestart" && exit 1
+  fi
 
-# Restart required services
-needrestart -r a -q > /dev/null || echo "upgraded succesfully, error restarting services" && exit 1
+  # Restart required services
+  sudo needrestart -r a -q > /dev/null || echo "Security updates have been executed successfully, error restarting services" && exit 1
 
-# Reboot if still needed
-NEEDS_TO_BE_RESTARTED=$(needrestart -r l -q)
-if [ ! -z "$NEEDS_TO_BE_RESTARTED" ]; then
-  echo "some services need a reboot"
-  reboot
-fi
+  # Reboot if still needed
+  NEEDS_TO_BE_RESTARTED=$(sudo needrestart -r l -q)
+  if [ ! -z "$NEEDS_TO_BE_RESTARTED" ]; then
+    echo "Security updates have been executed successfully, a reboot is needed to implement such updates"
+    exit 0
+  fi
+
+  echo "Security updates have been executed successfully, no reboot needed" && exit 0
+}
+
+###########
+#MAIN-LOOP#
+###########
+pre_update
+update
+post_upgrade_clean
+post_upgrade_restart
+
 # NOTE: perl throw warmings on remote machines
-
-
