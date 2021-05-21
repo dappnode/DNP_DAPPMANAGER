5f657ec480ab8cc346e8abeb138814a48ab866f9
pablo <mendez4a@gmail.com>
2021-05-21 09:32:41 +0200

    Splitted into functions. Added buttons to get credentials

--- packages/admin-ui/src/pages/vpn/components/wireguard/WireguardDeviceDetails.tsx
+++ packages/admin-ui/src/pages/vpn/components/wireguard/WireguardDeviceDetails.tsx
@@ -19,24 +19,9 @@ import { WireguardDeviceCredentials } from "types";
 // Utils
 import { urlJoin } from "utils/url";
 
-function WireguardDeviceDetailsLoaded({
-  id,
-  device
-}: {
-  id: string;
-  device: WireguardDeviceCredentials;
-}) {
-  const [showQrRemote, setShowQrRemote] = useState(false);
-  const [showQrLocal, setShowQrLocal] = useState(false);
-  const { configRemote, configLocal } = device;
-
-  useEffect(() => {
-    // Activate the copy functionality
-    new ClipboardJS(".copy-input-copy");
-  }, []);
-
+function WireguardCredentialsHeader({ id }: { id: string }) {
   return (
-    <Card className="wireguard-device-settings" spacing>
+    <>
       <header>
         <h5 className="card-title">{id || "Device not found"}</h5>
 
@@ -55,61 +40,113 @@ function WireguardDeviceDetailsLoaded({
         Use remote credentials by default. In case of your router does not
         support NAT loopback use local credentials
       </div>
+    </>
+  );
+}
 
-      <Form.Group>
-        <Form.Label>VPN remote credentials URL</Form.Label>
-        <div className="credentials-config">{configRemote}</div>
-      </Form.Group>
+function WireguardCredentialsFooter() {
+  return (
+    <div className="alert alert-secondary" role="alert">
+      Beware of shoulder surfing attacks (unsolicited observers), This data
+      grants admin access to your DAppNode
+    </div>
+  );
+}
+
+function WireguardCredentialsForm({
+  configType,
+  type
+}: {
+  configType: string;
+  type: "local" | "remote";
+}) {
+  return (
+    <Form.Group>
+      <Form.Label>VPN {type} credentials URL</Form.Label>
+      <div className="credentials-config">{configType}</div>
+    </Form.Group>
+  );
+}
+
+function WireguardDeviceDetailsLoaded({
+  id,
+  device
+}: {
+  id: string;
+  device: WireguardDeviceCredentials;
+}) {
+  const [showQrRemote, setShowQrRemote] = useState(false);
+  const [showQrLocal, setShowQrLocal] = useState(false);
+  const [showCreds, setShowCreds] = useState("");
+  const { configRemote, configLocal } = device;
+
+  useEffect(() => {
+    // Activate the copy functionality
+    new ClipboardJS(".copy-input-copy");
+  }, []);
+
+  return (
+    <Card className="wireguard-device-settings" spacing>
+      <WireguardCredentialsHeader id={id} />
 
       <div className="buttons">
-        <Button data-clipboard-text={configRemote}>
-          <span>
-            <GoClippy />
-            <span>Copy remote config</span>
-          </span>
+        <Button onClick={() => setShowCreds("remote")}>
+          Remote credentials
         </Button>
-
-        <Button onClick={() => setShowQrRemote(!showQrRemote)}>
-          <span>
-            <FaQrcode />
-            <span>{showQrRemote ? "Hide" : "Show"} remote config QR code</span>
-          </span>
-        </Button>
+        <Button onClick={() => setShowCreds("local")}>Local credentials</Button>
       </div>
+      {showCreds === "remote" ? (
+        <>
+          <WireguardCredentialsForm configType={configRemote} type={"remote"} />
+          <div className="buttons">
+            <Button data-clipboard-text={configRemote}>
+              <span>
+                <GoClippy />
+                <span>Copy remote config</span>
+              </span>
+            </Button>
 
-      {showQrRemote && configRemote && (
-        <QrCode url={configRemote} width={"400px"} />
-      )}
+            <Button onClick={() => setShowQrRemote(!showQrRemote)}>
+              <span>
+                <FaQrcode />
+                <span>
+                  {showQrRemote ? "Hide" : "Show"} remote config QR code
+                </span>
+              </span>
+            </Button>
+          </div>
+          {showQrRemote && configRemote && (
+            <QrCode url={configRemote} width={"400px"} />
+          )}
+        </>
+      ) : showCreds === "local" ? (
+        <>
+          <WireguardCredentialsForm configType={configLocal} type={"local"} />
+          <div className="buttons">
+            <Button data-clipboard-text={configLocal}>
+              <span>
+                <GoClippy />
+                <span>Copy local config</span>
+              </span>
+            </Button>
 
-      <Form.Group>
-        <Form.Label>VPN local credentials URL</Form.Label>
-        <div className="credentials-config">{configLocal}</div>
-      </Form.Group>
+            <Button onClick={() => setShowQrLocal(!showQrLocal)}>
+              <span>
+                <FaQrcode />
+                <span>
+                  {showQrLocal ? "Hide" : "Show"} local config QR code
+                </span>
+              </span>
+            </Button>
+          </div>
 
-      <div className="buttons">
-        <Button data-clipboard-text={configLocal}>
-          <span>
-            <GoClippy />
-            <span>Copy local config</span>
-          </span>
-        </Button>
+          {showQrLocal && configLocal && (
+            <QrCode url={configLocal} width={"400px"} />
+          )}
+        </>
+      ) : null}
 
-        <Button onClick={() => setShowQrLocal(!showQrLocal)}>
-          <span>
-            <FaQrcode />
-            <span>{showQrLocal ? "Hide" : "Show"} local config QR code</span>
-          </span>
-        </Button>
-      </div>
-
-      {showQrLocal && configLocal && (
-        <QrCode url={configLocal} width={"400px"} />
-      )}
-
-      <div className="alert alert-secondary" role="alert">
-        Beware of shoulder surfing attacks (unsolicited observers), This data
-        grants admin access to your DAppNode
-      </div>
+      <WireguardCredentialsFooter />
     </Card>
   );
 }
