27c1931e5a7560097d9e8a8f5ed6f5a3c800938a
dapplion <35266934+dapplion@users.noreply.github.com>
2021-07-13 13:58:30 +0200

    Add package sent data db entry

--- packages/dappmanager/src/db/package.ts
+++ packages/dappmanager/src/db/package.ts
@@ -5,6 +5,7 @@ import { stripDots } from "./dbUtils";
 const PACKAGE_GETTING_STARTED_SHOW = "package-getting-started-show";
 const PACKAGE_INSTALL_TIME = "package-install-time";
 const PACKAGE_LATEST_KNOWN_VERSION = "package-latest-known-version";
+const PACKAGE_SENT_DATA = "package-sent-data";
 
 export const packageGettingStartedShow = dbCache.indexedByKey<boolean, string>({
   rootKey: PACKAGE_GETTING_STARTED_SHOW,
@@ -31,5 +32,16 @@ export const packageLatestKnownVersion = dbCache.indexedByKey<
 >({
   rootKey: PACKAGE_LATEST_KNOWN_VERSION,
   // Do NOT strip dots so the packages can be indexed by dnpName doing .getAll()
+  getKey: dnpName => dnpName
+});
+
+/**
+ * Store data sent by the package and show it to the UI
+ */
+export const packageSentData = dbCache.indexedByKey<
+  Record<string, string>,
+  string
+>({
+  rootKey: PACKAGE_SENT_DATA,
   getKey: dnpName => dnpName
 });
