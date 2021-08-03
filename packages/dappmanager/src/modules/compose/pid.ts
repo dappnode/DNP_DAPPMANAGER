73850666fc8103bf6532ed050f09ca29862eba97
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-03 19:28:31 +0200

    Update pid.ts

--- packages/dappmanager/src/modules/compose/pid.ts
+++ packages/dappmanager/src/modules/compose/pid.ts
@@ -5,18 +5,8 @@ import { InstallPackageData } from "../../types";
  * in any of its services
  */
 export function hasPid(pkg: InstallPackageData): boolean {
-  const composeServices = Object.keys(pkg.compose.services);
-
-  if (composeServices.length === 1) {
-    // Monoservice package
-    return pkg.compose.services[pkg.dnpName].pid ? true : false;
-  } else if (composeServices.length > 1) {
-    // Multiservice package
-    for (const composeService of composeServices) {
-      if (pkg.compose.services[composeService].pid) return true;
-    }
-    return false;
+  for (const service of Object.values(pkg.compose.services)) {
+    if (service.pid) return true;
   }
-  // Unexpected number of services
-  else throw Error(`Unexpected number of services: ${composeServices.length}`);
+  return false;
 }
