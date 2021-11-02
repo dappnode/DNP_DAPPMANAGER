97b4fba42b3fc7ab6a10dea7d13a9f302665e235
Tropicar <zerozept@protonmail.com>
2021-11-02 15:30:26 +0100

    Undo the changes in the Front end side
    Now the sort function is not done in this side of the dappmanager.

--- packages/admin-ui/src/pages/dashboard/components/ChainCard.tsx
+++ packages/admin-ui/src/pages/dashboard/components/ChainCard.tsx
@@ -10,9 +10,7 @@ import { Link } from "react-router-dom";
 import { rootPath as packagesRootPath } from "pages/packages";
 
 export function ChainCards() {
-  const chainData = useChainData().sort((a, b) =>
-    a.dnpName.localeCompare(b.dnpName)
-  );
+  const chainData = useChainData();
 
   return (
     <div className="dashboard-cards">
