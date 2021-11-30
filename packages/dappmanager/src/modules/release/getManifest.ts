16c9f7c12ae809cb430d50dc41d8941787db0605
pablo <mendez4a@gmail.com>
2021-11-30 12:12:37 +0100

    fix buf gettting manifest from ipfs remote

--- packages/dappmanager/src/modules/release/getManifest.ts
+++ packages/dappmanager/src/modules/release/getManifest.ts
@@ -1,7 +1,9 @@
-import { ipfs } from "../ipfs";
+import { ipfs, IPFSEntry } from "../ipfs";
 import { parseManifest, validateManifestBasic } from "../manifest";
 import { Manifest } from "../../types";
 import { isDirectoryRelease } from "./ipfs/isDirectoryRelease";
+import { IpfsClientTarget } from "../../common";
+import { releaseFiles } from "../../params";
 
 export async function getManifest(contentUri: string): Promise<Manifest> {
   let data: string;
@@ -9,9 +11,7 @@ export async function getManifest(contentUri: string): Promise<Manifest> {
     const ipfsEntries = await ipfs.list(contentUri);
     const isDirectory = await isDirectoryRelease(ipfsEntries);
     if (isDirectory) {
-      data = await ipfs.writeFileToMemory(
-        `${contentUri}/dappnode_package.json`
-      );
+      data = await getManifestFromDir(ipfsEntries, contentUri);
     } else {
       data = await ipfs.writeFileToMemory(contentUri);
     }
@@ -20,4 +20,21 @@ export async function getManifest(contentUri: string): Promise<Manifest> {
   }
 
   return validateManifestBasic(parseManifest(data));
+}
+
+async function getManifestFromDir(
+  ipfsEntries: IPFSEntry[],
+  contentUri: string
+): Promise<string> {
+  if (ipfs.ipfsClientTarget === IpfsClientTarget.remote) {
+    const manifestPath = ipfsEntries.find(file =>
+      releaseFiles.manifest.regex.test(file.name)
+    );
+    if (!manifestPath) throw Error("Manifest not found using IPFS gateway");
+    // The ipfs dag.export method does not allow hash using "path" format
+    return await ipfs.writeFileToMemory(manifestPath.cid.toString());
+  } else {
+    // The ipfs cat method does allow to use hash using "path" format
+    return await ipfs.writeFileToMemory(`${contentUri}/dappnode_package.json`);
+  }
 }
--- packages/dappmanager/test/integration/ipfs/ipfsGateway.test.int.ts
+++ packages/dappmanager/test/integration/ipfs/ipfsGateway.test.int.ts
@@ -1,10 +1,12 @@
 import "mocha";
 import { expect } from "chai";
 import { mockManifestWithImage } from "../../testUtils";
-import { ManifestWithImage } from "../../../src/types";
+import { ManifestWithImage, IpfsClientTarget } from "../../../src/types";
 import { uploadManifestRelease } from "../../integrationSpecs/buildReleaseManifest";
-import { localIpfsGateway } from "../../testIpfsUtils";
-import { catCarReaderToMemory } from "../../../src/modules/ipfs/writeFileToMemory";
+import { uploadDirectoryRelease } from "../../integrationSpecs/buildReleaseDirectory";
+import { ipfs } from "../../../src/modules/ipfs";
+import { getManifest } from "../../../src/modules/release/getManifest";
+import { ipfsGatewayUrl } from "../../testIpfsUtils";
 
 describe("IPFS remote", function () {
   this.timeout(100000 * 5);
@@ -14,16 +16,41 @@ describe("IPFS remote", function () {
     ...mockManifestWithImage,
     name: dnpName
   };
-  let releaseHash: string;
+  let manifestHash: string;
+  let dnpReleaseHash: string;
 
   before(async () => {
-    releaseHash = await uploadManifestRelease(manifest);
+    // Set remote IPFS host
+    ipfs.changeHost(ipfsGatewayUrl, IpfsClientTarget.remote);
+    // Upload manifest and dnp directrory
+    manifestHash = await uploadManifestRelease(manifest);
+    dnpReleaseHash = await uploadDirectoryRelease({
+      manifest,
+      compose: {
+        version: "3.5",
+        services: {
+          [dnpName]: {
+            restart: "unless-stopped"
+          }
+        }
+      }
+    });
   });
 
-  it("Should get content from IPFS gateway", async function () {
+  it("Should get manifest from IPFS gateway", async function () {
     // If the content hashed does not match the CID there is thrown an error
-    const buff = await catCarReaderToMemory(localIpfsGateway, releaseHash);
+    const buff = await ipfs.writeFileToMemory(manifestHash);
     const contentParsed = JSON.parse(buff.toString());
     expect(contentParsed).to.be.ok;
+  });
+
+  it("Should get manifest from directory using IPFS gateway", async function () {
+    const manifest = await getManifest(dnpReleaseHash);
+    expect(manifest).to.be.ok;
+  });
+
+  after(async () => {
+    // Set remote IPFS host
+    ipfs.changeHost(ipfsGatewayUrl, IpfsClientTarget.local);
   });
 });
