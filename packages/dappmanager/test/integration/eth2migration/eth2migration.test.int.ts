ef81e206b49da90a8694b3f59224ce0fa034f39d
pablo <mendez4a@gmail.com>
2022-01-14 16:47:18 +0100

    added keystore and slashing file for int tests

---
+++ packages/dappmanager/test/integration/eth2migration/eth2migration.test.int.ts
@@ -0,0 +1,16 @@
+import "mocha";
+import { expect } from "chai";
+
+describe.only("eth2migrations", function () {
+  before(async () => {
+    // Connect to IPFS node for:
+    // - web3signer installation package
+    // - eth2client installation package
+  });
+
+  before(async () => {
+    // Setup validator container
+    // Import keystores
+    // Import slashing protection
+  });
+});
