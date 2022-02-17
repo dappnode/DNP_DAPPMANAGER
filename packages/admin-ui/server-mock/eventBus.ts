65f4ad4ce1f888bc915bed2e6d703a8ff2c871ae
dapplion <35266934+dapplion@users.noreply.github.com>
2022-02-17 19:23:15 +0530

    Remove directory event

--- packages/admin-ui/server-mock/eventBus.ts
+++ packages/admin-ui/server-mock/eventBus.ts
@@ -5,7 +5,6 @@ const emptySubscription = { on: () => {}, emit: () => {} };
 
 export const eventBus: EventBus = {
   chainData: emptySubscription,
-  directory: emptySubscription,
   registry: emptySubscription,
   logUi: emptySubscription,
   logUserAction: emptySubscription,
