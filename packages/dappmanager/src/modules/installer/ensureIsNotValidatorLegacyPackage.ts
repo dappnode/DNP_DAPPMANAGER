4172d8c6a610388f0cc66b4ae750562efa34b54a
pablo <mendez4a@gmail.com>
2022-03-07 14:27:59 +0100

    ensure not installing old validato clients

---
+++ packages/dappmanager/src/modules/installer/ensureIsNotValidatorLegacyPackage.ts
@@ -0,0 +1,39 @@
+import { InstallPackageData } from "../../types";
+import semver from "semver";
+
+/**
+ * Prevent from installing legacy version (previous to remote signer support) for packages:
+ * - Prysm
+ * - Prysm-Prater
+ * - Gnosis-Beacon-Chain-Prysm
+ */
+export async function ensureIsNotValidatorLegacyPackage(
+  packagesData: InstallPackageData[]
+): Promise<void> {
+  for (const pkg of packagesData) {
+    switch (pkg.dnpName) {
+      case "prysm-prater.dnp.dappnode.eth":
+        if (semver.lte(pkg.semVersion, "1.0.7"))
+          throw Error(
+            `${pkg.dnpName}:${pkg.semVersion} is a legacy validator client, install a more recent version with remote signer support`
+          );
+        continue;
+
+      // To be included once after prysm-prater
+      /*       case "gnosis-beacon-chain-prysm.dnp.dappnode.eth":
+        if (semver.lte(pkg.semVersion, "1.0.0"))
+          throw Error(
+            `${pkg.dnpName}:${pkg.semVersion} is a legacy validator client, install a more recent version with remote signer support`
+          );
+        continue;
+      case "prysm.dnp.dappnode.eth":
+        if (semver.lte(pkg.semVersion, "1.0.0"))
+          throw Error(
+            `${pkg.dnpName}:${pkg.semVersion} is a legacy validator client, install a more recent version with remote signer support`
+          );
+        continue; */
+      default:
+        continue;
+    }
+  }
+}
