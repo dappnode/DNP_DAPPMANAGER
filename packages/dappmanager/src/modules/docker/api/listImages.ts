4ba262225a19f795b9cb55c5b8048e4cc69876c6
pablo <mendez4a@gmail.com>
2022-03-28 15:18:56 +0200

    remove deprecated docker list options

--- packages/dappmanager/src/modules/docker/api/listImages.ts
+++ packages/dappmanager/src/modules/docker/api/listImages.ts
@@ -10,42 +10,6 @@ import { docker } from "./docker";
  *   }
  * }
  */
-export async function imagesList(
-  options?: DockerApiListImagesOptions
-): Promise<Dockerode.ImageInfo[]> {
-  return docker.listImages(options || {}) as Promise<Dockerode.ImageInfo[]>;
-}
-
-export interface DockerApiListImagesOptions {
-  /**
-   * Show all images. Only images from a final layer (no children) are shown by default.
-   */
-  all?: boolean;
-  /**
-   * A JSON encoded value of the filters (a map[string][]string) to process on the images list. Available filters:
-   */
-  filters?: {
-    /**
-     * (<image-name>[:<tag>], <image id> or <image@digest>),
-     */
-    before?: string;
-    dangling?: Record<string, unknown>;
-    /**
-     * key or label="key=value" of an image label
-     */
-    label?: string[];
-    /**
-     * (<image-name>[:<tag>])
-     * Partial RepoTag, i.e. "dappmanager.dnp.dappnode.eth"
-     */
-    reference?: string[];
-    /**
-     * (<image-name>[:<tag>], <image id> or <image@digest>)
-     */
-    since?: string;
-  };
-  /**
-   * Show digest information as a RepoDigests field on each image
-   */
-  digests?: boolean;
+export async function imagesList(): Promise<Dockerode.ImageInfo[]> {
+  return docker.listImages();
 }
