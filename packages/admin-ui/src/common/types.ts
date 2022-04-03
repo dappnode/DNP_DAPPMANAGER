d193d454d423adae8ccb67a905c76a78104a6f33
pablo <mendez4a@gmail.com>
2022-04-03 19:28:32 +0200

    fix typo in chain driver

--- packages/admin-ui/src/common/types.ts
+++ packages/admin-ui/src/common/types.ts
@@ -429,7 +429,7 @@ export type ContainerState =
 export type ChainDriver = ChainDriverType | ChainDriverSpecs;
 
 export type ChainDriverSpecs = {
-  driver: ChainDriverType;
+  chain: ChainDriverType;
   serviceName?: string;
   portNumber?: number;
 };
