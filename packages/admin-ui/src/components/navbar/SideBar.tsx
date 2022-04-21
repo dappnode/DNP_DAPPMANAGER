7eff4e1735e404d94a45ba1fbf893e0215f2b551
pablo <mendez4a@gmail.com>
2022-04-21 14:02:38 +0200

    Use reactContext. Set responsive sidebar
    set transition to sidebar and border 0 on collapsed
    
    fix supported by
    
    setup colors for topbar and sidebar
    
    setup colors for text

--- packages/admin-ui/src/components/navbar/SideBar.tsx
+++ packages/admin-ui/src/components/navbar/SideBar.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { sidenavItems, fundedBy } from "./navbarItems";
-import logo from "img/dappnode-logo-wide-min.png";
 import "./sidebar.scss";
 
 if (!Array.isArray(sidenavItems)) throw Error("sidenavItems must be an array");
@@ -11,80 +10,28 @@ if (!Array.isArray(fundedBy)) throw Error("fundedBy must be an array");
 // In order to avoid the App or redux to be aware of the
 // sidebar state while allowing the tobar to toggle the sidebar
 // Both components will communicate through window events
-const toggleSideNavEvent = "toggleSideNavEvent";
-export function toggleSideNav() {
-  window.dispatchEvent(new Event(toggleSideNavEvent));
-}
 
-export default function SideBar({ theme }: { theme: "light" | "dark" }) {
-  const [collapsed, setCollapsed] = useState(true);
-
-  const sidebarEl = useRef<HTMLDivElement>(null);
-
-  function collapseSideNav() {
-    setCollapsed(true);
-  }
-
-  useEffect(() => {
-    const handleToggleSideNav = () => setCollapsed(_collapsed => !_collapsed);
-    window.addEventListener(toggleSideNavEvent, handleToggleSideNav);
-    return () => {
-      window.removeEventListener(toggleSideNavEvent, handleToggleSideNav);
-    };
-  }, []);
-
-  useEffect(() => {
-    // width is the previous width, to compute the direction of the change
-    let width = window.innerWidth;
-    // Always collapse the navbar when crossing the breakpoint, going from big to small
-    function onWindowResize() {
-      const breakPointPx = getBreakPointPx();
-      if (width > breakPointPx && window.innerWidth <= breakPointPx)
-        collapseSideNav();
-      width = window.innerWidth;
-    }
-    window.addEventListener("resize", onWindowResize);
-    return () => {
-      window.removeEventListener("resize", onWindowResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (collapsed) return; // Prevent unnecessary listeners
-    function handleMouseDown(e: MouseEvent) {
-      if (sidebarEl.current && !sidebarEl.current.contains(e.target as Node))
-        setCollapsed(true);
-    }
-    document.addEventListener("mousedown", handleMouseDown);
-    return () => {
-      document.removeEventListener("mousedown", handleMouseDown);
-    };
-  }, [collapsed]);
-
+export default function SideBar({ screenWidth }: { screenWidth: number }) {
   return (
-    <div
-      id="sidebar"
-      ref={sidebarEl}
-      className={collapsed ? "collapsed" : `sidebar-${theme}`}
-    >
-      <NavLink className="sidenav-item top" to={"/"} onClick={collapseSideNav}>
-        <img className="sidebar-logo header" src={logo} alt="logo" />
-      </NavLink>
-
+    <div id="sidebar">
       <div className="nav">
-        <div className="sidenav-item">
-          <div className="subheader">ADMIN UI</div>
-        </div>
+        {screenWidth > 640 && (
+          <div className="sidenav-item">
+            <div className="subheader">ADMIN UI</div>
+          </div>
+        )}
 
         {sidenavItems.map(item => (
           <NavLink
             key={item.name}
             className="sidenav-item selectable"
-            onClick={collapseSideNav}
             to={item.href}
           >
             <item.icon />
-            <span className="name svg-text">{item.name}</span>
+            {/* 640 px = 40 rem */}
+            {screenWidth > 640 && (
+              <span className="name svg-text">{item.name}</span>
+            )}
           </NavLink>
         ))}
       </div>
@@ -93,7 +40,9 @@ export default function SideBar({ theme }: { theme: "light" | "dark" }) {
       <div className="spacer" />
 
       <div className="funded-by">
-        <div className="funded-by-text">SUPPORTED BY</div>
+        {screenWidth > 640 && (
+          <div className="funded-by-text">SUPPORTED BY</div>
+        )}
         <div className="funded-by-logos">
           {fundedBy.map((item, i) => (
             <a key={i} href={item.link}>
@@ -112,16 +61,4 @@ export default function SideBar({ theme }: { theme: "light" | "dark" }) {
       </div>
     </div>
   );
-}
-
-// Utility
-
-function getBreakPointPx() {
-  const breakPointRem = parseFloat(
-    getComputedStyle(document.body).getPropertyValue("--sidebar-breakpoint")
-  );
-  const baseDocumentFontSize = parseFloat(
-    getComputedStyle(document.documentElement).fontSize
-  );
-  return breakPointRem * baseDocumentFontSize;
 }
