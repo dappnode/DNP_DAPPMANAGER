ad4f389a3b539ea136d72273945710e99ba9671d
dapplion <35266934+dapplion@users.noreply.github.com>
2022-05-16 19:53:13 +0700

    Remove properties from knownChains

--- packages/dappmanager/src/modules/chains/getChainDriverName.ts
+++ packages/dappmanager/src/modules/chains/getChainDriverName.ts
@@ -11,14 +11,11 @@ export function getChainDriverName(
 }
 
 const knownChains: { [dnpName: string]: ChainDriver } = {
-  "openethereum.dnp.dappnode.eth": "ethereum",
-  "ropsten.dnp.dappnode.eth": "ethereum",
-  "rinkeby.dnp.dappnode.eth": "ethereum",
-  "kovan.dnp.dappnode.eth": "ethereum",
-  "bitcoin.dnp.dappnode.eth": "bitcoin",
-  "monero.dnp.dappnode.eth": "monero",
-  "prysm.dnp.dappnode.eth": "ethereum-beacon-chain",
-  "prysm-prater.dnp.dappnode.eth": "ethereum-beacon-chain",
-  "lighthouse-prater.dnp.dappnode.eth": "ethereum-beacon-chain",
-  "teku-prater.dnp.dappnode.eth": "ethereum-beacon-chain"
+  // Pending: https://github.com/dappnode/DAppNodePackage-prysm/pull/65
+  "prysm.dnp.dappnode.eth": "ethereum-beacon-chain"
+  // ===============================
+  // DO NOT ADD ANY NEW PACKAGE HERE
+  // ===============================
+  // Instead add "chain" property in your package, like https://github.com/dappnode/DAppNodePackage-prysm/pull/65
+  // Full docs: https://docs.dappnode.io/developers/manifest-reference#chain
 };
