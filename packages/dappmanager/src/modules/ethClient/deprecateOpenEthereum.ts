c6c45cdb530b973b0945a45fd20b50497423fe83
Pablo <mendez4a@gmail.com>
2022-06-21 11:35:19 +0200

    set ethClientTarget to other client before remote

--- packages/dappmanager/src/modules/ethClient/deprecateOpenEthereum.ts
+++ packages/dappmanager/src/modules/ethClient/deprecateOpenEthereum.ts
@@ -2,27 +2,76 @@ import * as db from "../../db";
 import { listPackageNoThrow } from "../docker/list";
 import { logs } from "../../logs";
 import { EthClientTarget } from "../../types";
-import { packageRemove } from "../../calls";
+import { packageGet, packageRemove } from "../../calls";
+import { ethClientData } from "../../params";
 
+/**
+ * Removes deprecated openethereum packages: gnosis and mainet
+ * Switches the ethclient target if openethereum was selected
+ */
 export async function deprecateOpenEthereum(): Promise<void> {
-  const openEthereumPackage = await listPackageNoThrow({
-    dnpName: "openethereum.dnp.dappnode.eth"
-  });
-  if (!openEthereumPackage) return;
+  const openEthereumPackagesNames = [
+    "openethereum.dnp.dappnode.eth",
+    "openethereum-gnosis-chain.dnp.dappnode.eth"
+  ];
 
-  logs.info("Openethereum is deprecated, removing it");
-  await packageRemove({
-    dnpName: "openethereum.dnp.dappnode.eth",
-    deleteVolumes: true
-  });
+  for (const openEthereumPackageName of openEthereumPackagesNames) {
+    const openEthereumPackage = await listPackageNoThrow({
+      dnpName: openEthereumPackageName
+    });
+    if (!openEthereumPackage) return;
+
+    logs.info(`${openEthereumPackageName} is deprecated, removing it`);
+    await packageRemove({
+      dnpName: openEthereumPackageName,
+      deleteVolumes: true
+    });
+  }
 
   const ethClientTarget = db.ethClientTarget.get() as
     | EthClientTarget
     | null
     | "openethereum"; // Add old deprecated type
+  if (ethClientTarget === "openethereum") await switchEthClientTarget();
+}
 
-  if (ethClientTarget === "openethereum") {
-    logs.info("Openethereum is deprecated, setting ethClientTarget to remote");
-    db.ethClientTarget.set("remote");
+/**
+ * Switches ethClientTarget in the following preference order: geth/geth light > nethermind > remote
+ */
+async function switchEthClientTarget(): Promise<void> {
+  // Check for geth
+  const gethPackage = await listPackageNoThrow({
+    dnpName: ethClientData.geth.dnpName
+  });
+  if (gethPackage) {
+    logs.info("Setting ethClientTarget to geth");
+    const gethPackageData = await packageGet({
+      dnpName: ethClientData.geth.dnpName
+    });
+
+    const gethEnvironment = gethPackageData.userSettings?.environment;
+    if (!gethEnvironment)
+      throw Error("geth package installed but no environment found");
+
+    const isLightClient =
+      gethEnvironment["geth.dnp.dappnode.eth"]["SYNCMODE"] === "light";
+
+    isLightClient
+      ? db.ethClientTarget.set("geth-light")
+      : db.ethClientTarget.set("geth");
+    return;
   }
+
+  // Check for nethermind
+  const nethermindPackage = await listPackageNoThrow({
+    dnpName: ethClientData.nethermind.dnpName
+  });
+  if (nethermindPackage) {
+    logs.info("Setting ethClientTarget to nethermind");
+    db.ethClientTarget.set("nethermind");
+    return;
+  }
+
+  logs.info("Setting ethClientTarget to remote");
+  db.ethClientTarget.set("remote");
 }
