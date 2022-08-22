2385500b6d182d1f549066c13ac3fbf0c71e82af
Pablo <mendez4a@gmail.com>
2022-08-22 17:43:32 +0200

    implement advance view
    improved UIX

--- packages/admin-ui/src/common/types.ts
+++ packages/admin-ui/src/common/types.ts
@@ -1177,6 +1177,37 @@ export interface TrustedReleaseKey {
 
 export type Network = "mainnet" | "prater" | "gnosis";
 
+// Mainnet
+
+export type ConsensusClientMainnet =
+  | "prysm.dnp.dappnode.eth"
+  | "lighthouse.dnp.dappnode.eth"
+  | "teku.dnp.dappnode.eth"
+  | "nimbus.dnp.dappnode.eth";
+export type ExecutionClientMainnet =
+  | "geth.dnp.dappnode.eth"
+  | "besu.dnp.dappnode.eth"
+  | "erigon.dnp.dappnode.eth"
+  | "nethermind.dnp.dappnode.eth";
+
+// Prater
+
+export type ConsensusClientPrater =
+  | "prysm-prater.dnp.dappnode.eth"
+  | "lighthouse-prater.dnp.dappnode.eth"
+  | "teku-prater.dnp.dappnode.eth"
+  | "nimbus-prater.dnp.dappnode.eth";
+export type ExecutionClientPrater = "goerli-geth.dnp.dappnode.eth";
+
+// Gnosis
+
+export type ConsensusClientGnosis =
+  | "gnosis-beacon-chain-prysm.dnp.dappnode.eth"
+  | "lighthouse-gnosis.dnp.dappnode.eth"
+  | "teku-gnosis.dnp.dappnode.eth"
+  | "nimbus-gnosis.dnp.dappnode.eth";
+export type ExecutionClientGnosis = "nethermind-xdai.dnp.dappnode.eth";
+
 export interface StakerConfigGet {
   executionClients: PkgStatus[];
   consensusClients: PkgStatus[];
@@ -1196,8 +1227,8 @@ export interface StakerConfigSet {
   network: Network;
   executionClient?: string;
   consensusClient?: string;
-  installWeb3signer?: boolean;
-  installMevBoost?: boolean;
+  enableWeb3signer?: boolean;
+  enableMevBoost?: boolean;
   graffiti?: string;
   feeRecipient?: string;
 }
