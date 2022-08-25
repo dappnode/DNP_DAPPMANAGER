00fe9103fbc3e10dd54ce0b5460181e6434fc0c2
Pablo <mendez4a@gmail.com>
2022-08-25 10:29:43 +0200

    improve UIX

--- packages/admin-ui/src/pages/stakers/components/columns/MevBoost.tsx
+++ packages/admin-ui/src/pages/stakers/components/columns/MevBoost.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import Card from "components/Card";
-import { BsCircle, BsCircleFill } from "react-icons/bs";
 import { prettyDnpName } from "utils/format";
+import { joinCssClass } from "utils/css";
+import "./columns.scss";
 
 export default function MevBoost({
   mevBoost,
   setEnableMevBoost,
   isInstalled,
-  enableMevBoost
+  isSelected
 }: {
   mevBoost: string;
   setEnableMevBoost: (installMevBoost: boolean) => void;
   isInstalled: boolean;
-  enableMevBoost: boolean;
+  isSelected: boolean;
 }) {
   return (
     <Card
-      onClick={() => setEnableMevBoost(!enableMevBoost)}
-      shadow={enableMevBoost}
+      className={`mev-boost ${joinCssClass({ isSelected })}`}
+      onClick={() => setEnableMevBoost(!isSelected)}
+      shadow={isSelected}
     >
-      <p>{prettyDnpName(mevBoost)}</p>
-      {isInstalled ? <BsCircleFill /> : <BsCircle />} Installed <br />
-      {enableMevBoost ? <BsCircleFill /> : <BsCircle />} Enabled
+      <div className="title">{prettyDnpName(mevBoost)}</div>
     </Card>
   );
 }
