1e26546ebedfae2d3ddbc28d917c68b947af963a
pablomendezroyo <mendez4a@gmail.com>
2022-11-17 17:06:13 +0100

    bind mount the code in a volume

--- docker/start_dev.sh
+++ docker/start_dev.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# link important directories
 ln -s /app/packages/admin-ui/build/ /app/packages/dappmanager/dist
 ln -s /usr/src/app/dnp_repo/ /app/packages/dappmanager
 ln -s /usr/src/app/DNCORE/ /app/packages/dappmanager
 
-tmux new-session -d -s dev 'cd /app/packages/dappmanager;yarn dev'
-tmux split-window;
-tmux send 'cd /app/packages/admin-ui;yarn dev' ENTER;
+# Build admin-ui
+cd /app/packages/admin-ui/ && yarn build
 
-while true
-do
-	echo "Press [CTRL+C] to stop.."
-	sleep 1000
-done
+# Build dappmanager
+cd /app/packages/dappmanager/ && yarn build
+
+
+# execute the scripts in background and wait
+cd /app/packages/dappmanager && yarn dev &
+cd /app/packages/admin-ui && yarn dev &
+echo "========================================="
+echo "Access the ui at http://172.33.1.7:5000/"
+echo "========================================="
+wait
