05aab7b6887e310ed33ad5ad880670187fe9d0a9
pablomendezroyo <mendez4a@gmail.com>
2022-11-17 18:01:31 +0100

    run lerna first

--- docker/start_dev.sh
+++ docker/start_dev.sh
@@ -5,17 +5,17 @@ ln -s /app/packages/admin-ui/build/ /app/packages/dappmanager/dist
 ln -s /usr/src/app/dnp_repo/ /app/packages/dappmanager
 ln -s /usr/src/app/DNCORE/ /app/packages/dappmanager
 
+# Install lerna first
+yarn --frozen-lockfile --non-interactive --ignore-scripts --ignore-optional
+yarn
+
 # Build admin-ui
 cd /app/packages/admin-ui/ && yarn build
 
 # Build dappmanager
 cd /app/packages/dappmanager/ && yarn build
 
-
 # execute the scripts in background and wait
 cd /app/packages/dappmanager && yarn dev &
 cd /app/packages/admin-ui && yarn dev &
-echo "========================================="
-echo "Access the ui at http://172.33.1.7:5000/"
-echo "========================================="
 wait
