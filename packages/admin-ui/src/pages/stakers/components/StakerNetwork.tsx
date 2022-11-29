350678a92de4e840d4abd5fa1a8ad5499e3d4b28
pablomendezroyo <mendez4a@gmail.com>
2022-11-29 20:17:51 +0100

    Fix staker UIX for gnosis

--- packages/admin-ui/src/pages/stakers/components/StakerNetwork.tsx
+++ packages/admin-ui/src/pages/stakers/components/StakerNetwork.tsx
@@ -264,8 +264,12 @@ export default function StakerNetwork<T extends Network>({
             (2) Choose a Consensus Layer client (+ validator) <br />
             (3) Install the web3signer, which will hold the validator keys and
             sign <br />
-            (4) Optional; delegate block-building capacities through the MEV
-            Boost network and potentially profit from MEV
+            {network !== "gnosis" && (
+              <>
+                (4) Optional; delegate block-building capacities through the MEV
+                Boost network and potentially profit from MEV
+              </>
+            )}
           </p>
           <br />
           <p>{description}</p>
