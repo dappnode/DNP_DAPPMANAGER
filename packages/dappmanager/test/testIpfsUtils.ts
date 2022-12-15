9582185c5a8a95833c60a9d26b1c1513fa11c743
pablomendezroyo <mendez4a@gmail.com>
2022-12-15 15:26:53 +0100

    catch error starting ipfs container

--- packages/dappmanager/test/testIpfsUtils.ts
+++ packages/dappmanager/test/testIpfsUtils.ts
@@ -44,7 +44,9 @@ export async function setUpIpfsNode(): Promise<void> {
   // Startup ipfs container
   await shell(
     `docker run --rm -d --name ${ipfsTestContainerName} -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 ipfs/go-ipfs:v0.17.0`
-  );
+  ).catch(e => {
+    throw Error(`Error starting up local IPFS node ${e}`);
+  });
 
   // Wait until ipfs is available
   let isIpfsAvail = false;
