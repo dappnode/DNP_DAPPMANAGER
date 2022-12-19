d4227a9123b4f9f3c54fc697c8088f937b29e865
pablomendezroyo <mendez4a@gmail.com>
2022-12-19 16:35:10 +0100

    Implement url node native

--- packages/dappmanager/test/endToEnd/apiCalls/statsCpuGet.test.end.ts
+++ packages/dappmanager/test/endToEnd/apiCalls/statsCpuGet.test.end.ts
@@ -2,12 +2,15 @@ import "mocha";
 import { expect } from "chai";
 import { dappmanagerTestApiUrl } from "../endToEndUtils";
 import { validateRoutesReturn } from "../../../src/common";
+import fetch from "node-fetch";
+import { URL } from "url";
 
 const apiCallMethod = "statsCpuGet";
 
 describe(`API call ${apiCallMethod}`, async () => {
   it("Should return the cpu use percentage", async () => {
-    const response = await fetch(`${dappmanagerTestApiUrl}/${apiCallMethod}`);
+    const url = new URL(`${dappmanagerTestApiUrl}/${apiCallMethod}`);
+    const response = await fetch(url);
     expect(response.status).to.equal(200);
     const body = await response.json();
     expect(validateRoutesReturn(apiCallMethod, body)).to.be.ok;
