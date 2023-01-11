cacb1f06faf411c429264eee7261ad3bd40d840b
pablomendezroyo <mendez4a@gmail.com>
2023-01-11 12:39:56 +0100

    set webpack config as esm

--- packages/dappmanager/webpack.config.js
+++ packages/dappmanager/webpack.config.js
@@ -1,12 +1,23 @@
-const path = require("path");
-const webpack = require("webpack"); //to access built-in plugins
+import path from "path";
+import webpack from "webpack";
+import { fileURLToPath } from "url";
 const { NODE_ENV = "production" } = process.env;
-module.exports = {
-  entry: "./src/index.ts",
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const paths = {
+  // Source files
+  src: path.resolve(__dirname, "./src"),
+  // Production build files
+  build: path.resolve(__dirname, "./build")
+};
+
+export default {
+  entry: paths.src + "/index.ts",
   mode: NODE_ENV,
   target: "node",
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: paths.build,
     filename: "index.js"
   },
   // externals: [/node_modules/, "bufferutil", "utf-8-validate"],
