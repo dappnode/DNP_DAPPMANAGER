80c9995e890dfaa7289a19746b4b3f56b4594687
Voss <voss@visnovalabs.io>
2023-02-09 01:28:39 -0600

    revert for conflicts

--- packages/admin-ui/src/common/types.ts
+++ packages/admin-ui/src/common/types.ts
@@ -1215,7 +1215,6 @@ export type ConsensusClientPrater =
   | "lighthouse-prater.dnp.dappnode.eth"
   | "teku-prater.dnp.dappnode.eth"
   | "nimbus-prater.dnp.dappnode.eth"
-  | "lodestar-prater.dnp.dappnode.eth"
   | "";
 
 export type ExecutionClientPrater =
