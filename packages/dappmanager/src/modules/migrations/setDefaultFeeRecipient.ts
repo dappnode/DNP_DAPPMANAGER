6d33f705240927df52131a9155caae6f53a101ec
Pablo Mendez <mendez4a@gmail.com>
2023-02-15 13:20:53 +0100

    fix import

--- packages/dappmanager/src/modules/migrations/setDefaultFeeRecipient.ts
+++ packages/dappmanager/src/modules/migrations/setDefaultFeeRecipient.ts
@@ -1,6 +1,6 @@
 import { ConsensusClient, Network } from "@dappnode/common";
 import * as db from "../../db/index.js";
-import { packageGet } from "../../calls";
+import { packageGet } from "../../calls/index.js";
 import { listPackageNoThrow } from "../docker/list/listPackages.js";
 import { getValidatorServiceName } from "../stakerConfig/utils.js";
 
