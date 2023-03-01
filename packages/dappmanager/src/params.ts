fbdd0e0930cc880222487034a0f2e5c046fc526c
Diego <dsimog01@estudiantes.unileon.es>
2023-03-01 11:15:01 +0100

    Fixed formatting

--- packages/dappmanager/src/params.ts
+++ packages/dappmanager/src/params.ts
@@ -1,9 +1,7 @@
 import path from "path";
 import { FileFormat } from "./types.js";
 import { Architecture } from "@dappnode/dappnodesdk";
-import {
-  EthClientTargetPackage,
-  UserSettings} from "@dappnode/common";
+import { EthClientTargetPackage, UserSettings } from "@dappnode/common";
 
 const devMode = process.env.LOG_LEVEL === "DEV_MODE";
 
