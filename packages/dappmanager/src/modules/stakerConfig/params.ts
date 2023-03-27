cb973289b7ff454919f062ab327d56d7a6727e44
Diego <dsimog01@estudiantes.unileon.es>
2023-03-27 11:03:29 +0200

    Updated prater lodestar minimum versions

--- packages/dappmanager/src/modules/stakerConfig/params.ts
+++ packages/dappmanager/src/modules/stakerConfig/params.ts
@@ -7,23 +7,23 @@ import {
 const lodestarStakersPraterMinimumVersions: ExecutionClientOrSignerVersion<"prater"> =
   {
     "goerli-nethermind.dnp.dappnode.eth": "1.0.8",
-    "goerli-geth.dnp.dappnode.eth": "0.4.27", // Needs publishment
-    "goerli-erigon.dnp.dappnode.eth": "0.1.4",
-    "goerli-besu.dnp.dappnode.eth": "0.1.1", // Needs publishment
-    "web3signer-prater.dnp.dappnode.eth": "0.1.18" // Needs publishment
+    "goerli-geth.dnp.dappnode.eth": "0.4.28",
+    "goerli-erigon.dnp.dappnode.eth": "0.1.5",
+    "goerli-besu.dnp.dappnode.eth": "0.1.1",
+    "web3signer-prater.dnp.dappnode.eth": "0.1.18"
   };
 
 const lodestarStakersGnosisMinimumVersions: ExecutionClientOrSignerVersion<"gnosis"> =
   {
-    "nethermind-xdai.dnp.dappnode.eth": "1.0.31", // Needs publishment
+    "nethermind-xdai.dnp.dappnode.eth": "1.0.29", // Needs publishment
     "web3signer-gnosis.dnp.dappnode.eth": "0.1.13" // Needs publishment
   };
 
 const lodestarStakersMainnetMinimumVersions: ExecutionClientOrSignerVersion<"mainnet"> =
   {
-    "nethermind.public.dappnode.eth": "1.0.32",
+    "nethermind.public.dappnode.eth": "1.0.35", // Not working
     "geth.dnp.dappnode.eth": "0.1.38", // Needs publishment
-    "erigon.dnp.dappnode.eth": "0.1.37",
+    "erigon.dnp.dappnode.eth": "0.1.38",
     "besu.public.dappnode.eth": "1.2.7", // Needs publishment
     "web3signer.dnp.dappnode.eth": "0.1.7" // Needs publishment
   };
