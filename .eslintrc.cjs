e7303d278dc5dc3e28cb693b10695429519b8014
dsimog01 <dsimog01@estudiantes.unileon.es>
2023-04-11 15:49:24 +0000

    Fix ignore patterns path

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,24 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
-    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
   // "extends": ["plugin:prettier/recommended","eslint:recommended"],
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: "module" // Allows for the use of imports
+    sourceType: "module", // Allows for the use of imports
   },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: [
+    "packages/dappmanager/src/modules/chains/drivers/bitcoin.ts",
+    "packages/dappmanager/src/modules/chains/drivers/monero.ts",
+    "packages/dappmanager/src/modules/ipfs/writeStreamToFs.ts",
+  ],
   env: {
     node: true,
     mocha: true,
-    es6: true
+    es6: true,
   },
-  ignorePatterns: [
-    "src/modules/chains/drivers/bitcoin.ts",
-    "src/modules/chains/drivers/monero.ts",
-    "src/modules/ipfs/writeStreamToFs.ts"
-  ],
   rules: {
     "max-len": ["error", 1000],
     // ##### Some libraries do not have typings and the compiler does not understand .d.ts files
@@ -25,6 +26,6 @@ module.exports = {
     // ##### typescript does not understand hoisting
     "@typescript-eslint/no-use-before-define": "off",
     // ### This project uses docker-compose which has many variables not in camelCase
-    "@typescript-eslint/camelcase": "off"
-  }
+    "@typescript-eslint/camelcase": "off",
+  },
 };
