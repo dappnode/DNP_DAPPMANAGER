7e0123c27a27a9bc796fb016c87318d1f7d0c891
dsimog01 <dsimog01@estudiantes.unileon.es>
2023-04-25 14:07:18 +0000

    Added check for IP regex

--- packages/dappmanager/hostScripts/set_static_ip.sh
+++ packages/dappmanager/hostScripts/set_static_ip.sh
@@ -3,21 +3,6 @@
 # Exit on error
 set -e
 
-# Check if nmcli exists
-if ! type nmcli >/dev/null 2>&1; then
-  echo "The nmcli command is not available. Attempting to install NetworkManager..."
-  
-  # Update package list and install NetworkManager
-  sudo apt-get update
-  sudo apt-get install -y network-manager
-
-  # Check if nmcli is available after the installation
-  if ! type nmcli >/dev/null 2>&1; then
-    echo "NetworkManager installation failed. Please install NetworkManager manually and try again."
-    exit 1
-  fi
-fi
-
 # Check if the required argument is provided
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <STATIC_IP>"
@@ -25,3 +10,19 @@ if [ "$#" -ne 1 ]; then
 fi
 
 STATIC_IP="$1"
+
+if ! [[ ${STATIC_IP} =~ ^[0-9]{1,3}(\.[0-9]{1,3}){3}$ ]]; then
+    echo "Invalid IPv4 address format. Please provide a valid IPv4 address."
+    exit 1
+fi
+
+
+# Get the default Ethernet interface name
+INTERFACE_NAME=$(ip -o -4 route show default | awk '{print $5}')
+
+# TODO: Add the code here to set the static IP in the host machine (any Linux distribution)
+# Disable DHCP if it is enabled
+# Make the script as compatible as possible with all Linux distros
+
+echo "Static IP address ${STATIC_IP} has been configured for the default Ethernet interface (${INTERFACE_NAME})."
+exit 0
