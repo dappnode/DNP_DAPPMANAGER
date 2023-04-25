f1766a87c110db87796ea9bab79a6887d4dd3fb7
dsimog01 <dsimog01@estudiantes.unileon.es>
2023-04-25 14:13:15 +0000

    Handled Debian and RHEL based OS

--- packages/dappmanager/hostScripts/set_static_ip.sh
+++ packages/dappmanager/hostScripts/set_static_ip.sh
@@ -16,13 +16,38 @@ if ! [[ ${STATIC_IP} =~ ^[0-9]{1,3}(\.[0-9]{1,3}){3}$ ]]; then
     exit 1
 fi
 
-
 # Get the default Ethernet interface name
 INTERFACE_NAME=$(ip -o -4 route show default | awk '{print $5}')
 
-# TODO: Add the code here to set the static IP in the host machine (any Linux distribution)
-# Disable DHCP if it is enabled
-# Make the script as compatible as possible with all Linux distros
+# Function to update the Debian-based system's configuration
+update_debian() {
+  NETWORK_CONFIG_PATH="/etc/network/interfaces"
+
+  # TODO: Check if DHCP is enabled and disable it
+  # TODO: Edit the config file to set the static IP address
+}
+
+# Function to update the RHEL-based system's configuration
+update_rhel() {
+  NETWORK_CONFIG_PATH="/etc/sysconfig/network-scripts/ifcfg-${INTERFACE_NAME}"
+  # TODO: Check if DHCP is enabled and disable it
+  # TODO: Edit the config file to set the static IP address
+}
+
+# Check if the Debian-based system's configuration file exists
+if [ -f "/etc/network/interfaces" ]; then
+    update_debian
+    sudo systemctl restart networking
+    echo "Debian-based system updated."
+# Check if the RHEL-based system's configuration file exists
+elif [ -f "/etc/sysconfig/network-scripts/ifcfg-${INTERFACE_NAME}" ]; then
+    update_rhel
+    sudo systemctl restart network
+    echo "RHEL-based system updated."
+else
+    echo "Unsupported system or network configuration file not found."
+    exit 1
+fi
 
 echo "Static IP address ${STATIC_IP} has been configured for the default Ethernet interface (${INTERFACE_NAME})."
 exit 0
