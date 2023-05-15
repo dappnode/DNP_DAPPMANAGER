ec3334d7029632668da3fed61faca16c63dc9553
Pablo Mendez <mendez4a@gmail.com>
2023-05-15 10:59:10 +0200

    set root alias to monoservice pkg

--- packages/dappmanager/src/modules/compose/setDappnodeComposeDefaults.ts
+++ packages/dappmanager/src/modules/compose/setDappnodeComposeDefaults.ts
@@ -26,6 +26,7 @@ export function setDappnodeComposeDefaults(
   const dnpName = manifest.name;
   const version = manifest.version;
   const isCore = getIsCore(manifest);
+  const isMonoService = getIsMonoService(composeUnsafe);
 
   return cleanCompose({
     version: ensureMinimumComposeVersion(composeUnsafe.version),
@@ -58,7 +59,8 @@ export function setDappnodeComposeDefaults(
           networks: setServiceNetworksWithAliases(serviceUnsafe.networks, {
             serviceName,
             dnpName,
-            isMain: manifest.mainService === serviceName
+            // The root pkg alias will be added to the main service or if it is a mono service
+            isMain: isMonoService || manifest.mainService === serviceName
           })
         });
       }
@@ -148,4 +150,11 @@ function setNetworks(
  */
 function sortServiceKeys(service: ComposeService): ComposeService {
   return fromPairs(sortBy(toPairs(service), "0")) as ComposeService;
+}
+
+/**
+ * Returns true if there is only one service in the compose file
+ */
+function getIsMonoService(compose: Compose): boolean {
+  return Object.keys(compose.services).length === 1;
 }
