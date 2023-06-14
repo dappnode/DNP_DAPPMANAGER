fbaf94b6d0068a68e025e1e16ced4de19c9b928c
pablomendezroyo <mendez4a@gmail.com>
2023-06-14 11:41:20 +0200

    remove tests

--- packages/dappmanager/test/integration/hostScripts.test.int.ts
+++ packages/dappmanager/test/integration/hostScripts.test.int.ts
@@ -5,20 +5,6 @@ import shell from "../../src/utils/shell.js";
 const hostScriptsPath = process.cwd() + "/hostScripts";
 
 describe("Host scripts", () => {
-  it("Should fetch docker engine update requirements", async () => {
-    const dockerEngineUpdateRequirements = await shell(
-      `sudo bash ${hostScriptsPath}/docker_engine_update.sh --print-host-info`
-    );
-    expect(dockerEngineUpdateRequirements).to.be.ok;
-  });
-
-  it("Should fetch docker compose update requirements", async () => {
-    const dockerComposeUpdateRequirements = await shell(
-      `sudo bash ${hostScriptsPath}/docker_compose_update.sh --version`
-    );
-    expect(dockerComposeUpdateRequirements).to.be.ok;
-  });
-
   it("Should fetch host info", async () => {
     const hostInfo = await shell(
       `sudo bash ${hostScriptsPath}/collect_host_info.sh`
