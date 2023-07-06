645cf6ad1f0f45910ef985437d0f6ebf75f95995
pablomendezroyo <mendez4a@gmail.com>
2023-07-06 11:36:40 +0200

    add js extension

--- packages/dappmanager/src/modules/stakerConfig/set/setMevBoost.ts
+++ packages/dappmanager/src/modules/stakerConfig/set/setMevBoost.ts
@@ -6,7 +6,7 @@ import {
 } from "@dappnode/common";
 import { MevBoostMainnet, MevBoostPrater, Network } from "@dappnode/types";
 import { packageInstall, packageSetEnvironment } from "../../../calls/index.js";
-import { logs } from "../../../logs";
+import { logs } from "../../../logs.js";
 import { dockerComposeUpPackage } from "../../docker/index.js";
 import { stopAllPkgContainers } from "./stopAllPkgContainers.js";
 import * as db from "../../../db/index.js";
