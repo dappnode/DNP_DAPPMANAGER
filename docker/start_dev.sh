cfdc858a9964d5f9c9862a81e5043801dab31805
pablomendezroyo <mendez4a@gmail.com>
2023-07-07 10:41:27 +0200

    fix dev mode

--- docker/start_dev.sh
+++ docker/start_dev.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
+# Copy from the /tmp-app to /app to overwrite the node_modules and builds
+# this is needed because the docker volume is mounted after the docker build
+# and the node_modules and builds are not available in the docker build
+cp -r /tmp-app/* /app/
+
 # link important directories
 ln -s /app/packages/admin-ui/build/ /app/packages/dappmanager/dist
 ln -s /usr/src/app/dnp_repo/ /app/packages/dappmanager
 ln -s /usr/src/app/DNCORE/ /app/packages/dappmanager
-
-# Install lerna first
-rm -rf node_modules
-yarn --frozen-lockfile --non-interactive --ignore-scripts --ignore-optional
-yarn
-
-# Build common
-cd /app/packages/common/ && rm -rf node_modules && yarn && yarn generate && yarn build
-
-# Build admin-ui
-cd /app/packages/admin-ui/ && rm -rf node_modules && yarn && yarn build
-
-# Build dappmanager
-cd /app/packages/dappmanager/ && rm -rf node_modules && yarn && yarn build
 
 # execute the scripts in background and wait
 cd /app/packages/dappmanager && yarn dev &
