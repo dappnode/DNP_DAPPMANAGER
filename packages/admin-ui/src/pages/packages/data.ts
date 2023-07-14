689c339dbdbea11a0d4a0bc42f43ec435af2a095
pablomendezroyo <mendez4a@gmail.com>
2023-07-14 14:49:09 +0200

    add subpath my

--- packages/admin-ui/src/pages/packages/data.ts
+++ packages/admin-ui/src/pages/packages/data.ts
@@ -1,6 +1,6 @@
 // This will be used later in our root reducer and selectors
-export const relativePath = "packages"; // default redirect to packages
-export const rootPath = relativePath + "/*";
+export const relativePath = "packages/my"; // default redirect to packages
+export const rootPath = "packages/*";
 export const title = "Packages";
 
 // SubPaths
