757c7fd02d9d2c0683d183aa162a436ef5c83805
pablomendezroyo <mendez4a@gmail.com>
2023-09-19 18:09:03 +0200

    add modules ui to db

--- packages/dappmanager/src/db/ui.ts
+++ packages/dappmanager/src/db/ui.ts
@@ -1,7 +1,13 @@
 import { dbMain } from "./dbFactory.js";
-import { NewFeatureId, NewFeatureStatus } from "@dappnode/common";
+import {
+  NewFeatureId,
+  NewFeatureStatus,
+  UiModuleId,
+  UiModuleStatus
+} from "@dappnode/common";
 
 const NEW_FEATURE_STATUS = "new-feature-status";
+const UI_MODULES = "ui-modules";
 
 export const newFeatureStatus = dbMain.indexedByKey<
   NewFeatureStatus,
@@ -9,4 +15,9 @@ export const newFeatureStatus = dbMain.indexedByKey<
 >({
   rootKey: NEW_FEATURE_STATUS,
   getKey: featureId => featureId
+});
+
+export const uiModules = dbMain.indexedByKey<UiModuleStatus, UiModuleId>({
+  rootKey: UI_MODULES,
+  getKey: moduleId => moduleId
 });
