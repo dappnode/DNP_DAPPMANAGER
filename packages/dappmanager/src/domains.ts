178074f7b16448e01f00e0a3d2b4c423feadfec9
pablomendezroyo <mendez4a@gmail.com>
2023-09-26 14:32:00 +0200

    Add determineNetworkAlias

--- packages/dappmanager/src/domains.ts
+++ packages/dappmanager/src/domains.ts
@@ -1,38 +1,65 @@
 import { params } from "@dappnode/params";
 import { getContainerDomain } from "@dappnode/types";
 
-export function stripCharacters(s: string): string {
-  return s.replace(RegExp("_", "g"), "");
-}
+export type ContainerNames = { serviceName: string; dnpName: string };
 
-export function stripBadDomainChars(s: string): string {
-  // eslint-disable-next-line no-useless-escape
-  return s.replace(/[^a-zA-Z\-]+/g, "-");
+/**
+ * This function return fullNetworkAlias or shortNetworkAlias:
+ * - shortDnpName is dnpName.split(0)["."]. i.e dnpName = "dappmanager.dnp.dappnode.eth" => shortDnpName = "dappmanager"
+ *
+ * - i.e fullNetworkAlias: (serviceName..dappnode) "dappmanager.dnp.dappnode.eth.dappmanager.dappnode"
+ * - i.e shortNetworkAlias: () "dappmanager.dappnode"
+ *
+ * - For dnp packages ( example.dnp.dappnode.eth). Whether it's a mono or multi-service package:
+ *   - Each service should have an alias format of <service_name>.example.dappnode.
+ *   - If it's the main service, also include example.dappnode.
+ *
+ * - For public packages ( example.public.dappnode.eth). Whether it's a mono or multi-service package:
+ *   - Each service should have an alias format of <service_name>.example.public.dappnode.
+ *   - If it's the main service, also include example.public.dappnode.
+ *
+ * @param dnpName dappnode dnp name
+ * @param serviceName container service name
+ * @param isMainOrMonoservice IF true returns the full network alias, otherwise the short network alias
+ * @param isExternal is the container external
+ * @param short if true, it will return the short version of the dnpName otherwise the full network alias
+ * @returns the root private network alias. It
+ */
+export function determineNetworkAlias({
+  dnpName,
+  serviceName,
+  isMainOrMonoservice,
+  isExternal
+}: {
+  dnpName: string;
+  serviceName: string;
+  isMainOrMonoservice: boolean;
+  isExternal: boolean;
+}): string {
+  const endDomain = isExternal ? "dappnode.external" : "dappnode";
+
+  return isMainOrMonoservice
+    ? `${getShortDnpName(dnpName)}.${endDomain}`
+    : `${serviceName}.${getShortDnpName(dnpName)}.${endDomain}`;
 }
 
 /**
- * - Strip container prefix
- * - Strip .dappnode, .eth, .dnp
- * - Strip "_"
- *
- * @param name "bitcoin.dnp.dappnode.eth"
- * @returns "bitcoin"
- * - "bitcoin.dnp.dappnode.eth" > "bitcoin"
- * - "other.public.dappnode.eth" > "other.public"
+ * Get the private network alias.
+ * if dnpName === serviceName || !serviceName => dnpName.split(".")[0].dappnode
+ * else serviceName.dnpName.split(".").dappnode
+ * @param container
+ * @returns
  */
-export function shortUniqueDappnodeEns(dnpName: string): string {
-  for (const s of [".dnp.dappnode.eth", ".dappnode.eth", ".eth"])
-    if (dnpName.endsWith(s)) dnpName = dnpName.slice(0, -s.length);
-  return stripCharacters(dnpName);
-}
-
-export type ContainerNames = { serviceName: string; dnpName: string };
-
 export function getPrivateNetworkAlias(container: ContainerNames): string {
   const fullEns = getContainerDomain(container);
-  return `${shortUniqueDappnodeEns(fullEns)}.dappnode`;
+  return `${getShortDnpName(fullEns)}.dappnode`;
 }
 
+/**
+ * It return an array of aliases which includes at lease the private network alias
+ * @param container
+ * @returns
+ */
 export function getPrivateNetworkAliases(
   container: ContainerNames & { isMain: boolean }
 ): string[] {
@@ -56,10 +83,39 @@ export function getPrivateNetworkAliases(
 
 export function getExternalNetworkAlias(container: ContainerNames): string {
   const fullEns = getContainerDomain(container);
-  return `${shortUniqueDappnodeEns(fullEns)}.external`;
+  return `${getShortDnpName(fullEns)}.external`;
 }
 
 export function getPublicSubdomain(container: ContainerNames): string {
   const fullEns = getContainerDomain(container);
-  return stripBadDomainChars(shortUniqueDappnodeEns(fullEns));
+  return stripBadDomainChars(getShortDnpName(fullEns));
+}
+
+/**
+ * UTILS
+ */
+
+export function stripCharacters(s: string): string {
+  return s.replace(RegExp("_", "g"), "");
+}
+
+export function stripBadDomainChars(s: string): string {
+  // eslint-disable-next-line no-useless-escape
+  return s.replace(/[^a-zA-Z\-]+/g, "-");
+}
+
+/**
+ * - Strip container prefix
+ * - Strip .dappnode, .eth, .dnp
+ * - Strip "_"
+ *
+ * @param name "bitcoin.dnp.dappnode.eth"
+ * @returns "bitcoin"
+ * - "bitcoin.dnp.dappnode.eth" > "bitcoin"
+ * - "other.public.dappnode.eth" > "other.public"
+ */
+function getShortDnpName(dnpName: string): string {
+  for (const s of [".dnp.dappnode.eth", ".dappnode.eth", ".eth"])
+    if (dnpName.endsWith(s)) dnpName = dnpName.slice(0, -s.length);
+  return stripCharacters(dnpName);
 }
--- packages/dappmanager/src/modules/optimism/getOptimismConfig.ts
+++ packages/dappmanager/src/modules/optimism/getOptimismConfig.ts
@@ -26,39 +26,46 @@ export async function getOptimismConfig(): Promise<OptimismConfigGet> {
 
     return {
       executionClients: await Promise.all(
-        executionClientsOptimism.map(async execClient => {
-          try {
-            if (!(await releaseFetcher.repoExists(execClient)))
-              throw Error(`Repository ${execClient} does not exist`);
+        executionClientsOptimism
+          .filter(name => name === "op-geth.dnp.dappnode.eth")
+          .map(async execClient => {
+            try {
+              // if (!(await releaseFetcher.repoExists(execClient)))
+              //   throw Error(`Repository ${execClient} does not exist`);
 
-            const pkgData = await getPkgData(releaseFetcher, execClient);
+              const hash =
+                "/ipfs/QmQB881QkvvSZ84yHEKxtiC4V2Wge4ynQ6rm3PUYrHPPrM";
 
-            return {
-              status: "ok",
-              dnpName: execClient,
-              avatarUrl: fileToGatewayUrl(pkgData.avatarFile),
-              isInstalled: getIsInstalled(pkgData, dnpList),
-              isUpdated: getIsUpdated(pkgData, dnpList),
-              isRunning: getIsRunning(pkgData, dnpList),
-              data: pkgData,
-              isSelected: execClient === currentOptimismExecutionClient,
-              enableHistorical
-            };
-          } catch (error) {
-            return {
-              status: "error",
-              dnpName: execClient,
-              error,
-              enableHistorical
-            };
-          }
-        })
+              const pkgData = await getPkgData(releaseFetcher, hash);
+
+              return {
+                status: "ok",
+                dnpName: execClient,
+                avatarUrl: fileToGatewayUrl(pkgData.avatarFile),
+                isInstalled: getIsInstalled(pkgData, dnpList),
+                isUpdated: getIsUpdated(pkgData, dnpList),
+                isRunning: getIsRunning(pkgData, dnpList),
+                data: pkgData,
+                isSelected: execClient === currentOptimismExecutionClient,
+                enableHistorical
+              };
+            } catch (error) {
+              return {
+                status: "error",
+                dnpName: execClient,
+                error,
+                enableHistorical
+              };
+            }
+          })
       ),
       rollup: await new Promise<OptimismItem<"rollup">>(resolve => {
         (async () => {
           try {
-            if (!(await releaseFetcher.repoExists(optimismNode)))
-              throw Error(`Repository ${optimismNode} does not exist`);
+            // if (!(await releaseFetcher.repoExists(optimismNode)))
+            //   throw Error(`Repository ${optimismNode} does not exist`);
+
+            const hash = "/ipfs/QmVJV1ZbDyTNDReeNu6ykQfUXZ4SKXmLFiqULQ3S1JYcXG";
 
             const pkgData = await getPkgData(releaseFetcher, optimismNode);
             const mainnetRpcUrl = getOptimismNodeRpcUrlIfExists();
@@ -88,10 +95,12 @@ export async function getOptimismConfig(): Promise<OptimismConfigGet> {
       archive: await new Promise<OptimismItem<"archive">>(resolve => {
         (async () => {
           try {
-            if (!(await releaseFetcher.repoExists(optimismL2Geth)))
-              throw Error(`Repository ${optimismL2Geth} does not exist`);
+            // if (!(await releaseFetcher.repoExists(optimismL2Geth)))
+            //   throw Error(`Repository ${optimismL2Geth} does not exist`);
 
-            const pkgData = await getPkgData(releaseFetcher, optimismL2Geth);
+            const hash = "/ipfs/QmRdMuBf1iLqv9eRMAL6QFsEkWcvM73ZKfAcLPfUXGnbY7";
+
+            const pkgData = await getPkgData(releaseFetcher, hash);
             const isRunning = getIsRunning(pkgData, dnpList);
             resolve({
               status: "ok",
