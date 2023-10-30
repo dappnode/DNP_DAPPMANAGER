ff9bb213c55c0a591636f9204e7850c78a8c4370
Pablo Mendez <mendez4a@gmail.com>
2023-10-30 18:10:42 +0100

    remove unused code from dappmanager pkg

--- packages/dappmanager/src/api/middlewares/ethForward/utils/index.ts
+++ packages/dappmanager/src/api/middlewares/ethForward/utils/index.ts
@@ -2,4 +2,3 @@ export * from "./decodeContent.js";
 export * from "./decodeContentHash.js";
 export * from "./decodeDnsLink.js";
 export * from "./isEmpty.js";
-export * from "./sanitizeIpfsPath.js";
