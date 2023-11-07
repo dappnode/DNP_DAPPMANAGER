34c1a55b0bf1e8cfd7aeb1e5b937c1885a4f33f1
pablomendezroyo <mendez4a@gmail.com>
2023-11-07 09:58:02 +0100

    added frontend

---
+++ packages/admin-ui/src/pages/system/components/Advanced/UpdateUpgrade.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { api } from "api";
+import Card from "components/Card";
+import Button from "components/Button";
+import { confirm } from "components/ConfirmDialog";
+import { withToastNoThrow } from "components/toast/Toast";
+
+export default function UpdateUpgrade() {
+  async function updateUpgrade() {
+    await new Promise<void>(resolve =>
+      confirm({
+        title: `Updating and upgrading the host machine`,
+        text: `This action might update docker among other packages, you might loose connectivity to the dappnode.`,
+        label: "Update and upgrade",
+        onClick: resolve
+      })
+    );
+
+    await withToastNoThrow(() => api.cleanCache(), {
+      message: "Updating and upgrading...",
+      onSuccess: "Updated and upgraded"
+    });
+  }
+  return (
+    <Card spacing>
+      <p>
+        Update and upgrade the host machine. This action update and upgrade the
+        list of packages of the host machine. This action might require a reboot
+        of the host machine, you will receive a notification if so.
+      </p>
+
+      <Button variant="outline-danger" onClick={updateUpgrade}>
+        Update and upgrade
+      </Button>
+    </Card>
+  );
+}
