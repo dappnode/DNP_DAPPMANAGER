bebb380ddd91a411860bb8660cbbb555320b8e8a
pablomendezroyo <mendez4a@gmail.com>
2023-11-21 12:11:03 +0100

    Add schemas as internal module

---
+++ packages/schemas/test/testUtils.ts
@@ -0,0 +1,8 @@
+import fs from "fs";
+import { rimraf } from "rimraf";
+
+export const testDir = "test_files";
+export function cleanTestDir(): void {
+  rimraf.sync(testDir);
+  fs.mkdirSync(testDir, { recursive: true });
+}
