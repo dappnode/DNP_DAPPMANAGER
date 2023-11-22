a044feaa8c64787221046d480c196d2707514921
pablomendezroyo <mendez4a@gmail.com>
2023-11-22 09:15:07 +0100

    fix admin ui imports

--- packages/admin-ui/src/__mock-backend__/data/dnpInstalled.ts
+++ packages/admin-ui/src/__mock-backend__/data/dnpInstalled.ts
@@ -2,7 +2,12 @@ import { InstalledPackageDetailData, PackageContainer } from "@dappnode/common";
 import { sampleContainer, sampleDnp } from "./sample";
 import { MockDnp } from "./dnps/types";
 import { mockDnps } from "./dnps";
-import { getImageTag, getContainerName } from "@dappnode/utils";
+
+// The functions: getContainerName, getContainerDomain and getImageTag are utility functions from the module @dappnode/utils
+// they are used in the server mock, and to avoid compiling issues due to not able to use node modules in the browser
+// they are copied here
+
+// TODO: either export them from uitls in a subpath separately or find a way to use them in the browser without duplicating them
 
 function getInstalledDnp(dnp: MockDnp): InstalledPackageDetailData {
   const dnpName = dnp.metadata.name;
@@ -59,5 +64,47 @@ function getInstalledDnp(dnp: MockDnp): InstalledPackageDetailData {
     ...dnp.installedData
   };
 }
+
+const getContainerName = ({
+  dnpName,
+  serviceName,
+  isCore
+}: {
+  dnpName: string;
+  serviceName: string;
+  isCore: boolean;
+}): string =>
+  // Note: _PREFIX variables already end with the character "-"
+  [
+    isCore ? "DAppNodeCore-" : "DAppNodePackage-",
+    getContainerDomain({ dnpName, serviceName })
+  ].join("");
+
+const getContainerDomain = ({
+  dnpName,
+  serviceName
+}: {
+  serviceName?: string;
+  dnpName: string;
+}): string => {
+  return !serviceName || serviceName === dnpName
+    ? dnpName
+    : `${serviceName}.${dnpName}`;
+};
+
+const getImageTag = ({
+  dnpName,
+  serviceName,
+  version
+}: {
+  dnpName: string;
+  serviceName: string;
+  version: string;
+}): string => {
+  if (!version) throw new Error("Version is required");
+  if (!dnpName) throw new Error("DAppNode package name is required");
+  if (!serviceName) throw new Error("Service name is required");
+  return [getContainerDomain({ dnpName, serviceName }), version].join(":");
+};
 
 export const dnpInstalled = mockDnps.map(getInstalledDnp);
