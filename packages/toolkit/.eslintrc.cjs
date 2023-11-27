c52d233c24b319c2404234b7c0c34412f7f8e090
pablomendezroyo <mendez4a@gmail.com>
2023-11-27 16:32:19 +0100

    ignore the graph

--- packages/toolkit/.eslintrc.cjs
+++ packages/toolkit/.eslintrc.cjs
@@ -2,4 +2,6 @@ module.exports = {
   extends: [
     "../../.eslintrc.cjs", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
-}
+  // ignore the directory theGraph
+  ignorePatterns: ["theGraph"],
+};
