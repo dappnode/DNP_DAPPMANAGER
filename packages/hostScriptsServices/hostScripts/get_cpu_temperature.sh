28e1f181e191f51e72f5d7fd404e7a16f63e638f
dappnode <dappnode@dappnode.io>
2023-11-28 14:34:37 +0100

    add doc

--- packages/hostScriptsServices/hostScripts/get_cpu_temperature.sh
+++ packages/hostScriptsServices/hostScripts/get_cpu_temperature.sh
@@ -3,6 +3,9 @@
 THERMAL_ZONE_DIRS="/sys/class/thermal/thermal_zone*"
 HWMON_PATH="/sys/devices/pci0000:00/*/hwmon/hwmon*/temp1_input"
 
+# The directory /sys/class/thermal/thermal_zone* contains the temperature values of some sensors
+# The paste command search for that values and print the values and the type corresponding
+# Using grep, takes only the temperature of the cpu
 #Iterate through paths
 for path in $THERMAL_ZONE_DIRS;do
     if [ -e "$path/type" ] && [ -e "$path/temp" ]; then
@@ -12,14 +15,16 @@ for path in $THERMAL_ZONE_DIRS;do
     fi
 done
 
+
+# If the first directory does not exist, search in another that contains the value of the cpu temp in a file
 #Iterate through paths
 for path in $HWMON_PATH;do
     if [ -e "$path" ]; then
-        temp=$(cat /sys/devices/pci0000:00/*/hwmon/hwmon*/temp1_input | sed 's/\(.\)..$/.\1/')
+        temp=$(cat $path | sed 's/\(.\)..$/.\1/')
         echo $temp
         exit 0
     fi
 done
 
-#If cannot get the temperature
+#In some cases, neither directory exists, cannot get temperature
 exit 1
