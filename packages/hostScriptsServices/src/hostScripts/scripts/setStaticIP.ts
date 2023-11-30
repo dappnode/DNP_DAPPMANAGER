7fa412cab89a02da44b3fb59841a13f87748449c
dappnode <dappnode@dappnode.io>
2023-11-30 15:34:30 +0100

    backend to set static ip

---
+++ packages/hostScriptsServices/src/hostScripts/scripts/setStaticIP.ts
@@ -0,0 +1,8 @@
+import { runScript } from "../runScripts.js";
+
+/**
+ * Set a static IP address of the host machine
+ */
+export async function setStaticIP(IpAddress: string): Promise<void> {
+    await runScript("set_static_ip.sh", IpAddress);
+}
