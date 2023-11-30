9a24788c4c532e8622700f3c440c197cb8e7473d
pablomendezroyo <mendez4a@gmail.com>
2023-11-30 11:56:39 +0100

    skip host uptime test

--- packages/dappmanager/test/unit/calls/getHostUptime.test.ts
+++ packages/dappmanager/test/unit/calls/getHostUptime.test.ts
@@ -2,7 +2,7 @@ import "mocha";
 import { expect } from "chai";
 import { getHostUptime } from "../../../src/calls/getHostUptime.js";
 
-describe("Call function: getHostUptime", function () {
+describe.skip("Call function: getHostUptime", function () {
   it("Should return the uptime of the host", async () => {
     const output = await getHostUptime();
     const uptimePattern =
