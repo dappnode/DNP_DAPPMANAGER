f2918df2baa993d0aa02b44931dc37947563c60c
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2023-12-05 10:31:47 +0100

    fix hostuptime memo (#1723)

--- packages/dappmanager/src/calls/getHostUptime.ts
+++ packages/dappmanager/src/calls/getHostUptime.ts
@@ -5,19 +5,22 @@ import memoize from "memoizee";
 /**
  * Returns uptime of the host
  */
-export const getHostUptime = memoize(
-  async function getHostUptime(): Promise<string> {
-    try {
-      const output = await shellHost("uptime -- -p");
-      return output;
-    } catch (error) {
-      error.message += `Unable to retrieve the uptime from te host machine: ${error.message}`;
-      logs.error(error.message);
-      throw error;
-    }
-  },
-  {
-    promise: true,
-    maxAge: 60 * 1000 * 5 // 5 minutes
+export async function getHostUptime(): Promise<string> {
+  return await getHostUptimeMemo();
+}
+
+async function _getHostUptime(): Promise<string> {
+  try {
+    const output = await shellHost("uptime -- -p");
+    return output;
+  } catch (error) {
+    error.message += `Unable to retrieve the uptime from te host machine: ${error.message}`;
+    logs.error(error.message);
+    throw error;
   }
-);
+}
+
+const getHostUptimeMemo = memoize(_getHostUptime, {
+  promise: true,
+  maxAge: 60 * 1000 * 5 // 5 minutes
+});
