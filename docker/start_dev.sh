502741c53c83cb5fb8ea73fefc5e07c67da7a77d
pablomendezroyo <mendez4a@gmail.com>
2023-12-11 11:20:56 +0100

    execute dev in every submodule

--- docker/start_dev.sh
+++ docker/start_dev.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Copy from the /tmp-app to /app to overwrite the node_modules and builds
-# this is needed because the docker volume is mounted after the docker build
-# and the node_modules and builds are not available in the docker build
-cp -r /tmp-app/* /app/
-
 # link important directories
 ln -s /app/packages/admin-ui/build/ /app/packages/dappmanager/dist
 ln -s /usr/src/app/dnp_repo/ /app/packages/dappmanager
 ln -s /usr/src/app/DNCORE/ /app/packages/dappmanager
 
-# execute the scripts in background and wait
-yarn dev &
+# execute the scripts in background
+for dir in /app/packages/*; do
+    if [ -d "$dir" ]; then
+        cd $dir
+        yarn dev &
+    fi
+done
+
+# wait for all processes to finish
 wait
