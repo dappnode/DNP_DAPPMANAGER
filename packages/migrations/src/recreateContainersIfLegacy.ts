33599d923638aa0ac843d8e0013dd2cad272f1f5
pablomendezroyo <mendez4a@gmail.com>
2023-12-20 10:20:54 +0100

    fix recreate containers

--- packages/migrations/src/recreateContainersIfLegacy.ts
+++ packages/migrations/src/recreateContainersIfLegacy.ts
@@ -1,23 +1,23 @@
 import { logs } from "@dappnode/logger";
 import { listPackages, docker, dockerComposeUp } from "@dappnode/dockerapi";
 import { getDockerComposePath } from "@dappnode/utils";
+import { params } from "@dappnode/params";
 
 /**
  * Recreate docker containers if legacy dns is still set in runtime.
  * Skip for dappmanager container to avoid dangerous restart loops
  */
 export async function recreateContainersIfLegacyDns(): Promise<void> {
-  const legacyDns = "172.33.1.2";
-
   const pkgs = await listPackages();
 
   await Promise.all(
     pkgs.map(async (pkg) => {
       const hasLegacyDns = pkg.containers.some(async (c) => {
         try {
-          return (
-            await docker.getContainer(c.containerName).inspect()
-          ).HostConfig.Dns?.includes(legacyDns);
+          const dns = (await docker.getContainer(c.containerName).inspect())
+            .HostConfig.Dns;
+          if (dns && dns.includes(params.DOCKER_LEGACY_DNS)) return true;
+          return false;
         } catch (e) {
           // continue with migration on error
           logs.error(`Error inspecting container ${c.containerName}: ${e}`);
@@ -26,8 +26,8 @@ export async function recreateContainersIfLegacyDns(): Promise<void> {
       });
 
       if (hasLegacyDns) {
-        logs.info(`pkg ${pkg.dnpName} has legacy dns, restarting it...`);
         // containers must be recreated
+        logs.info(`pkg ${pkg.dnpName} has legacy dns, restarting it...`);
         await dockerComposeUp(getDockerComposePath(pkg.dnpName, pkg.isCore));
       }
     })
