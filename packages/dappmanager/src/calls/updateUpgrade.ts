cbb3ea1c0f9bb7b8d7be698d4e8a23c20453bba1
pablomendezroyo <mendez4a@gmail.com>
2023-12-26 17:43:25 +0100

    fix typo

--- packages/dappmanager/src/calls/updateUpgrade.ts
+++ packages/dappmanager/src/calls/updateUpgrade.ts
@@ -1,8 +1,8 @@
-import { checkDockerNetwork } from "@dappnode/hostscriptsservices";
+import { updateUpgradeHost } from "@dappnode/hostscriptsservices";
 
 /**
  * Updates and upgrades the host machine
  */
 export async function updateUpgrade(): Promise<string> {
-  return await checkDockerNetwork();
+  return await updateUpgradeHost();
 }
