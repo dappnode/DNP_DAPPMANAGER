ae9d88b280a081afc0386abd8df98c1ee6f66750
pablomendezroyo <mendez4a@gmail.com>
2024-01-03 11:27:10 +0100

    fix typo int test

--- packages/dockerApi/test/int/getNetworkAliasesMap.int.test.ts
+++ packages/dockerApi/test/int/getNetworkAliasesMap.int.test.ts
@@ -1,6 +1,7 @@
 import "mocha";
 import { expect } from "chai";
-import { docker, getNetworkAliasesMapNotThrow } from "@dappnode/dockerapi";
+import { docker } from "../../src/index.js";
+import { getNetworkAliasesIpsMapNotThrow } from "../../src/api/network.js";
 
 describe("Ensure docker network config migration => getDockerNetworkNameFromSubnet", () => {
   const testNetworkName = "docker_network_test";
@@ -53,7 +54,6 @@ describe("Ensure docker network config migration => getDockerNetworkNameFromSubn
       return container;
     });
 
-
     // Connect containers to network
     await Promise.all(
       containerPromises.map(async (containerPromise) => {
@@ -72,7 +72,7 @@ describe("Ensure docker network config migration => getDockerNetworkNameFromSubn
   });
 
   it("should return a map of the containers and aliases connected to the test network", async () => {
-    const aliasMap = await getNetworkAliasesMapNotThrow(testNetworkName);
+    const aliasMap = await getNetworkAliasesIpsMapNotThrow(testNetworkName);
 
     expect(aliasMap.size).to.equal(testContainerNames.length);
     testContainerNames.forEach((containerName) => {
@@ -83,7 +83,6 @@ describe("Ensure docker network config migration => getDockerNetworkNameFromSubn
         `Container ${containerName} should have alias ${expectedAlias}`
       );
     });
-
   });
 
   after(async () => {
@@ -97,7 +96,7 @@ describe("Ensure docker network config migration => getDockerNetworkNameFromSubn
         const container = docker.getContainer(cn);
         if (container) {
           // eslint-disable-next-line @typescript-eslint/no-empty-function
-          await container.remove({ force: true }).catch(() => { });
+          await container.remove({ force: true }).catch(() => {});
         }
       })
     );
@@ -105,7 +104,7 @@ describe("Ensure docker network config migration => getDockerNetworkNameFromSubn
     // Remove docker network
     if (testNetwork) {
       // eslint-disable-next-line @typescript-eslint/no-empty-function
-      await testNetwork.remove().catch(() => { });
+      await testNetwork.remove().catch(() => {});
     }
   }
 });
