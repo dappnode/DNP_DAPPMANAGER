8d023578a8804d92ebf2bce73047367e18e4e6fc
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2024-01-15 11:01:14 +0100

    Add bind checker daemon (#1787)
    * add bind checker daemon
    
    * fix grammar
    
    * reuse disconnectConflictingContainerIfAny
    
    * set to 30 min

---
+++ packages/daemons/src/bind/ensureBindContainerIpAndRunning.ts
@@ -0,0 +1,69 @@
+import {
+  docker,
+  dockerComposeUp,
+  disconnectConflictingContainerIfAny,
+} from "@dappnode/dockerapi";
+import { logs } from "@dappnode/logger";
+import { params } from "@dappnode/params";
+import { getDockerComposePath } from "@dappnode/utils";
+import Dockerode from "dockerode";
+
+async function disconnectConflictingContainerAndStartBind(): Promise<void> {
+  const network = docker.getNetwork(params.DOCKER_PRIVATE_NETWORK_NAME);
+  const conflictingContainer = await disconnectConflictingContainerIfAny(
+    network,
+    params.BIND_IP
+  );
+  logs.info(`Starting ${params.bindContainerName} container`);
+  // The docker compose will start the container with the right IP
+  await dockerComposeUp(getDockerComposePath(params.bindDnpName, true));
+  // connect back the conflicting container to the network
+  if (conflictingContainer) {
+    logs.info(
+      `Connecting back ${conflictingContainer.Name} container to ${params.DOCKER_PRIVATE_NETWORK_NAME} network`
+    );
+    await network.connect({
+      Container: conflictingContainer.Name,
+    });
+  }
+}
+
+export async function ensureBindContainerIpAndRunning(): Promise<void> {
+  const isBindRunning = (
+    await docker.getContainer(params.bindContainerName).inspect()
+  ).State.Running;
+
+  // check if the bind container is running
+  if (!isBindRunning) {
+    logs.info(`${params.bindContainerName} container is not running`);
+    await disconnectConflictingContainerAndStartBind();
+  } else {
+    // check is connected to dncore_network
+    const isConnectedToNetwork = Object.values(
+      (
+        (await docker
+          .getNetwork(params.DOCKER_PRIVATE_NETWORK_NAME)
+          .inspect()) as Dockerode.NetworkInspectInfo
+      ).Containers ?? []
+    ).some((container) => container.Name === params.bindContainerName);
+
+    if (!isConnectedToNetwork) {
+      logs.info(
+        `${params.bindContainerName} container is not connected to ${params.DOCKER_PRIVATE_NETWORK_NAME} network`
+      );
+      await disconnectConflictingContainerAndStartBind();
+    } else {
+      // check it has the right IP
+      const hasRightIp =
+        (await docker.getContainer(params.bindContainerName).inspect())
+          .NetworkSettings.Networks[params.DOCKER_PRIVATE_NETWORK_NAME]
+          .IPAddress === params.BIND_IP;
+      if (hasRightIp) {
+        logs.info(`${params.bindContainerName} container has right IP`);
+      } else {
+        logs.info(`${params.bindContainerName} container has wrong IP`);
+        await disconnectConflictingContainerAndStartBind();
+      }
+    }
+  }
+}
