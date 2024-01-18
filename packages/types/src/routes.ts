97425faf3eeb9b95be49ba69983598746a9637da
pablomendezroyo <mendez4a@gmail.com>
2024-01-18 19:47:16 +0100

    set it as mandatory ethremote

--- packages/types/src/routes.ts
+++ packages/types/src/routes.ts
@@ -215,7 +215,7 @@ export interface Routes {
    */
   ethClientTargetSet: (kwargs: {
     target: Eth2ClientTarget;
-    ethRemoteRpc?: string;
+    ethRemoteRpc: string;
     sync?: boolean;
     useCheckpointSync?: boolean;
     deletePrevExecClient?: boolean;
