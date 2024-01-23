64e6c6ee2882979a6d55a84dcdabd0b574d54817
dappnode <dappnode@dappnode.io>
2024-01-23 19:32:21 +0100

    - More corrections

--- packages/migrations/src/addDappnodePeerToLocalIpfsNode.ts
+++ packages/migrations/src/addDappnodePeerToLocalIpfsNode.ts
@@ -1,13 +1,14 @@
 import { logs } from "@dappnode/logger";
 
+//Adds Dappnode IPFS peer to the local IPFS node for more stability when fetching IPFS content
 export async function addDappnodePeerToLocalIpfsNode() {
-  const IpfsPeer =
+  const dappnodeIpfsPeer =
     "/ip4/167.86.114.131/tcp/4001/p2p/QmfB6dT5zxUq1BXiXisgcZKYkvjywdDYBK5keRaqDKH633";
-  const IpfsAlias = "ipfs.dappnode";
+  const ipfsAlias = "ipfs.dappnode";
 
   logs.info("adding dappnode peer to local ipfs node");
   await fetch(
-    `http://${IpfsAlias}:5001/api/v0/swarm/peering/add?arg=${IpfsPeer}`,
+    `http://${ipfsAlias}:5001/api/v0/swarm/peering/add?arg=${dappnodeIpfsPeer}`,
     {
       method: "POST",
     }
