3c7a2ec54a2fce5f67a186d785d3b5ac45d214ed
dappnodedev <144998261+dappnodedev@users.noreply.github.com>
2024-02-20 10:32:39 +0100

    Fix support>activity broken path

--- packages/admin-ui/src/pages/support/data.ts
+++ packages/admin-ui/src/pages/support/data.ts
@@ -1,5 +1,5 @@
 // This will be used later in our root reducer and selectors
 export const relativePath = "support/auto-diagnose"; // default redirect to auto-diagnose
-export const rootPath = "support/*";
+export const rootPath = "support/";
 export const title = "Support";
 export const activityPath = `${rootPath}/activity`;
