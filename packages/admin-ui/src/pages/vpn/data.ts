d8b17de49acbb4bf4e4173e48fe3060975011a11
dappnodedev <144998261+dappnodedev@users.noreply.github.com>
2024-02-20 10:40:32 +0100

    Fix all paths with '*'

--- packages/admin-ui/src/pages/vpn/data.ts
+++ packages/admin-ui/src/pages/vpn/data.ts
@@ -1,14 +1,14 @@
 // This will be used later in our root reducer and selectors
 export const relativePath = "vpn/wireguard"; // default redirect to wireguard vpn
-export const rootPath = "vpn/*";
+export const rootPath = "vpn";
 export const title = "VPN";
 export const openVpnSubtitle = "Open-VPN";
 export const wireguardSubtitle = "Wireguard";
 
 // Subpaths
 export const subPaths = {
-  openVpn: "openvpn/*",
-  wireguard: "wireguard/*"
+  openVpn: "openvpn",
+  wireguard: "wireguard"
 };
 
 // Additional data
