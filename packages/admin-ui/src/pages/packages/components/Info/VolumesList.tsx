63236dca4a3dca0d58b8c39f0b25824d317f35b1
Giselle Chacon Nessi <53020124+GiselleNessi@users.noreply.github.com>
2024-03-11 09:19:00 +0100

    Bug Fix and Enhancement: Volume Size Calculation (#1875)
    * Remove redundant check for undefined volume sizes
    
    * Refactor volume size calculation to handle null or undefined values explicitly

--- packages/admin-ui/src/pages/packages/components/Info/VolumesList.tsx
+++ packages/admin-ui/src/pages/packages/components/Info/VolumesList.tsx
@@ -79,10 +79,8 @@ export const VolumesList = ({ dnp }: { dnp: InstalledPackageDetailData }) => {
     return null;
   }
 
-  const isTotalVolumeSizeIllDefined = volumes.some(v => !v.size);
-
   const totalVolumeSize = volumes.reduce(
-    (total, vol) => total + (vol.size || 0),
+    (total, vol) => (vol.size != null ? total + vol.size : total),
     0
   );
 
@@ -104,7 +102,7 @@ export const VolumesList = ({ dnp }: { dnp: InstalledPackageDetailData }) => {
           </span>
         </span>
         <span>
-          {isTotalVolumeSizeIllDefined ? "-" : prettyBytes(totalVolumeSize)}
+          {prettyBytes(totalVolumeSize)}
         </span>
 
         <BsTrash
