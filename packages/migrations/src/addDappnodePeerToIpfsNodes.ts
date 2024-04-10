870fa8e7a67694d8e4d5c4126d7c80bf25a242cf
mateumiralles <mateumiralles714@gmail.com>
2024-04-10 09:05:46 +0200

    add dappnode peers review fix

--- packages/migrations/src/addDappnodePeerToIpfsNodes.ts
+++ packages/migrations/src/addDappnodePeerToIpfsNodes.ts
@@ -1,18 +1,29 @@
 import { logs } from "@dappnode/logger";
 
-//Adds Dappnode IPFS peers to the local IPFS node for more stability when fetching IPFS content
+interface IPFSnode {
+  peer: string;
+  gateway: string;
+}
+
+//Adds Dappnode IPFS peers to the IPFS nodes for more stability when fetching IPFS content
 export async function addDappnodePeerToIpfsNodes() {
-  const nodesIps = [
-    "167.86.114.131", //local node
-    "65.109.51.31", //production node
+  const nodesIps: IPFSnode[] = [
+    {
+      peer: "QmfB6dT5zxUq1BXiXisgcZKYkvjywdDYBK5keRaqDKH633",
+      gateway: "167.86.114.131",
+    }, //remote - dev/testing node
+    {
+      peer: "12D3KooWLdrSru7LzYY4YDcfnJsrJeshTQooR2j38NkGvoj2yADp",
+      gateway: "65.109.51.31",
+    }, //remote - production node
   ];
 
   const ipfsAlias = "ipfs.dappnode";
-  const dappnodeIpfsPeer = (ipfsNodeIp: string) =>
-    `/ip4/${ipfsNodeIp}/tcp/4001/p2p/QmfB6dT5zxUq1BXiXisgcZKYkvjywdDYBK5keRaqDKH633`;
+  const dappnodeIpfsPeer = (node: IPFSnode) =>
+    `/ip4/${node.gateway}/tcp/4001/p2p/${node.peer}`;
 
-  // forEach node adding
-  nodesIps.forEach(async (node) => {
+  // Adding peer for each node
+  for (const node of nodesIps) {
     logs.info(`adding dappnode peer to ipfs node: ${node}`);
     await fetch(
       `http://${ipfsAlias}:5001/api/v0/swarm/peering/add?arg=${dappnodeIpfsPeer(
@@ -22,5 +33,5 @@ export async function addDappnodePeerToIpfsNodes() {
         method: "POST",
       }
     );
-  });
+  }
 }
