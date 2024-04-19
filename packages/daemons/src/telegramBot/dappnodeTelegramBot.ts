02190bbd1960fec3acc0c527203e596df156f119
pablomendezroyo <mendez4a@gmail.com>
2024-04-19 10:28:46 +0100

    refactor telegrambot class

--- packages/daemons/src/telegramBot/dappnodeTelegramBot.ts
+++ packages/daemons/src/telegramBot/dappnodeTelegramBot.ts
@@ -9,175 +9,159 @@ import { params } from "@dappnode/params";
 import { TelegramCommandMessageHeader } from "./types.js";
 
 // Note: Telegram commands MUST NOT contain "-", only "_"
-export const enableAutoUpdatesCmd = "/enable_auto_updates";
-export const startCmd = "/start";
-export const unsubscribeCmd = "/unsubscribe";
-export const helpCmd = "/help";
-export const getWireguardCredentials = "/get_wireguard_credentials";
+interface ICommand {
+  description: string;
+  action:
+    | "handleEnableAutoUpdates"
+    | "handleDisableAutoUpdates"
+    | "subscribeCmd"
+    | "unsubscribeCmd"
+    | "helpCmd"
+    | "handleWireguardCredentials";
+  requiresAuth?: boolean;
+}
 
-const cmds = [
-  {
-    cmd: enableAutoUpdatesCmd,
-    help: "Enable auto-updates for all packages",
+const COMMANDS: Record<string, ICommand> = {
+  "/enable_auto_updates": {
+    description: "Enable auto-updates for all packages",
+    action: "handleEnableAutoUpdates",
+    requiresAuth: true,
   },
-  {
-    cmd: startCmd,
-    help: "Subscribe to future notifications",
+  disable_auto_updates: {
+    description: "Disable auto-updates for all packages",
+    action: "handleDisableAutoUpdates",
+    requiresAuth: true,
   },
-  {
-    cmd: unsubscribeCmd,
-    help: "Unsubcribe from future notifications",
+  "/start": {
+    description: "Subscribe to future notifications",
+    action: "subscribeCmd",
   },
-  {
-    cmd: helpCmd,
-    help: "Display all available commands",
+  "/unsubscribe": {
+    description: "Unsubscribe from future notifications",
+    action: "unsubscribeCmd",
   },
-  {
-    cmd: helpCmd,
-    help: "Display all available commands",
+  "/help": {
+    description: "Display all available commands",
+    action: "helpCmd",
   },
-  {
-    cmd: getWireguardCredentials,
-    help: "Fetch wireguard credentials",
+  "/get_wireguard_credentials": {
+    description: "Fetch wireguard credentials",
+    action: "handleWireguardCredentials",
+    requiresAuth: true,
   },
-];
+};
 
-/**
- * Polls for commands and responds.
- * Channel ID is not needed to response to messages
- */
 export class DappnodeTelegramBot {
   private bot: TelegramBot;
 
   constructor(telegramToken: string) {
-    this.bot = new TelegramBot(telegramToken);
-
+    this.bot = new TelegramBot(telegramToken, { polling: true });
     // POLLING ERRORS
     // 1. EFATAL if error was fatal e.g. network error
     // 2. EPARSE if response body could not be parsed
     // 3. ETELEGRAM if error was returned from Telegram servers
     // ETELEGRAM: 409 Conflict  =>  More than one bot instance polling
     // ETELEGRAM: 404 Not Found => wrong token or not found
-    this.bot.on("polling_error", (error) => {
-      logs.error(`${error.name}: ${error.message}`);
-    });
-
-    // Listen for any messages. If channel ID does not exists, it saves the channel ID
-    this.bot.on("message", async (msg) => {
-      try {
-        if (!msg.text) return;
-
-        if (msg.text.startsWith(enableAutoUpdatesCmd)) {
-          await this.enableAutoUpdatesCmd(msg);
-        } else if (msg.text.startsWith(unsubscribeCmd)) {
-          await this.unsubscribeCmd(msg);
-        } else if (msg.text.startsWith(helpCmd)) {
-          await this.helpCmd(msg);
-        } else if (msg.text.startsWith(getWireguardCredentials)) {
-          // IMPORTANT: verify user ID is whitelisted
-          const userId = msg.from?.id;
-          if (!userId) {
-            logs.error("This command requires user ID authentication");
-            await this.sendMessage(
-              msg.chat.id.toString(),
-              "This command requires user ID authentication. Set the auth user ID in your dappnode at http://my.dappnode/system/notifications"
-            );
-            return;
-          }
-          if (userId.toString() !== db.telegramUserId.get()) {
-            logs.error(`Unauthorized user: ${userId}`);
-            await this.sendMessage(
-              msg.chat.id.toString(),
-              "Unauthorized user. Please contact the admin"
-            );
-          } else {
-            // default device https://github.com/dappnode/DNP_WIREGUARD/blob/4a074010c98b5d3003d1c3306edcb75392b247f4/docker-compose.yml#L12
-            const deviceName = msg.text.split(" ")[1] || "dappnode_admin";
-            logs.info(
-              `Fetching wireguard credentials from device ${deviceName} for user: ${userId}`
-            );
-            try {
-              const credentials = await this.getWireguardCredentialsCmd(
-                deviceName
-              );
-              await this.sendMessage(msg.chat.id.toString(), credentials);
-            } catch (e) {
-              logs.error(`Error fetching wireguard credentials: ${e}`);
-              // beauty error message for device not found
-              if (e.statusCode === 404)
-                e.message = `Device not found: ${deviceName}`;
-              await this.sendMessage(
-                msg.chat.id.toString(),
-                `Error fetching wireguard credentials: ${e.message}`
-              );
-            }
-          }
-        } else {
-          // If channel is not subscribed yet, subscribe
-          if (!this.channelIdExists(msg.chat.id.toString())) {
-            await this.subscribeCmd(msg);
-          } else if (msg.text.startsWith(helpCmd)) {
-            await this.sendMessage(
-              msg.chat.id.toString(),
-              "Already subscribed"
-            );
-          }
-        }
-      } catch (e) {
-        logs.error(`Error on TelegramBot message handler: ${msg.text}`, e);
-      }
-    });
+    this.bot.on("polling_error", (error) =>
+      logs.error(`${error.name}: ${error.message}`)
+    );
+    this.bot.on("message", (msg) => this.handleMessage(msg));
   }
 
-  async start(): Promise<void> {
-    await this.bot.startPolling();
+  private async handleMessage(msg: TelegramBot.Message): Promise<void> {
+    if (!msg.text) return;
+
+    const command = Object.keys(COMMANDS).find((cmd) =>
+      msg.text?.startsWith(cmd)
+    );
+    if (!command) {
+      await this.checkSubscription(msg);
+      return;
+    }
+
+    const commandConfig = COMMANDS[command];
+    if (commandConfig.requiresAuth && !this.isUserAuthorized(msg.from?.id))
+      await this.sendUnauthorizedMessage(msg.chat.id);
+    else {
+      const actionMethod = this[commandConfig.action];
+      if (actionMethod) await actionMethod.call(this, msg);
+    }
   }
 
-  async stop(): Promise<void> {
-    await this.bot.stopPolling();
-  }
-
-  async sendMessage(chatId: string | number, text: string): Promise<void> {
-    await this.bot.sendMessage(chatId, text, {
-      parse_mode: "Markdown",
-    });
-  }
-
-  // Utils
+  // HANDLERS
 
   /**
    *  Fetch wireguard credentials with the default device
    */
-  private async getWireguardCredentialsCmd(
-    deviceName?: string
-  ): Promise<string> {
+  private async handleWireguardCredentials(
+    msg: TelegramBot.Message
+  ): Promise<void> {
+    // default device name is dappnode_admin, see https://github.com/dappnode/DNP_WIREGUARD/blob/4a074010c98b5d3003d1c3306edcb75392b247f4/docker-compose.yml#L12
+    const deviceName = msg.text?.split(" ")[1] || "dappnode_admin";
     // build url with params.WIREGUARD_API_URL and deviceName
     const url = `${params.WIREGUARD_API_URL}/${deviceName}`;
     const res = await fetch(url);
     const configRemote = await res.text();
     if (!res.ok) {
-      if (res.status === 404) throw Error(`Device not found: ${configRemote}`);
-      throw Error(
-        `Error fetching credentials: ${res.statusText} ${configRemote}`
-      );
+      if (res.status === 404)
+        return await this.sendMessage(
+          msg.chat.id,
+          `Device ${deviceName} not found. Please provide a valid device name or if you have setup dappnode cloud wait for the credentials to be generated.`
+        );
+      else
+        return await this.sendMessage(
+          msg.chat.id,
+          `Error fetching wireguard credentials. Please try again later.`
+        );
     }
 
-    return configRemote;
+    await this.sendMessage(msg.chat.id, configRemote);
   }
 
   /**
    * Enable auto-updates for system and regular packages
    */
-  private async enableAutoUpdatesCmd(msg: TelegramBot.Message): Promise<void> {
+  private async handleEnableAutoUpdates(
+    msg: TelegramBot.Message
+  ): Promise<void> {
     editDnpSetting(true);
     editCoreSetting(true);
-
-    const message = [
-      "Successfully enabled auto-updates",
-      "You can manage or disable auto-update in the Admin UI",
-    ].join("\n\n");
-    await this.sendMessage(msg.chat.id.toString(), message);
+    await this.sendMessage(
+      msg.chat.id,
+      "Successfully enabled auto-updates\n\nYou can manage or disable auto-update in the Admin UI at http://my.dappnode/system/auto-updates"
+    );
   }
+
+  /**
+   * Disable auto-updates for system and regular packages
+   */
+  private async handleDisableAutoUpdates(
+    msg: TelegramBot.Message
+  ): Promise<void> {
+    editDnpSetting(false);
+    editCoreSetting(false);
+    await this.sendMessage(
+      msg.chat.id,
+      "Successfully disabled auto-updates\n\nYou can manage or enable auto-update in the Admin UI at http://my.dappnode/system/auto-updates"
+    );
+  }
+
+  /**
+   * Print help
+   */
+  private async helpCmd(msg: TelegramBot.Message): Promise<void> {
+    const chatId = msg.chat.id.toString();
+    const message = [
+      bold("Commands"),
+      ...Object.entries(COMMANDS).map(
+        ([command, { description }]) => `${command} - ${description}`
+      ),
+    ].join("\n\n");
+
+    await this.sendMessage(chatId, message);
+  }
+
+  // Channel ID subscription
 
   /**
    * Add channel ID
@@ -213,17 +197,31 @@ export class DappnodeTelegramBot {
     await this.sendMessage(chatId, message);
   }
 
-  /**
-   * Print help
-   */
-  private async helpCmd(msg: TelegramBot.Message): Promise<void> {
-    const chatId = msg.chat.id.toString();
-    const message = [
-      bold("Commands"),
-      cmds.map(({ cmd, help }) => `${bold(cmd)} ${help}`).join("\n"),
-    ].join("\n\n");
+  // HELPERS
 
-    await this.sendMessage(chatId, message);
+  private async sendMessage(
+    chatId: number | string,
+    text: string
+  ): Promise<void> {
+    await this.bot.sendMessage(chatId, text, { parse_mode: "Markdown" });
+  }
+
+  private isUserAuthorized(userId: number | undefined): boolean {
+    return !!userId && userId.toString() === db.telegramUserId.get();
+  }
+
+  private async sendUnauthorizedMessage(
+    chatId: number | string
+  ): Promise<void> {
+    await this.sendMessage(
+      chatId,
+      "Unauthorized user. Please contact the admin"
+    );
+  }
+
+  private async checkSubscription(msg: TelegramBot.Message): Promise<void> {
+    const chatId = msg.chat.id.toString();
+    if (!this.channelIdExists(chatId)) await this.subscribeCmd(msg);
   }
 
   private channelIdExists(chatId: string): boolean {
