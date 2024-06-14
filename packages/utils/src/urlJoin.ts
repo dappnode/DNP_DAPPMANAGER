95532a042a8796096945edd736d52ccd017d7575
Voss <voss@visnovalabs.io>
2024-06-14 08:36:32 -0500

    fix redirects involving joining rootPaths

--- packages/utils/src/urlJoin.ts
+++ packages/utils/src/urlJoin.ts
@@ -2,9 +2,10 @@
  * Joins multiple url parts safely
  * - Does not break the protocol double slash //
  * - Cleans double slashes at any point
+ * - Removes any asterisks *  primarily from rootPaths
  * @param args ("http://ipfs.io", "ipfs", "Qm")
  * @returns "http://ipfs.io/ipfs/Qm"
  */
 export function urlJoin(...args: string[]): string {
-  return args.join("/").replace(/([^:]\/)\/+/g, "$1");
+  return args.join("/").replace(/(?<!\:)\/{2,}|\*/g, "$1");
 }
