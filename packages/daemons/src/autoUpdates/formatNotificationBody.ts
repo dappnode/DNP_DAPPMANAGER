da2c86670ea7eb9783c68c74b9696ca8f557fbc2
Voss <voss@visnovalabs.io>
2024-06-19 19:15:18 -0500

    add full import with extension to comply with typescript

--- packages/daemons/src/autoUpdates/formatNotificationBody.ts
+++ packages/daemons/src/autoUpdates/formatNotificationBody.ts
@@ -1,6 +1,6 @@
 import { CoreUpdateDataAvailable } from "@dappnode/types";
 import { urlJoin, prettyDnpName } from "@dappnode/utils";
-import { getInstallerPath } from "@dappnode/admin-ui/src/pages/installer";
+import { getInstallerPath } from "@dappnode/admin-ui/src/pages/installer/index.js";
 
 const adminUiUpdateCoreUrl = "http://my.dappnode/system/update";
 
