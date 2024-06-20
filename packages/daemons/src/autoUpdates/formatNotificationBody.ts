e8a95cfc517ed4c63ec456b95d51cc874ecd9d3b
Voss <voss@visnovalabs.io>
2024-06-19 20:02:31 -0500

    remove extension in import

--- packages/daemons/src/autoUpdates/formatNotificationBody.ts
+++ packages/daemons/src/autoUpdates/formatNotificationBody.ts
@@ -1,6 +1,6 @@
 import { CoreUpdateDataAvailable } from "@dappnode/types";
 import { urlJoin, prettyDnpName } from "@dappnode/utils";
-import { getInstallerPath } from "./../../../admin-ui/src/pages/installer/data.ts";
+import { getInstallerPath } from "./../../../admin-ui/src/pages/installer/data";
 
 const adminUiUpdateCoreUrl = "http://my.dappnode/system/update";
 
