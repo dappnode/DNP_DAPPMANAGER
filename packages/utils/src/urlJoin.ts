0a50426b30f522f3443a84e06ac1c9909f85f1d2
Voss <voss@visnovalabs.io>
2024-06-25 17:52:25 -0500

    revert chnage to url join function

--- packages/utils/src/urlJoin.ts
+++ packages/utils/src/urlJoin.ts
@@ -2,10 +2,16 @@
  * Joins multiple url parts safely
  * - Does not break the protocol double slash //
  * - Cleans double slashes at any point
- * - Removes any asterisks *  primarily from rootPaths
  * @param args ("http://ipfs.io", "ipfs", "Qm")
  * @returns "http://ipfs.io/ipfs/Qm"
  */
 export function urlJoin(...args: string[]): string {
-  return args.join("/").replace(/(?<!:)\/{2,}|\*/g, "$1");
+  return (
+    args
+      .join("/")
+      // Remove duplicated // expect for the protocol
+      .replace(/([^:]\/)\/+/g, "$1")
+      // Remove duplicated // at the start of the string, "//route" => "/route"
+      .replace(/^\/+/, "/")
+  );
 }
