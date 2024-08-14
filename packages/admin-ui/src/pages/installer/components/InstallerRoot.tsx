548abda2e8190e8dcb81407aade885632f60fff4
Dappnodedev <diego@dappnode.io>
2024-08-14 11:18:29 +0000

    Add version to path

--- packages/admin-ui/src/pages/installer/components/InstallerRoot.tsx
+++ packages/admin-ui/src/pages/installer/components/InstallerRoot.tsx
@@ -105,7 +105,7 @@ const InstallerRoot: React.FC = () => {
             element={
               <Routes>
                 <Route index element={<route.component />} />
-                <Route path=":id/*" element={<InstallDnpContainer />} />
+                <Route path=":id/:version?/*" element={<InstallDnpContainer />} />
               </Routes>
             }
           />
