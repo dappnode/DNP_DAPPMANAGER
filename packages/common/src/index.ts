1c483cc77563951aadf4ea828304edc6c5f22507
pablomendezroyo <mendez4a@gmail.com>
2024-09-02 11:26:31 +0200

    add ts expect errors

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,8 +2,9 @@ export * from "./transport/jsonRpc/index.js";
 export * from "./transport/socketIo/index.js";
 
 // Schemas
-import routesArgumentsSchema from "./validation/schemas/routesArguments.schema.js";
-import routesReturnSchema from "./validation/schemas/routesReturn.schema.js";
-import subscriptionsArgumentsSchema from "./validation/schemas/subscriptionsArguments.schema.js";
-
-export { routesArgumentsSchema, routesReturnSchema, subscriptionsArgumentsSchema };
+// @ts-expect-error the schemas will appear once the package is built
+export { routesArgumentsSchema } from "./validation/schemas/routesArguments.schema.js";
+// @ts-expect-error the schemas will appear once the package is built
+export { routesReturnSchema } from "./validation/schemas/routesReturn.schema.js";
+// @ts-expect-error the schemas will appear once the package is built
+export { subscriptionsArgumentsSchema } from "./validation/schemas/subscriptionsArguments.schema.js";
