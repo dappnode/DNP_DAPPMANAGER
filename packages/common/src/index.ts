6180c014dfea075a39db05c8cb7616e598fb7d37
pablomendezroyo <mendez4a@gmail.com>
2024-09-02 11:35:47 +0200

    add expect error

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,6 +2,9 @@ export * from "./transport/jsonRpc/index.js";
 export * from "./transport/socketIo/index.js";
 
 // Schemas
+// @ts-expect-error expected error until schemas are gnerated
 export { default as routesArgumentsSchema } from "./validation/schemas/routesArguments.schema.js";
+// @ts-expect-error expected error until schemas are gnerated
 export { default as routesReturnSchema } from "./validation/schemas/routesReturn.schema.js";
+// @ts-expect-error expected error until schemas are gnerated
 export { default as subscriptionsArgumentsSchema } from "./validation/schemas/subscriptionsArguments.schema.js";
