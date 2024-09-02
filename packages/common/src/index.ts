c36f923a1ecfb28acb5c6373f7fef84cc5b33097
pablomendezroyo <mendez4a@gmail.com>
2024-09-02 11:30:56 +0200

    add default as to exports in common schemas

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,9 +2,6 @@ export * from "./transport/jsonRpc/index.js";
 export * from "./transport/socketIo/index.js";
 
 // Schemas
-// @ts-expect-error the schemas will appear once the package is built
-export { routesArgumentsSchema } from "./validation/schemas/routesArguments.schema.js";
-// @ts-expect-error the schemas will appear once the package is built
-export { routesReturnSchema } from "./validation/schemas/routesReturn.schema.js";
-// @ts-expect-error the schemas will appear once the package is built
-export { subscriptionsArgumentsSchema } from "./validation/schemas/subscriptionsArguments.schema.js";
+export { default as routesArgumentsSchema } from "./validation/schemas/routesArguments.schema.js";
+export { default as routesReturnSchema } from "./validation/schemas/routesReturn.schema.js";
+export { default as subscriptionsArgumentsSchema } from "./validation/schemas/subscriptionsArguments.schema.js";
