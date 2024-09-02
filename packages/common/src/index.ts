991ff0694ef2ce79fa2349f2bf33b621a3a6ca15
pablomendezroyo <mendez4a@gmail.com>
2024-09-02 11:51:53 +0200

    fix eslint error

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,10 +1,13 @@
 export * from "./transport/jsonRpc/index.js";
 export * from "./transport/socketIo/index.js";
 
-// Schemas
-// @ts-expect-error expected error until schemas are gnerated
+// Schemas: expected ESLINT error until schemas are gnerated
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export { default as routesArgumentsSchema } from "./validation/schemas/routesArguments.schema.js";
-// @ts-expect-error expected error until schemas are gnerated
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export { default as routesReturnSchema } from "./validation/schemas/routesReturn.schema.js";
-// @ts-expect-error expected error until schemas are gnerated
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export { default as subscriptionsArgumentsSchema } from "./validation/schemas/subscriptionsArguments.schema.js";
