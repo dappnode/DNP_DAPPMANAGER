4b9bcdb529df65bf679d4377e9d155fa33d348c7
pablomendezroyo <mendez4a@gmail.com>
2024-12-10 17:29:56 +0100

    remove validator and beacon fullnode alises

--- packages/stakers/src/mevBoost.ts
+++ packages/stakers/src/mevBoost.ts
@@ -84,7 +84,7 @@ export class MevBoost extends StakerComponent {
     await super.setNew({
       newStakerDnpName: newMevBoostDnpName,
       dockerNetworkName: params.DOCKER_STAKER_NETWORKS[network],
-      fullnodeAlias: `mev-boost.${network}.dncore.dappnode`,
+      fullnodeAliases: [`mev-boost.${network}.dncore.dappnode`],
       compatibleClients: compatibleMevBoost ? [compatibleMevBoost] : null,
       userSettings: newMevBoostDnpName ? this.getUserSettings(network, newRelays) : {},
       prevClient: compatibleMevBoost ? compatibleMevBoost.dnpName : null
