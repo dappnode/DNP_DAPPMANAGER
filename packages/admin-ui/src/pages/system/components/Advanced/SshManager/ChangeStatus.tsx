de8893b1a7f7bd346090de41daa9a6c7d7a1805b
mateumiralles <mateumiralles714@gmail.com>
2025-03-03 16:14:17 +0100

    Refactor SSH section in `/system/advanced`

--- packages/admin-ui/src/pages/system/components/Advanced/SshManager/ChangeStatus.tsx
+++ packages/admin-ui/src/pages/system/components/Advanced/SshManager/ChangeStatus.tsx
@@ -1,25 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { api } from "api";
-import Button from "components/Button";
 import ErrorView from "components/ErrorView";
-import Input from "components/Input";
 import Ok from "components/Ok";
 import { confirm } from "components/ConfirmDialog";
 import { ReqStatus } from "types";
-import "./sshManager.scss";
 import { withToast } from "components/toast/Toast";
 import { ShhStatus } from "@dappnode/types";
+import Switch from "components/Switch";
+import "./sshManager.scss";
 
-export function SshManagerChangeStatus() {
-  const [reqGetStatus, setReqGetStatus] = useState<ReqStatus<ShhStatus>>({});
+interface SshManagerChangeStatusProps {
+  reqGetStatus: ReqStatus<ShhStatus>;
+  setReqGetStatus: (reqGetStatus: ReqStatus<ShhStatus>) => void;
+}
+
+export function SshManagerChangeStatus({ reqGetStatus, setReqGetStatus }: SshManagerChangeStatusProps) {
   const [reqSetStatus, setReqSetStatus] = useState<ReqStatus<ShhStatus>>({});
+
+  useEffect(() => {
+    fetchSshStatus();
+  }, []);
 
   async function changeSshStatus(status: ShhStatus) {
     if (status === "disabled") {
       await new Promise<void>((resolve) => {
         confirm({
           title: `Disabling SSH service`,
-          text: "Warning, you will loose SSH access to your DAppNode. Having direct access to your host machine may be necessary to fix bugs. Make sure to have an alternative way to access your DAppNode, such as physically with a screen and keyboard before disabling SSH access",
+          text:
+            "Warning, you will loose SSH access to your DAppNode. Having direct access to your host machine may be necessary to fix bugs. Make sure to have an alternative way to access your DAppNode, such as physically with a screen and keyboard before disabling SSH access",
           label: "Disable",
           onClick: resolve
         });
@@ -52,30 +60,23 @@ export function SshManagerChangeStatus() {
     }
   }
 
+  const handleToggle = () => {
+    if (reqGetStatus.result === "enabled") {
+      changeSshStatus("disabled");
+    } else {
+      changeSshStatus("enabled");
+    }
+  };
+  console.log(reqGetStatus.result);
   return (
     <>
-      <Input
-        value={reqGetStatus.result || "?"}
-        onValueChange={() => {}}
-        prepend="SSH status"
-        append={
-          <Button disabled={reqGetStatus.loading} onClick={fetchSshStatus}>
-            Fetch status
-          </Button>
-        }
-      />
+      <div className="section-wrapper">
+        <div className="subtle-header">ENABLE, DISABLE SSH</div>
+        <Switch checked={reqGetStatus.result === "enabled"} onToggle={handleToggle} disabled={reqGetStatus.loading} />
+      </div>
 
       {reqGetStatus.loading && <Ok loading msg="Fetching SSH status..."></Ok>}
       {reqGetStatus.error && <ErrorView error={reqGetStatus.error} hideIcon red />}
-
-      <div className="ssh-status-manager-buttons">
-        <Button disabled={reqSetStatus.loading} onClick={() => changeSshStatus("enabled")}>
-          Enable
-        </Button>
-        <Button disabled={reqSetStatus.loading} onClick={() => changeSshStatus("disabled")}>
-          Disable
-        </Button>
-      </div>
 
       {reqSetStatus.loading && <Ok loading msg="Changing SSH status..."></Ok>}
       {reqSetStatus.result && <Ok ok msg={`Successfully ${reqSetStatus.result} SSH`}></Ok>}
