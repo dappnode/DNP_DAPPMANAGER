c92b6b40c312dcf44c246cb19927ce254ca6fd17
mateumiralles <mateumiralles714@gmail.com>
2025-03-06 02:48:46 +0100

    Notifications card & Install notifications pkg

--- packages/admin-ui/src/pages/notifications/components/tabs/Inbox.tsx
+++ packages/admin-ui/src/pages/notifications/components/tabs/Inbox.tsx
@@ -1,6 +1,9 @@
 import SubTitle from "components/SubTitle";
-import React from "react";
+import React, { useState } from "react";
 import Card from "components/Card";
+import { Accordion } from "react-bootstrap";
+
+import "../notifications.scss";
 
 interface Notification {
   timestamp: string;
@@ -16,6 +19,12 @@ export default function Inbox() {
       title: "New Notification",
       dnp: "package-name.dnp.dappnode.eth",
       body: "This is a new notification"
+    },
+    {
+      timestamp: "2025-06-06T13:00:00Z",
+      title: "Validator exited sucesfully",
+      dnp: "lido-csm-mainnet.dnp.dappnode.eth",
+      body: "Your validator 10802082 has entered the exit queue automatically. No manual action required"
     }
   ];
   const seenNotifications: Notification[] = [
@@ -24,20 +33,28 @@ export default function Inbox() {
       title: "Seen Notification",
       dnp: "package-name.dnp.dappnode.eth",
       body: "This is a seen notification"
+    },
+    {
+      timestamp: "2025-06-04T12:00:00Z",
+      title: "Updated Telegram configuration",
+      dnp: "lido-csm-mainnet.dnp.dappnode.eth",
+      body: "Your telegram configuration has been updated successfully"
+    },
+    {
+      timestamp: "2025-06-06T12:00:00Z",
+      title: "Validator exit request",
+      dnp: "lido-csm-mainnet.dnp.dappnode.eth",
+      body: "Your validator 10802082 has requested to exit the network. Executing automatic exit"
     }
   ];
+
   return (
     <>
       {newNotifications.length > 0 && (
         <>
           <SubTitle>New Notifications</SubTitle>
           {newNotifications.map((notification) => (
-            <Card key={notification.timestamp}>
-              <div>{new Date(notification.timestamp).toLocaleString()}</div>
-              <div>{notification.title}</div>
-              <div>{notification.dnp}</div>
-              <div>{notification.body}</div>
-            </Card>
+            <NotificationCard key={notification.timestamp} notification={notification} />
           ))}
         </>
       )}
@@ -47,14 +64,33 @@ export default function Inbox() {
         <Card>No notifications</Card>
       ) : (
         seenNotifications.map((notification) => (
-          <Card key={notification.timestamp}>
-            <div>{new Date(notification.timestamp).toUTCString()}</div>
-            <div>{notification.title}</div>
-            <div>{notification.dnp}</div>
-            <div>{notification.body}</div>
-          </Card>
+          <NotificationCard key={notification.timestamp} notification={notification} />
         ))
       )}
     </>
+  );
+}
+
+function NotificationCard({ notification }: { notification: Notification }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Accordion defaultActiveKey={isOpen ? "0" : "1"} >
+      <Accordion.Toggle as={"div"} eventKey="0" onClick={() => setIsOpen(!isOpen)} className="notification-card">
+        <div className="notification-header">
+          <div className="notification-img"> IMG </div>
+          <div className="notification-header-data">
+            <div className="notification-header-details">
+              <div>{new Date(notification.timestamp).toLocaleString()}</div>
+              <div>{notification.dnp}</div>
+            </div>
+            <div className="notification-title">{notification.title}</div>
+          </div>
+        </div>
+        <Accordion.Collapse eventKey="0">
+          <div className="notification-body">{notification.body}</div>
+        </Accordion.Collapse>
+      </Accordion.Toggle>{" "}
+    </Accordion>
   );
 }
