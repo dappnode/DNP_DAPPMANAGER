e958a41c45e01cecdc0d1b647f34fde57c678f83
mateumiralles <mateumiralles714@gmail.com>
2025-03-06 22:35:45 +0100

    Avatars in notifications list

--- packages/admin-ui/src/pages/notifications/tabs/Inbox/components/NotificationsCard.tsx
+++ packages/admin-ui/src/pages/notifications/tabs/Inbox/components/NotificationsCard.tsx
@@ -3,18 +3,23 @@ import { Accordion } from "react-bootstrap";
 import { Notification } from "../Inbox";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
-export function NotificationCard({ notification }: { notification: Notification }) {
+interface NotificationCardProps {
+  notification: Notification;
+  avatarUrl: string;
+}
+
+export function NotificationCard({ notification, avatarUrl }: NotificationCardProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Accordion defaultActiveKey={isOpen ? "0" : "1"}>
       <Accordion.Toggle as={"div"} eventKey="0" onClick={() => setIsOpen(!isOpen)} className="notification-card">
         <div className="notification-header">
-          <div className="notification-img"> IMG </div>
+          <img className="avatar" src={avatarUrl} alt={notification.dnp} />
           <div className="notification-header-data">
             <div className="notification-header-row secondary-text">
               <div>{notification.dnp}</div>
-              <div>{new Date(notification.timestamp).toLocaleString()}</div>
+              <i>{new Date(notification.timestamp).toLocaleString()}</i>
             </div>
             <div className="notification-header-row ">
               <div className="notification-title">{notification.title}</div>
@@ -25,7 +30,7 @@ export function NotificationCard({ notification }: { notification: Notification 
         <Accordion.Collapse eventKey="0">
           <div className="notification-body">{notification.body}</div>
         </Accordion.Collapse>
-      </Accordion.Toggle>{" "}
+      </Accordion.Toggle>
     </Accordion>
   );
 }
