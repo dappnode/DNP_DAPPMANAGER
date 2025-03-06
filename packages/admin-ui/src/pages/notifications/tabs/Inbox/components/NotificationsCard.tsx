9f6ec00fcb13fabe6e6c371d0db19bd01be2f5dd
mateumiralles <mateumiralles714@gmail.com>
2025-03-06 21:50:19 +0100

    Notifications card component updated

--- packages/admin-ui/src/pages/notifications/tabs/Inbox/components/NotificationsCard.tsx
+++ packages/admin-ui/src/pages/notifications/tabs/Inbox/components/NotificationsCard.tsx
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
 import { Accordion } from "react-bootstrap";
 import { Notification } from "../Inbox";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 export function NotificationCard({ notification }: { notification: Notification }) {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    return (
-      <Accordion defaultActiveKey={isOpen ? "0" : "1"} >
-        <Accordion.Toggle as={"div"} eventKey="0" onClick={() => setIsOpen(!isOpen)} className="notification-card">
-          <div className="notification-header">
-            <div className="notification-img"> IMG </div>
-            <div className="notification-header-data">
-              <div className="notification-header-details">
-                <div>{notification.dnp}</div>
-                <div>{new Date(notification.timestamp).toLocaleString()}</div>
-              </div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Accordion defaultActiveKey={isOpen ? "0" : "1"}>
+      <Accordion.Toggle as={"div"} eventKey="0" onClick={() => setIsOpen(!isOpen)} className="notification-card">
+        <div className="notification-header">
+          <div className="notification-img"> IMG </div>
+          <div className="notification-header-data">
+            <div className="notification-header-row secondary-text">
+              <div>{notification.dnp}</div>
+              <div>{new Date(notification.timestamp).toLocaleString()}</div>
+            </div>
+            <div className="notification-header-row ">
               <div className="notification-title">{notification.title}</div>
+              {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
             </div>
           </div>
-          <Accordion.Collapse eventKey="0">
-            <div className="notification-body">{notification.body}</div>
-          </Accordion.Collapse>
-        </Accordion.Toggle>{" "}
-      </Accordion>
-    );
-  }
+        </div>
+        <Accordion.Collapse eventKey="0">
+          <div className="notification-body">{notification.body}</div>
+        </Accordion.Collapse>
+      </Accordion.Toggle>{" "}
+    </Accordion>
+  );
+}
