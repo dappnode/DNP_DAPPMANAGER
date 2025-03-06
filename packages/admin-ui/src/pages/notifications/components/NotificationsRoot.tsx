1e55e1c258cee029f23cae00a34c2451ec89a843
mateumiralles <mateumiralles714@gmail.com>
2025-03-06 02:48:46 +0100

    Notifications card & Install notifications pkg

--- packages/admin-ui/src/pages/notifications/components/NotificationsRoot.tsx
+++ packages/admin-ui/src/pages/notifications/components/NotificationsRoot.tsx
@@ -8,6 +8,8 @@ import Inbox from "./tabs/Inbox";
 // CSS
 import "./notifications.scss";
 import { useApi } from "api";
+import { InstallNotificationsPkg } from "./InstallNotifications";
+import Loading from "components/Loading";
 
 export const NotificationsRoot: React.FC = () => {
   const availableRoutes: {
@@ -28,32 +30,41 @@ export const NotificationsRoot: React.FC = () => {
     }
   ];
 
-  
+  const dnpsRequest = useApi.packagesGet();
+  const loading = dnpsRequest.isValidating;
+  const installedDnps = dnpsRequest.data;
 
-    const dnpsRequest = useApi.packagesGet();
-    console.log('dnpsRequest', dnpsRequest);
-    
+  const notificationsDnpName = "notifications.public.dappnode.eth";
+  const isNotificationsPkgInstalled = installedDnps?.some((dnp) => dnp.dnpName === notificationsDnpName);
   
   return (
     <>
       <Title title={title} />
-      <div className="horizontal-navbar">
-        {availableRoutes.map((route) => (
-          <button key={route.subPath} className="item-container">
-            <NavLink to={route.subPath} className="item no-a-style" style={{ whiteSpace: "nowrap" }}>
-              {route.name}
-            </NavLink>
-          </button>
-        ))}
-      </div>
+      {loading ? (
+        <Loading steps={["Loading data"]} />
+      ) : !isNotificationsPkgInstalled ? (
+        <InstallNotificationsPkg pkgName={notificationsDnpName}/>
+      ) : (
+        <>
+          <div className="horizontal-navbar">
+            {availableRoutes.map((route) => (
+              <button key={route.subPath} className="item-container">
+                <NavLink to={route.subPath} className="item no-a-style" style={{ whiteSpace: "nowrap" }}>
+                  {route.name}
+                </NavLink>
+              </button>
+            ))}
+          </div>
 
-      <div className="section-spacing">
-        <Routes>
-          {availableRoutes.map((route) => (
-            <Route key={route.subPath} path={route.subPath} element={<route.component />} />
-          ))}
-        </Routes>
-      </div>
+          <div className="section-spacing">
+            <Routes>
+              {availableRoutes.map((route) => (
+                <Route key={route.subPath} path={route.subPath} element={<route.component />} />
+              ))}
+            </Routes>
+          </div>
+        </>
+      )}
     </>
   );
 };
