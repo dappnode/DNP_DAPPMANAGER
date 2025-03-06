c02c85751e63e5c49f217ee2b1a4185a17bca820
Pablo Mendez <pablo@dappnode.io>
2025-03-06 09:47:57 +0100

    implement first approach backend calls

---
+++ packages/dappmanager/src/calls/gatusConfig.ts
@@ -0,0 +1,56 @@
+import { listPackages } from "@dappnode/dockerapi";
+import { GatusConfig, Endpoint, Manifest } from "@dappnode/types";
+import { getManifestPath } from "@dappnode/utils";
+import fs from "fs";
+
+/**
+ * Get gatus endpoints indexed by dnpName
+ */
+export async function gatusGetEndpoints(): Promise<Map<string, GatusConfig>> {
+  const packages = await listPackages();
+
+  // Read all manifests files and retrieve the gatus config
+  const endpoints = new Map<string, GatusConfig>();
+  for (const pkg of packages) {
+    const manifestPath = getManifestPath(pkg.dnpName, pkg.isCore);
+    const manifest: Manifest = JSON.parse(fs.readFileSync(manifestPath, "utf8"));
+    if (manifest.notifications) endpoints.set(pkg.dnpName, manifest.notifications);
+  }
+
+  return endpoints;
+}
+
+/**
+ * Update endpoint properties
+ * @param dnpName
+ * @param updatedEndpoint
+ */
+export async function gatusUpdateEndpoint({
+  dnpName,
+  updatedEndpoint
+}: {
+  dnpName: string;
+  updatedEndpoint: Endpoint;
+}): Promise<void> {
+  // Get current endpoint status
+  const manifest: Manifest = JSON.parse(fs.readFileSync(getManifestPath(dnpName, false), "utf8"));
+  if (!manifest.notifications) throw new Error("No notifications found in manifest");
+
+  const endpoint = manifest.notifications.endpoints.find((e) => e.name === updatedEndpoint.name);
+  if (!endpoint) throw new Error(`Endpoint ${updatedEndpoint.name} not found in manifest`);
+
+  // Update endpoint
+  Object.assign(endpoint, updatedEndpoint);
+
+  // Save manifest
+  fs.writeFileSync(getManifestPath(dnpName, false), JSON.stringify(manifest, null, 2));
+
+  // Update endpoint in gatus
+  // await fetch(`http://notifier.notifications.dappnode:8082/gatus/endpoints`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify(endpoint)
+  // });
+}
--- packages/dappmanager/src/calls/index.ts
+++ packages/dappmanager/src/calls/index.ts
@@ -22,6 +22,7 @@ export { getCoreVersion } from "./getCoreVersion.js";
 export { getUserActionLogs } from "./getUserActionLogs.js";
 export { getHostUptime } from "./getHostUptime.js";
 export { getIsConnectedToInternet } from "./getIsConnectedToInternet.js";
+export { gatusGetEndpoints, gatusUpdateEndpoint } from "./gatusConfig.js";
 export * from "./httpsPortal.js";
 export { ipfsTest } from "./ipfsTest.js";
 export { ipfsClientTargetSet } from "./ipfsClientTargetSet.js";
