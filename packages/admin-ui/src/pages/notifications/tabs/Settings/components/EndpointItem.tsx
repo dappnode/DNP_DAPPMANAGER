6bc6af4234f2a5614ec85e2971da50b288e747ec
mateumiralles <mateumiralles714@gmail.com>
2025-03-17 10:06:43 +0100

    Notifications settings components update

--- packages/admin-ui/src/pages/notifications/tabs/Settings/components/EndpointItem.tsx
+++ packages/admin-ui/src/pages/notifications/tabs/Settings/components/EndpointItem.tsx
@@ -1,23 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { Endpoint } from "@dappnode/types";
 import Switch from "components/Switch";
 import Slider from "components/Slider";
 
-export function EndpointItem({
-  endpoint,
-  index,
-  numEndpoints
-}: {
+interface EndpointItemProps {
   endpoint: Endpoint;
   index: number;
   numEndpoints: number;
-}) {
-  const [endpointEnabled, setEndpointEnabled] = useState(endpoint.enabled);
+  setPkgEndpoints: React.Dispatch<React.SetStateAction<Endpoint[]>>;
+}
+
+export function EndpointItem({ endpoint, index, numEndpoints, setPkgEndpoints }: EndpointItemProps) {
+  const endpointEnabled = endpoint.enabled;
 
   const handleEndpointToggle = () => {
-    // TODO: update "notifications.yaml" file
-    setEndpointEnabled(!endpointEnabled);
+    setPkgEndpoints((prevEndpoints) =>
+      prevEndpoints.map((ep, i) => (i === index ? { ...ep, enabled: !ep.enabled } : ep))
+    );
   };
+
+  // TODO: Parse conditions, and update its value according to the slider
+  const handleSliderUpdate = () => {
+    // setPkgEndpoints((prevEndpoints) =>
+    //   prevEndpoints.map((ep, i) => (i === index ? { ...ep, conditions: [''] } : ep))
+    // );
+  };
+
+  console.log("EndpointItem", endpoint, index);
 
   return (
     <>
@@ -26,16 +35,14 @@ export function EndpointItem({
           <strong>{endpoint.definition.title}</strong>
           <div>{endpoint.definition.description}</div>
         </div>
-        <Switch
-          checked={endpointEnabled}
-          onToggle={() => {
-            handleEndpointToggle();
-          }}
-        />
+        <Switch checked={endpointEnabled} onToggle={handleEndpointToggle} />
       </div>
       {endpointEnabled && endpoint.metric && (
         <div className="slider-wrapper">
-          <Slider />
+          <Slider
+            onChange={handleSliderUpdate}
+            // value={endpoint.conditions[0]}
+          />
         </div>
       )}
       {index + 1 < numEndpoints && <hr />}
--- packages/admin-ui/src/pages/notifications/tabs/Settings/components/ManagePackageSection.tsx
+++ packages/admin-ui/src/pages/notifications/tabs/Settings/components/ManagePackageSection.tsx
@@ -1,9 +1,10 @@
 import SubTitle from "components/SubTitle";
 import Switch from "components/Switch";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { EndpointItem } from "./EndpointItem";
 import { Endpoint } from "@dappnode/types";
 import { prettyDnpName } from "utils/format";
+import { useApi } from "api";
 
 interface ManagePackageSectionProps {
   dnpName: string;
@@ -12,6 +13,16 @@ interface ManagePackageSectionProps {
 export function ManagePackageSection({ dnpName, endpoints }: ManagePackageSectionProps) {
   const [pkgNotificationsEnabled, setPkgNotificationsEnabled] = useState(endpoints.some((ep) => ep.enabled));
   const [pkgEndpoints, setPkgEndpoints] = useState(endpoints);
+
+  useEffect(() => {
+    // TODO: Implement timeOut that waits for more config updates before sending the new Endpoints config
+    const updateConfig = async () => {
+      useApi.gatusUpdateEndpoints({
+        dnpName,
+        updatedEndpoints: pkgEndpoints});
+    };
+    updateConfig();
+  }, [pkgEndpoints]);
 
   const handlePkgToggle = () => {
     setPkgEndpoints(pkgEndpoints.map((ep) => ({ ...ep, enabled: !pkgNotificationsEnabled })));
