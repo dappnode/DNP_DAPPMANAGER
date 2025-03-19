5f096a80122837df4f907cd3bb499eded1c0594f
mateumiralles <mateumiralles714@gmail.com>
2025-03-19 16:31:57 +0100

    Searchbar component in Inbox

--- packages/admin-ui/src/pages/notifications/tabs/Inbox/Inbox.tsx
+++ packages/admin-ui/src/pages/notifications/tabs/Inbox/Inbox.tsx
@@ -1,9 +1,10 @@
 import SubTitle from "components/SubTitle";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import Card from "components/Card";
 import "./inbox.scss";
 import { NotificationCard } from "./NotificationsCard";
 import { useApi } from "api";
+import { Searchbar } from "components/Searchbar";
 import Loading from "components/Loading";
 import defaultAvatar from "img/defaultAvatar.png";
 import dappnodeIcon from "img/dappnode-logo-only.png";
@@ -11,10 +12,22 @@ import dappnodeIcon from "img/dappnode-logo-only.png";
 export function Inbox() {
   const dnpsRequest = useApi.packagesGet();
   const notifications = useApi.notificationsGetAll();
-  const newNotifications = notifications.data
+  
+  const [search, setSearch] = useState("");
+  const filteredNotifications = useMemo(() => {
+    if (!notifications.data) return [];
+
+    return notifications.data.filter(
+      (notification) =>
+        notification.title.toLowerCase().includes(search.toLowerCase()) ||
+        notification.dnpName.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [search, notifications.data]);
+
+  const newNotifications = filteredNotifications
     ?.filter((notification) => !notification.seen)
     .sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
-  const seenNotifications = notifications.data
+  const seenNotifications = filteredNotifications
     ?.filter((notification) => notification.seen)
     .sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
   const loading = dnpsRequest.isValidating;
@@ -30,13 +43,14 @@ export function Inbox() {
     return dnp.avatarUrl;
   };
 
+  console.log("filtered notis", filteredNotifications);
 
-  console.log('notis', notifications.data);
-  
   return loading ? (
     <Loading steps={["Loading data"]} />
   ) : (
     <>
+      <Searchbar value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search by package name or notification title..." />
+
       {newNotifications && newNotifications.length > 0 && (
         <>
           <SubTitle>New Notifications</SubTitle>
