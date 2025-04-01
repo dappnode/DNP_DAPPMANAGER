5d51f6647f6ffab20efe65d094609f4b7fa626ae
mateumiralles <mateumiralles714@gmail.com>
2025-04-01 14:29:46 +0200

    Add hoodi relays

--- packages/admin-ui/src/pages/stakers/components/columns/MevBoost.tsx
+++ packages/admin-ui/src/pages/stakers/components/columns/MevBoost.tsx
@@ -303,12 +303,30 @@ const getDefaultRelays = (network: Network): RelayIface[] => {
       ];
     case "hoodi":
       return [
-        // TODO: Add relays for hoodi when available
         {
           operator: "Flashbots",
           docs: "https://www.flashbots.net/",
-          url: "https://*@boost-relay-hoodi.flashbots.net"
-        }
+          url: "https://0xafa4c6985aa049fb79dd37010438cfebeb0f2bd42b115b89dd678dab0670c1de38da0c4e9138c9290a398ecd9a0b3110@boost-relay-hoodi.flashbots.net"
+        },
+        {
+          operator: "bloXroute",
+          docs: "https://bloxroute.holesky.blxrbdn.com/",
+          url:
+            "https://0x821f2a65afb70e7f2e820a925a9b4c80a159620582c1766b1b09729fec178b11ea22abb3a51f07b288be815a1a2ff516@bloxroute.hoodi.blxrbdn.com"
+        },
+        {
+          operator: "Titan",
+          docs: "https://docs.titanrelay.xyz/",
+          url:
+            "https://0xaa58208899c6105603b74396734a6263cc7d947f444f396a90f7b7d3e65d102aec7e5e5291b27e08d02c50a050825c2f@hoodi.titanrelay.xyz"
+        },
+        {
+          operator: "Aestus",
+          docs:
+            "https://flashbots.notion.site/Relay-API-Documentation-5fb0819366954962bc02e81cb33840f5#417abe417dde45caaff3dc15aaae65dd",
+          url:
+            "https://0x98f0ef62f00780cf8eb06701a7d22725b9437d4768bb19b363e882ae87129945ec206ec2dc16933f31d983f8225772b6@hoodi.aestus.live"
+        },
       ];
     default:
       return [];
