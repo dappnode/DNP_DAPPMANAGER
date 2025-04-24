846f2542af1f8e6d81ba753ddd97102b92c0eab0
mateumiralles <mateumiralles714@gmail.com>
2025-04-24 15:28:47 +0200

    Notifications general switch

--- packages/admin-ui/src/pages/notifications/tabs/Settings/Settings.tsx
+++ packages/admin-ui/src/pages/notifications/tabs/Settings/Settings.tsx
@@ -2,9 +2,13 @@ import SubTitle from "components/SubTitle";
 import React, { useEffect, useState } from "react";
 import Switch from "components/Switch";
 import { ManagePackageNotifications } from "./ManagePackageNotifications.js";
-import { useApi } from "api";
+import { api, useApi } from "api";
 import { CustomEndpoint, GatusEndpoint } from "@dappnode/types";
 import "./settings.scss";
+import { notificationsDnpName } from "params.js";
+import { confirm } from "components/ConfirmDialog";
+import { withToast } from "components/toast/Toast";
+import { continueIfCalleDisconnected } from "api/utils";
 
 interface EndpointsData {
   [dnpName: string]: {
@@ -15,9 +19,10 @@ interface EndpointsData {
 }
 
 export function NotificationsSettings() {
-  const [notificationsEnabled, setNotificationsEnabled] = useState(true);
+  const [notificationsDisabled, setNotificationsDisabled] = useState<boolean>(false);
   const [endpointsData, setEndpointsData] = useState<EndpointsData | undefined>();
   const endpointsCall = useApi.notificationsGetAllEndpoints();
+  const notificationsDnp = useApi.packageGet({ dnpName: notificationsDnpName });
 
   useEffect(() => {
     // Fetch the latest endpoints data when the component is mounted
@@ -26,22 +31,65 @@ export function NotificationsSettings() {
     }
   }, [endpointsCall.data]);
 
+  useEffect(() => {
+    if (notificationsDnp.data) {
+      console.log("notificationsDnp.data", notificationsDnp.data);
+
+      const isStopped = notificationsDnp.data.containers.some((c) => c.state !== "running");
+      console.log("isStopped", isStopped);
+
+      setNotificationsDisabled(isStopped);
+    }
+  }, [notificationsDnp.data]);
+
+  async function startStopNotifications(): Promise<void> {
+    try {
+      if (notificationsDnp.data) {
+        if (!notificationsDisabled)
+          await new Promise<void>((resolve) => {
+            confirm({
+              title: `Pause notifications package`,
+              text: `Attention, the notifications package may alert you to critical issues if they arise. Pausing this package could result in missing important notifications.`,
+              label: "Pause",
+              onClick: resolve
+            });
+          });
+
+        await withToast(
+          continueIfCalleDisconnected(
+            () => api.packageStartStop({ dnpName: notificationsDnpName }),
+            notificationsDnpName
+          ),
+          {
+            message: notificationsDisabled ? "Enabling notifications" : "Disabling notifications",
+            onSuccess: notificationsDisabled ? "Notifications Enabled" : "Notifications disabled"
+          }
+        );
+
+        notificationsDnp.revalidate();
+      }
+    } catch (e) {
+      console.error(`Error on start/stop notifications package: ${e}`);
+    }
+  }
+
   return (
     <div className="notifications-settings">
       <div>
         <div className="title-switch-row">
           <SubTitle className="notifications-section-title">Enable notifications</SubTitle>
           <Switch
-            checked={notificationsEnabled}
+            checked={!notificationsDisabled}
+            disabled={notificationsDnp.isValidating}
             onToggle={() => {
-              setNotificationsEnabled(!notificationsEnabled);
+              startStopNotifications();
             }}
           />
         </div>
         <div>Enable notifications to retrieve a registry of notifications on your Dappnode.</div>
       </div>
       <br />
-      {notificationsEnabled && (
+      {!notificationsDisabled && !notificationsDnp.isValidating && (
         <div>
           <SubTitle className="notifications-section-title">Manage notifications</SubTitle>
           <div>Enable, disable and customize notifications individually.</div>
--- packages/admin-ui/src/params.ts
+++ packages/admin-ui/src/params.ts
@@ -51,6 +51,7 @@ export const ipfsDnpName = "ipfs.dnp.dappnode.eth";
 export const coreDnpName = "core.dnp.dappnode.eth";
 export const bindDnpName = "bind.dnp.dappnode.eth";
 export const vpnDnpName = "vpn.dnp.dappnode.eth";
+export const notificationsDnpName = "notifications.dnp.dappnode.eth";
 export const dappmanagerDnpName = "dappmanager.dnp.dappnode.eth";
 export const mandatoryCoreDnps = [
   dappmanagerDnpName,
@@ -131,6 +132,3 @@ export const IPFS_GATEWAY_CHECKER = "https://ipfs.github.io/public-gateway-check
 export const MAIN_ADMIN_NAME = "dappnode_admin";
 
 // Support, where to send issues
-
-// Notifications
-export const notificationsPkgName = "notifications.dnp.dappnode.eth";
