db5b7fe6fbecd5ad9286e683d5ec585dd1d8b69a
pablomendezroyo <mendez4a@gmail.com>
2025-04-24 17:01:57 +0200

    remove wifi notif from notifications view

--- packages/admin-ui/src/components/NotificationsMain.tsx
+++ packages/admin-ui/src/components/NotificationsMain.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import RenderMarkdown from "components/RenderMarkdown";
 // Selectors
-import { getWifiStatus } from "services/dappnodeStatus/selectors";
 import { pathName as systemPathName, subPaths as systemSubPaths } from "pages/system/data";
 import Button from "components/Button";
 // Style
@@ -14,21 +12,13 @@ import { AlertDismissible } from "./AlertDismissible";
  * Aggregate notification and display logic
  */
 export default function NotificationsView() {
-  const wifiStatus = useSelector(getWifiStatus);
-
-  const notifications = [
-    /**
-     * [WIFI-PASSWORD]
-     * Tell the user to change the wifi credentials
-     */
-    {
-      id: "wifiCredentials",
-      linkText: "Change",
-      linkPath: systemPathName + "/" + systemSubPaths.security,
-      body: "**Change the Dappnode WiFi credentials**, they are insecure default values.",
-      active: wifiStatus?.isDefaultPassphrase && wifiStatus?.isRunning
-    }
-  ];
+  const notifications: {
+    id: string;
+    linkText?: string;
+    linkPath?: string;
+    body: string;
+    active: boolean;
+  }[] = [];
 
   return (
     <div>
