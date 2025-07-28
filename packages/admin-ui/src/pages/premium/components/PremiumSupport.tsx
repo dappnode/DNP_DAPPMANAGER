aed8d26d19d7ed2eaa53b38c1211c38408c29b8f
mateumiralles <mateumiralles714@gmail.com>
2025-07-28 22:08:49 +0200

    notifications and support tabs

--- packages/admin-ui/src/pages/premium/components/PremiumSupport.tsx
+++ packages/admin-ui/src/pages/premium/components/PremiumSupport.tsx
@@ -1,23 +1,26 @@
 import React from "react";
-import SubTitle from "components/SubTitle";
 import Button from "components/Button";
-import Card from "components/Card";
 import { premiumSupportCalUrl } from "params";
 import newTabProps from "utils/newTabProps";
+import "./premiumSupport.scss";
+import { Card } from "react-bootstrap";
+
 export function PremiumSupport() {
   return (
     <div>
       <Card>
-        <SubTitle>Premium Support</SubTitle>
-        <p>
-          <b>4 personalized sessions</b> with Dappnode experts are included in your annual subscription.
-        </p>
+        <div className="premium-support-cont">
+          <p>
+            <b>4 personalized sessions</b> with Dappnode experts are included in your annual subscription. <br />
+            Ensure to book the session with the same email you subscribed to Premium.
+          </p>
 
-        <div>
-          <Button variant="dappnode" href={premiumSupportCalUrl} {...newTabProps}>
-            {" "}
-            Book support session{" "}
-          </Button>
+          <div className="premium-support-actions">
+            <Button variant="dappnode" href={premiumSupportCalUrl} {...newTabProps}>
+              {" "}
+              Book support session{" "}
+            </Button>
+          </div>
         </div>
       </Card>
     </div>
