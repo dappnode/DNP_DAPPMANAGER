45b6faf5579c92ba2c5441f12702a5e189382236
mateumiralles <mateumiralles714@gmail.com>
2025-07-28 22:08:49 +0200

    notifications and support tabs

--- packages/admin-ui/src/pages/premium/components/AdvancedNotifications.tsx
+++ packages/admin-ui/src/pages/premium/components/AdvancedNotifications.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import SubTitle from "components/SubTitle";
 import Button from "components/Button";
-import Card from "components/Card";
 import { useNavigate } from "react-router-dom";
 import { pathName as notisPathName, subPaths as notisSubpaths } from "pages/notifications";
+import "./advancedNotifications.scss";
+import { Card } from "react-bootstrap";
 
 export function AdvancedNotifications() {
   const settingsTabUrl = `/${notisPathName}/${notisSubpaths.settings}`;
@@ -14,19 +14,21 @@ export function AdvancedNotifications() {
   return (
     <div>
       <Card>
-        <SubTitle>Advanced Notifications</SubTitle>
-        <p>
-          Only Premium users receive push notifications about your hardware and validator performance in the subscribed
-          devices (mobile and computer). Set up your preferred notifications and manage your devices.{" "}
-        </p>
+        <div className="premium-notifications-cont">
+          <p>
+            Only Premium users receive push notifications about your hardware and validator performance in the
+            subscribed devices (mobile and computer). <br /> Set up your preferred notifications and manage your
+            devices.{" "}
+          </p>
 
-        <div>
-          <Button variant="dappnode" onClick={() => navigate(settingsTabUrl)}>
-            Set up notifications
-          </Button>
-          <Button variant="dappnode" onClick={() => navigate(devicesTabUrl)}>
-            Manage Devices
-          </Button>
+          <div className="premium-notifications-actions">
+            <Button variant="dappnode" onClick={() => navigate(settingsTabUrl)}>
+              Set up notifications
+            </Button>
+            <Button variant="dappnode" onClick={() => navigate(devicesTabUrl)}>
+              Manage Devices
+            </Button>
+          </div>
         </div>
       </Card>
     </div>
