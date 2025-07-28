c004c107f5f39e0a8f57e10c523af56b5593e396
mateumiralles <mateumiralles714@gmail.com>
2025-07-28 10:06:39 +0200

    activate UI fixes

--- packages/admin-ui/src/pages/premium/components/ActivatePremium.tsx
+++ packages/admin-ui/src/pages/premium/components/ActivatePremium.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { IconType } from "react-icons";
+import React, { useEffect, useState } from "react";
 import { docsUrl, premiumLanding, stripeDashboard } from "params";
 import newTabProps from "utils/newTabProps";
 import Button from "components/Button";
@@ -9,32 +8,40 @@ import "./activatePremium.scss";
 
 interface ActivatePremiumProps {
   isActivated: boolean;
+  prefilledLicense: string | null;
 }
 
-export function ActivatePremium({ isActivated }: ActivatePremiumProps) {
+export function ActivatePremium({ isActivated, prefilledLicense }: ActivatePremiumProps) {
   const [activationCode, setActivationCode] = useState<string>("");
+
+  useEffect(() => {
+    if (prefilledLicense) {
+      setActivationCode(prefilledLicense);
+    }
+  }, [prefilledLicense]);
+
   const handleCodeChange = (newValue: string) => {
     setActivationCode(newValue);
   };
 
-  const features: { title: string; description: string; icon: IconType }[] = [
+  const features: { title: string; description: string; icon: JSX.Element }[] = [
     {
       title: "Advanced Notifications",
       description:
         "Monitor your hardware, validators and packages at any moment with instant notifications in your phone.",
-      icon: () => <span>🔔</span> // Replace with actual icon component
+      icon: <span>🔔</span>
     },
     {
       title: "Premium Support",
       description:
         "Talk to a Dappnode expert to solve your issues and receive personalized guidance on your staking process.",
-      icon: () => <span>💬</span> // Replace with actual icon component
+      icon: <span>💬</span>
     },
     {
       title: "Beacon Node Backup",
       description:
         "Use our backup to keep your validators attesting for 7 days in case you find issues in your staking setup.",
-      icon: () => <span>📀</span> // Replace with actual icon component
+      icon: <span>📀</span>
     }
   ];
 
@@ -45,9 +52,7 @@ export function ActivatePremium({ isActivated }: ActivatePremiumProps) {
         <div className="premium-features-col">
           {features.map((feature, i) => (
             <div key={i} className="premium-feature">
-              <div className="premium-feature-icon">
-                <feature.icon />
-              </div>
+              <div className="premium-feature-icon">{feature.icon}</div>
               <div className="premium-feature-text">
                 <div>
                   <strong>{feature.title}:</strong>
@@ -95,61 +100,70 @@ export function ActivatePremium({ isActivated }: ActivatePremiumProps) {
     </Card>
   );
 
-  const ActivateCard = () => {
-    return (
-      <Card>
-        <div className="premium-card">
-          <h5>Activate Premium subscription</h5>
-          <div className="premium-activate">
-            <div>Activate the subscription with the same email in which you received the activation code. </div>
-            <div>
-              <b>Activation code:</b>
-              <Input
-                type="text"
-                onValueChange={handleCodeChange}
-                value={activationCode}
-                placeholder="xxx-xxx-xxx-xxx-xxx"
-              />
-            </div>
-            <Button variant="dappnode" onClick={() => console.log("Activate premium")}>
-              Activate
-            </Button>
-          </div>
-        </div>
-      </Card>
-    );
-  };
-
-  const DeactivateCard = () => {
-    return (
-      <Card>
-        <div className="premium-card">
-          <h5>Deactivate Premium subscription</h5>
-          <div className="premium-activate">
-            <div>Before using your license key on a different Dappnode, you have to deactivate it on this device. </div>
-            <div>
-              <b>Activation code:</b>
-              <Input
-                type="text"
-                onValueChange={() => {}}
-                value={activationCode}
-                lock={true}
-                placeholder="xxx-xxx-xxx-xxx-xxx"
-              />
-            </div>
-            <Button variant="dappnode" onClick={() => console.log("Activate premium")}>
-              Deactivate
-            </Button>
-          </div>
-        </div>
-      </Card>
-    );
-  };
-
   return (
     <div className="premium-activate-cont">
       <InfoCard />
-      {isActivated ? <DeactivateCard /> : <ActivateCard />}
+      {isActivated ? (
+        <DeactivateCard activationCode={activationCode} />
+      ) : (
+        <ActivateCard activationCode={activationCode} onCodeChange={handleCodeChange} />
+      )}
     </div>
   );
 }
+
+const ActivateCard: React.FC<{
+  activationCode: string;
+  onCodeChange: (value: string) => void;
+}> = ({ activationCode, onCodeChange }) => {
+  return (
+    <Card>
+      <div className="premium-card">
+        <h5>Activate Premium subscription</h5>
+        <div className="premium-activate">
+          <div>Activate the subscription with the same email in which you received the activation code.</div>
+          <div>
+            <b>Activation code:</b>
+            <Input
+              type="text"
+              onValueChange={onCodeChange}
+              value={activationCode}
+              placeholder="XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX-V3"
+            />
+          </div>
+          <Button variant="dappnode" onClick={() => console.log("Activate premium")}>
+            Activate
+          </Button>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+const DeactivateCard: React.FC<{
+  activationCode: string;
+}> = ({ activationCode }) => {
+  return (
+    <Card>
+      <div className="premium-card">
+        <h5>Deactivate Premium subscription</h5>
+        <div className="premium-activate">
+          <div>Before using your license key on a different Dappnode, you have to deactivate it on this device.</div>
+          <div>
+            <b>Activation code:</b>
+            <Input
+              type="text"
+              onValueChange={() => {}}
+              value={activationCode}
+              lock={true}
+              placeholder="XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX-V3"
+            />
+          </div>
+          <Button variant="dappnode" onClick={() => console.log("Deactivate premium")}>
+            Deactivate
+          </Button>
+        </div>
+      </div>
+    </Card>
+  );
+};
