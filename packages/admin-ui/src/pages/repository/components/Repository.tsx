cbec140de71de865eba3bca4f062b5e00172e716
mateumiralles <mateumiralles714@gmail.com>
2025-08-06 12:26:49 +0200

    navbar reusable component

--- packages/admin-ui/src/pages/repository/components/Repository.tsx
+++ packages/admin-ui/src/pages/repository/components/Repository.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Eth from "./Eth";
 import Ipfs from "./Ipfs";
-import { NavLink, Route, Routes } from "react-router-dom";
 import { subPaths, title } from "../data";
 import Title from "components/Title";
+import { SectionNavigator } from "components/SectionNavigator";
+import { RouteType } from "types";
 
 export const Repository: React.FC = () => {
-  const availableRoutes: {
-    name: string;
-    subPath: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    component: React.ComponentType<any>;
-  }[] = [
+  const availableRoutes: RouteType[] = [
     {
       name: "Ethereum",
       subPath: subPaths.eth,
@@ -27,23 +23,7 @@ export const Repository: React.FC = () => {
   return (
     <>
       <Title title={title} />
-      <div className="horizontal-navbar">
-        {availableRoutes.map((route) => (
-          <button key={route.subPath} className="item-container">
-            <NavLink to={route.subPath} className="item no-a-style" style={{ whiteSpace: "nowrap" }}>
-              {route.name}
-            </NavLink>
-          </button>
-        ))}
-      </div>
-
-      <div className="section-spacing">
-        <Routes>
-          {availableRoutes.map((route) => (
-            <Route key={route.subPath} path={route.subPath} element={<route.component />} />
-          ))}
-        </Routes>
-      </div>
+      <SectionNavigator routes={availableRoutes} />
     </>
   );
 };
