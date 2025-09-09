1be7f21e7a9a62dc86236552199dd0c3b91465c9
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2025-09-09 10:46:33 +0200

    Remvoe graph and truffle submodules (#2244)

--- packages/toolkit/truffle/migrations/migrations_v0.5/2_deploy_escapable.js
+++
@@ -1,7 +0,0 @@
-const Escapable = artifacts.require("Escapable");
-
-module.exports = function (deployer) {
-  const escapeHatchCaller = "0x1234567890123456789012345678901234567890";
-  const escapeHatchDestination = "0x0987654321098765432109876543210987654321";
-  deployer.deploy(Escapable, escapeHatchCaller, escapeHatchDestination);
-};
