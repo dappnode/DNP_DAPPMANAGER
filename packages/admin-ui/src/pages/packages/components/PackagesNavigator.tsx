68aa0c0db7d2ae04cd6140a52440c5846959d9aa
mateumiralles <mateumiralles714@gmail.com>
2025-09-22 11:37:11 +0200

    packages titles and back btn

--- packages/admin-ui/src/pages/packages/components/PackagesNavigator.tsx
+++ packages/admin-ui/src/pages/packages/components/PackagesNavigator.tsx
@@ -1,11 +1,12 @@
 // PackagesNavigator.tsx
 import React from "react";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route, NavLink, useMatch } from "react-router-dom";
 import { SectionNavigator } from "components/SectionNavigator";
 import { PackagesList } from "../components/PackagesList";
-import { subPaths } from "../data";
+import { subPaths, title } from "../data";
 import { PackageById } from "../pages/ById";
 import { RouteType } from "types";
+import Title from "components/Title";
 import "./packages.scss";
 
 type NavRoute = {
@@ -43,17 +44,28 @@ export const PackagesNavigator: React.FC = () => {
     }
   ];
 
+  // Hide navbar when in a package detail view
+  const scopePath: string = "/packages/:scope";
+  const match = useMatch({ path: scopePath, end: true });
+  const isBaseSubpath = !!match && routesForNavbar.some((r) => r.link === (match.params?.scope ?? ""));
+
   return (
     <>
-      <div className="horizontal-navbar">
-        {routesForNavbar.map((r) => (
-          <button key={r.subPath} className="item-container">
-            <NavLink to={r.link} className="item no-a-style" style={{ whiteSpace: "nowrap" }}>
-              {r.name}
-            </NavLink>
-          </button>
-        ))}
-      </div>
+      {isBaseSubpath && (
+        <>
+          <Title title={title} />
+
+          <div className="horizontal-navbar">
+            {routesForNavbar.map((r) => (
+              <button key={r.subPath} className="item-container">
+                <NavLink to={r.link} className="item no-a-style" style={{ whiteSpace: "nowrap" }}>
+                  {r.name}
+                </NavLink>
+              </button>
+            ))}
+          </div>
+        </>
+      )}
 
       <SectionNavigator routes={sectionRoutes} hideNavbar />
     </>
--- packages/admin-ui/src/pages/packages/data.ts
+++ packages/admin-ui/src/pages/packages/data.ts
@@ -1,9 +1,13 @@
 // This will be used later in our root reducer and selectors
 export const relativePath = "packages/my"; // default redirect to packages
-export const rootPath = "packages/*";
+export const basePath = "packages";
+export const rootPath = `${basePath}/*`;
 export const title = "Packages";
 
+export const mySubPath = "my";
+export const systemSubPath = "system";
+
 export const subPaths = {
-  my: "my/*",
-  system: "system/*"
+  my: `${mySubPath}/*`,
+  system: `${systemSubPath}/*`
 };
