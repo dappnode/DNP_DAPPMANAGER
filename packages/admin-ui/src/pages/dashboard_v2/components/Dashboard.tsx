3f5a2f8969d06167161a37c8d6e42bf02d1633b7
mateumiralles <mateumiralles714@gmail.com>
2025-10-25 05:02:12 +0200

    Loading component small variant

--- packages/admin-ui/src/pages/dashboard_v2/components/Dashboard.tsx
+++ packages/admin-ui/src/pages/dashboard_v2/components/Dashboard.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 // Own module
-import { ChainCards } from "./ChainCard";
 // Components
-import SubTitle from "components/SubTitle";
 import Title from "components/Title";
 import SystemHealth from "./SystemHealth";
 import NetworkStats from "./NetworkStats";
@@ -17,15 +15,7 @@ export default function Dashboard() {
       </div>
 
       <SystemHealth />
-
       <NetworkStats />
-
-      <div className="dashboard-layout">
-        <div className="dashboard-left">
-          <SubTitle>Chains</SubTitle>
-          <ChainCards />
-        </div>
-      </div>
     </div>
   );
 }
