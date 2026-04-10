b3786f4489bac2c11c70d061d060d6f4ef1cdbe7
mateumiralles <mateumiralles714@gmail.com>
2026-04-10 09:57:52 +0200

    nexus page wip

--- packages/admin-ui/src/pages-new/ai/nexus/data.ts
+++ packages/admin-ui/src/pages-new/ai/nexus/data.ts
@@ -1,4 +1,5 @@
 const nexusRelativePath = "/ai/nexus";
 const nexusExternalUrl = "https://nexus.dappnode.com/";
+const nexusDocsUrl = "https://nexus.docs.dappnode.com";
 
-export { nexusRelativePath, nexusExternalUrl };
+export { nexusRelativePath, nexusExternalUrl, nexusDocsUrl as nexusLandingPageUrl };
